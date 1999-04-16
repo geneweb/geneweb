9515863b34164cb1e7d719a9bd2afc641d3d5100
ddr <ddr@56c106c9-b585-4593-932e-8dfca04823a8>
1999-04-16 18:35:07 +0000

    -

--- src/perso.mli
+++ src/perso.mli
@@ -1,4 +1,12 @@
-(* $Id: perso.mli,v 2.2 1999-04-05 23:42:28 ddr Exp $ *)
+(* $Id: perso.mli,v 2.3 1999-04-16 18:35:07 ddr Exp $ *)
 (* Copyright (c) 1999 INRIA *)
 
-value print : Config.config -> Def.base -> Def.person -> unit;
+open Def;
+open Config;
+
+value copy_string_with_macros : config -> string -> unit;
+value print_titles : config -> base -> bool -> string -> person -> unit;
+value print_dates : config -> base -> bool -> person -> unit;
+value print_marriage_text : config -> base -> bool -> family -> unit;
+
+value print : config -> base -> person -> unit;
