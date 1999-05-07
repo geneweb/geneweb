bb1f93825e1ae74e22f86c59110a29b87b461478
ddr <ddr@56c106c9-b585-4593-932e-8dfca04823a8>
1999-05-07 08:00:38 +0000

    -

--- etc/README.txt
+++ etc/README.txt
@@ -1,4 +1,4 @@
-GeneWeb 2.01-1
+GeneWeb 2.02
 
 If you are under Windows NT/95/98:
    Double-click on the file "index.htm" of the directory "doc" of the
--- src/version.ml
+++ src/version.ml
@@ -1,4 +1,4 @@
-(* $Id: version.ml,v 2.1 1999-04-29 19:56:01 ddr Exp $ *)
+(* $Id: version.ml,v 2.2 1999-05-07 08:00:38 ddr Exp $ *)
 (* Copyright (c) 1999 INRIA *)
 
-value txt = "2.01-2";
+value txt = "2.02";
