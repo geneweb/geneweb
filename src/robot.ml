992796d8358cabb3dd8279d567395e609c7d8be4
ddr <ddr@56c106c9-b585-4593-932e-8dfca04823a8>
1999-08-08 05:07:28 +0000

    -

--- src/robot.ml
+++ src/robot.ml
@@ -1,5 +1,5 @@
 (* camlp4r *)
-(* $Id: robot.ml,v 1.6 1999-08-08 02:53:35 ddr Exp $ *)
+(* $Id: robot.ml,v 1.7 1999-08-08 05:07:28 ddr Exp $ *)
 (* Copyright (c) 1999 INRIA *)
 
 open Util;
@@ -30,8 +30,8 @@ value robot_error from cnt sec =
      Wserver.wprint "</h1>\n";
      Wserver.wprint "
 This address made more than %d requests in less than %d seconds.
-Considering that you has probably sent a robot, your access
-has been disconnected.
+Considering that you probably sent a robot, your access has been
+disconnected.
 <p>
 Contact the site administrator to restore it.
 " cnt sec;
@@ -97,24 +97,26 @@ value check oc tm from max_call sec =
         let (r, _, _) =
           try W.find from xcl.who with [ Not_found -> ([], tm, 0) ]
         in
-        let (cnt, r, t, tm0, nb) =
-          count r where rec count =
+        let (cnt, tml, tm0) =
+          let sec = float sec in
+          count 1 [] r where rec count cnt tml =
             fun
-            [ [t :: tl] ->
-                if tm -. t < float sec then
-                  let (cnt, tl, t1, tm0, nb) = count tl in
-                  (cnt + 1, [t :: tl], if t1 = 0.0 then t else t1,
-                   if tl = [] then t else tm0, nb + 1)
-                else (1, [], t, t, 0)
-            | [] -> (1, [], 0.0, tm, 0) ]
+            [ [] -> (cnt, tml, tm)
+            | [tm1] ->
+                if tm -. tm1 < sec then (cnt + 1, [tm1 :: tml], tm1)
+                else (cnt, tml, tm1)
+            | [tm1 :: tml1] ->
+                if tm -. tm1 < sec then count (cnt + 1) [tm :: tml] tml1
+                else (cnt, tml, tm1) ]
         in
-        do xcl.who := W.add from ([tm :: r], tm0, nb + 1) xcl.who; return
+        let r = List.rev tml in
+        do xcl.who := W.add from ([tm :: r], tm0, cnt) xcl.who; return
         let refused =
           if cnt > max_call then
             do Printf.fprintf oc " --- %s is a robot" from;
                Printf.fprintf oc
                  " (%d > %d connections in %g <= %d seconds)\n" cnt
-                 max_call (tm -. t) sec; flush Pervasives.stderr;
+                 max_call (tm -. tm0) sec; flush Pervasives.stderr;
                xcl.excl := [(from, ref 1) :: xcl.excl];
                xcl.who := W.remove from xcl.who;
                xcl.max_conn := 0;
