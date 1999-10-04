683cfbc267229b12188b49b5a6a95171a2ea3b46
ddr <ddr@56c106c9-b585-4593-932e-8dfca04823a8>
1999-10-04 18:39:16 +0000

    -

--- src/iobase.mli
+++ src/iobase.mli
@@ -1,4 +1,4 @@
-(* $Id: iobase.mli,v 2.3 1999-09-17 06:29:56 ddr Exp $ *)
+(* $Id: iobase.mli,v 2.4 1999-10-04 18:39:16 ddr Exp $ *)
 (* Copyright (c) 1999 INRIA *)
 
 open Def;
@@ -7,6 +7,7 @@ value input : string -> base;
 value output : string -> base -> unit;
 
 value simple_output : string -> base -> unit;
+value gen_output : bool -> string -> base -> unit;
 
 value lock_file : string -> string;
 
