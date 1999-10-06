a14788f58252fd57aad50de71e691fcead2869a8
ddr <ddr@56c106c9-b585-4593-932e-8dfca04823a8>
1999-10-06 08:47:56 +0000

    -

--- src/updateInd.mli
+++ src/updateInd.mli
@@ -1,14 +1,16 @@
-(* $Id: updateInd.mli,v 2.4 1999-07-28 13:08:37 ddr Exp $ *)
+(* $Id: updateInd.mli,v 2.5 1999-10-06 08:47:56 ddr Exp $ *)
 (* Copyright (c) 1999 INRIA *)
 
 open Config;
 open Def;
 
-value print_add1 : config -> base -> gen_person Update.key string -> unit;
+value print_add1 :
+  config -> base -> gen_person Update.key string -> unit;
 value print_mod1 :
   config -> base -> gen_person Update.key string -> string -> unit;
 value merge_call : config -> unit;
-value print_person : config -> base -> gen_person Update.key string -> unit;
+value print_person :
+  config -> base -> gen_person Update.key string -> unit;
 
 value print_add : config -> base -> unit;
 value print_del : config -> base -> unit;
