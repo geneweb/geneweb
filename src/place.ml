f3c2289945f57fa31ec3ac610445eaff0bcdcd76
ddr <ddr@56c106c9-b585-4593-932e-8dfca04823a8>
2000-04-02 12:50:02 +0000

    -

--- src/place.ml
+++ src/place.ml
@@ -1,5 +1,5 @@
 (* camlp4r ./pa_html.cmo *)
-(* $Id: place.ml,v 3.4 2000-03-31 19:45:01 ddr Exp $ *)
+(* $Id: place.ml,v 3.5 2000-04-02 12:50:02 ddr Exp $ *)
 (* Copyright (c) 2000 INRIA *)
 
 open Def;
@@ -7,8 +7,8 @@ open Gutil;
 open Util;
 open Config;
 
-value fold_place s =
-  loop [] 0 0 where rec loop list i ibeg =
+value fold_place inverted s =
+  let rec loop list i ibeg =
     if i == String.length s then
       if i > ibeg then [String.sub s ibeg (i - ibeg) :: list] else list
     else if
@@ -17,11 +17,19 @@ value fold_place s =
     then
       let list =
         let j =
-          if i > 0 && String.unsafe_get s (i - 1) == ' ' then i - 1 else i
+          loop i where rec loop i =
+            if i > 0 && String.unsafe_get s (i - 1) == ' ' then loop (i - 1)
+            else i
         in
         if j > ibeg then [String.sub s ibeg (j - ibeg) :: list] else list
       in
-      [String.sub s (i + 1) (String.length s - i - 2) :: list]
+      let i =
+        loop (i + 1) where rec loop i =
+          if i < String.length s && String.unsafe_get s i == ' ' then
+            loop (i + 1)
+          else i
+      in
+      [String.sub s i (String.length s - i - 1) :: list]
     else
       let (list, ibeg) =
         match String.unsafe_get s i with
@@ -34,72 +42,60 @@ value fold_place s =
         | _ -> (list, ibeg) ]
       in
       loop list (i + 1) ibeg
+  in
+  let list = loop [] 0 0 in
+  if inverted then List.rev list else list
 ;
 
-value get_list conf base =
-(*
-  let _ = base.data.persons.array () in
-  let _ = base.data.strings.array () in
-*)
+value get_all conf base =
   let add_birth = p_getenv conf.env "bi" = Some "on" in
   let add_death = p_getenv conf.env "de" = Some "on" in
   let inverted =
     try List.assoc "places_inverted" conf.base_env = "yes" with
     [ Not_found -> False ]
   in
-  loop [] 0 where rec loop list i =
-    if i = base.data.persons.len then list
+  let ini = match p_getenv conf.env "k" with [ Some s -> s | None -> "" ] in
+  let ht = Hashtbl.create 5003 in
+  let ht_add istr p =
+    let (cnt, _) =
+      try Hashtbl.find ht (istr, p.surname) with
+      [ Not_found ->
+          let cnt = (ref 0, p.cle_index) in
+          do Hashtbl.add ht (istr, p.surname) cnt; return cnt ]
+    in
+    incr cnt
+  in
+  let empty = base.func.index_of_string "" in
+  loop 0 where rec loop i =
+    if i = base.data.persons.len then
+      let list = ref [] in
+      let len = ref 0 in
+      do Hashtbl.iter
+           (fun (istr_pl, _) (cnt, ip) ->
+              let s = fold_place inverted (sou base istr_pl) in
+              if ini = "" || List.hd s = ini then
+                do list.val := [(s, cnt.val, ip) :: list.val];
+                   incr len;
+                return ()
+              else ())
+           ht;
+      return
+      let list = Sort.list (fun (s1, _, _) (s2, _, _) -> s1 <= s2) list.val in
+      (list, len.val)
     else
       let p = base.data.persons.get i in
-      let s = sou base p.surname in
-      let pl_bi =
-        if add_birth then fold_place (sou base p.birth_place) else []
-      in
-      let pl_de =
-        if add_death then fold_place (sou base p.death_place) else []
-      in
-      let list =
-        if (pl_bi = [] && pl_de = []) || not (age_autorise conf base p) then
-          list
-        else
-          let list =
-            if pl_bi = [] then list
-            else if inverted then [(List.rev pl_bi, s, i) :: list]
-            else [(pl_bi, s, i) :: list]
-          in
-          let list =
-            if pl_de = [] || pl_de = pl_bi then list
-            else if inverted then [(List.rev pl_de, s, i) :: list]
-            else [(pl_de, s, i) :: list]
-          in
-          list
-      in 
-      loop list (i + 1)
+      let pl_bi = if add_birth then p.birth_place else empty in
+      let pl_de = if add_death then p.death_place else empty in
+      do if pl_bi == empty && pl_de == empty || not (fast_auth_age conf p)
+         then ()
+         else
+           do if pl_bi != empty then ht_add pl_bi p else ();
+              if pl_de != empty then ht_add pl_de p else ();
+           return ();
+      return loop (i + 1)
 ;
 
-value sort_by_place =
-  Sort.list
-    (fun (pl1, sn1, _) (pl2, sn2, _) ->
-       if pl1 < pl2 then True
-       else if pl1 > pl2 then False
-       else sn1 <= sn2)
-;
-
-value uniq_place =
-  fun
-  [ [] -> []
-  | [(pl, sn, i) :: l] ->
-      loop [] [] pl sn i 1 l where rec loop list snl pl sn i n =
-        fun
-        [ [(pl2, sn2, i2) :: l] ->
-            if pl2 = pl then
-              if sn2 = sn then loop list snl pl sn i2 (n + 1) l
-              else loop list [(sn, n, i) :: snl] pl sn2 i2 1 l
-            else
-              loop [(pl, List.rev [(sn, n, i) :: snl]) :: list] [] pl2 sn2 i2
-                1 l
-       | [] -> List.rev [(pl, List.rev [(sn, n, i) :: snl]) :: list] ] ]
-;
+value max_len = ref 2000;
 
 value print_html_places_surnames conf base =
   do Wserver.wprint "<ul>\n"; return
@@ -125,14 +121,27 @@ value print_html_places_surnames conf base =
                       List.iter (fun p -> Wserver.wprint "<ul>\n<li>%s\n" p)
                         l2;
                    return ()
-             | _ -> failwith "bizarre" ];
+             | _ -> assert False ];
            Wserver.wprint "<ul>\n<li>\n";
+           let snl =
+             List.map
+               (fun (len, ip) ->
+                  let p = poi base ip in
+                  let sn = sou base p.surname in
+                  (len, p, sn))
+               snl
+           in
+           let snl =
+             Sort.list
+               (fun (_, _, sn1) (_, _, sn2) ->
+                  Iobase.name_key sn1 <= Iobase.name_key sn2)
+               snl
+           in
            List.iter
-             (fun (sn, n, i) ->
+             (fun (len, p, sn) ->
                 do Wserver.wprint "<a href=\"%s" (commd conf);
-                   Wserver.wprint "%s"
-                     (acces conf base (base.data.persons.get i));
-                   Wserver.wprint "\">%s</a> (%d),\n" sn n;
+                   Wserver.wprint "%s" (acces conf base p);
+                   Wserver.wprint "\">%s</a> (%d),\n" sn len;
                 return ())
              snl;
            Wserver.wprint "</ul>\n";
@@ -140,17 +149,66 @@ value print_html_places_surnames conf base =
     | [] -> List.iter (fun _ -> Wserver.wprint "</ul>\n") prev ]
 ;
 
-value print_all_places_surnames conf base =
+value print_all_places_surnames_short conf list =
+  let title _ = Wserver.wprint "%s" (capitale (transl conf "place")) in
+  let list =
+    List.fold_left
+      (fun list (pl, len, ip) ->
+         let p = List.hd pl in
+         match list with
+         [ [(p1, len1, ip1) :: list1] when p1 = p ->
+             [(p1, len1 + len, ip1) :: list1]
+         | _ -> [(p, len, ip) :: list] ])
+      [] (List.rev list)
+  in
+  let add_birth = p_getenv conf.env "bi" = Some "on" in
+  let add_death = p_getenv conf.env "de" = Some "on" in
+  let opt =
+    (if add_birth then ";bi=on" else "") ^
+    (if add_death then ";de=on" else "")
+  in
+  do Util.header conf title;
+     print_link_to_welcome conf True;
+     stag "a" "href=\"%sm=PS%s;k=\"" (commd conf) opt begin
+       Wserver.wprint "%s" (transl conf "long display");
+     end;
+     Wserver.wprint "\n<p>\n";
+     List.iter
+       (fun (s, len, ip) ->
+          Wserver.wprint "<a href=\"%sm=PS%s;k=%s\">%s</a> (%d),\n"
+            (commd conf) opt (Util.code_varenv s) s len)
+       list;
+     Util.trailer conf;
+  return ()
+;
+
+value print_all_places_surnames_long conf base list =
+  let list =
+    List.fold_left
+      (fun list (pl, len, ip) ->
+         match list with
+         [ [(pl1, lpl1) :: list1] when pl = pl1 ->
+             [(pl1, [(len, ip) :: lpl1]) :: list1]
+         | _ -> [(pl, [(len, ip)]) :: list] ])
+      [] list
+  in
+  let list = Sort.list (fun (pl1, _) (pl2, _) -> pl1 <= pl2) list in
   let title _ =
     Wserver.wprint "%s / %s" (capitale (transl conf "place"))
       (capitale (transl_nth conf "surname/surnames" 0))
   in
-  let list = get_list conf base in
-  let list = sort_by_place list in
-  let list = uniq_place list in
   do Util.header conf title;
      print_link_to_welcome conf True;
      if list = [] then () else print_html_places_surnames conf base list;
      Util.trailer conf;
   return ()
+;
+
+value print_all_places_surnames conf base =
+  let ini = p_getenv conf.env "k" in
+  let (list, len) = get_all conf base in
+  if ini = None && len > max_len.val then
+    print_all_places_surnames_short conf list
+  else
+    print_all_places_surnames_long conf base list
 ;
