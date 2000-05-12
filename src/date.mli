1d25d39a8141293453b70edc3d28ff7475ef48cd
ddr <ddr@56c106c9-b585-4593-932e-8dfca04823a8>
2000-05-12 07:49:05 +0000

    -

--- src/date.mli
+++ src/date.mli
@@ -1,4 +1,4 @@
-(* $Id: date.mli,v 3.1 2000-01-10 02:14:38 ddr Exp $ *)
+(* $Id: date.mli,v 3.2 2000-05-12 07:49:04 ddr Exp $ *)
 (* Copyright (c) 2000 INRIA *)
 
 open Def;
@@ -11,8 +11,9 @@ value year_text : dmy -> string;
 value short_dates_text : config -> base -> person -> string;
 value short_marriage_date_text :
   config -> base -> family -> person -> person -> string;
-value print_dates : config -> base ->person -> unit;
+value print_dates : config -> base -> person -> unit;
 value print_calendar : config -> base -> unit;
+value get_birth_death_date : person -> (option date * option date * bool);
 
 (* Deprecated *)
 value afficher_dates_courtes : config -> base -> person -> unit;
