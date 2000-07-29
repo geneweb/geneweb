fa3383001b66c7cbe006d639ca3218ea65910448
ddr <ddr@56c106c9-b585-4593-932e-8dfca04823a8>
2000-07-29 05:46:05 +0000

    -

--- gwtp/gwtp.ml
+++ gwtp/gwtp.ml
@@ -1,11 +1,12 @@
-(* $Id: gwtp.ml,v 1.7 2000-07-28 07:00:51 ddr Exp $ *)
+(* $Id: gwtp.ml,v 1.8 2000-07-29 05:46:05 ddr Exp $ *)
 
 open Printf;
 
-value gwtp_dir = ref "gwtp_dir";
-value comm_ged2gwb = ref "../../geneweb/ged2gwb/ged2gwb";
-value comm_consang = ref "../../geneweb/src/consang";
-value token_tmout = ref 600.0;
+value gwtp_lib = ref "../../gwtp";
+value gwtp_etc = ref "gwtp_dir";
+value gwtp_usr = ref "gwtp_dir";
+value gwtp_tmp = ref "gwtp_dir";
+value token_tmout = ref 900.0;
 
 (* Get CGI contents *)
 
@@ -56,6 +57,22 @@ value server_extract str =
 
 (* Utilitaires *)
 
+value copy_file env fname =
+  let ic = open_in (Filename.concat gwtp_lib.val (fname ^ ".txt")) in
+  do try
+       while True do
+         match input_char ic with
+         [ '%' ->
+             let c = input_char ic in
+             try print_string (List.assoc c env) with
+             [ Not_found -> do print_char '%'; print_char c; return () ]
+         | c -> print_char c ];
+       done
+     with [ End_of_file -> () ];
+     close_in ic;
+  return ()
+;
+
 value html_escaped s =
   let s = String.escaped s in
   loop 0 0 where rec loop i len =
@@ -85,7 +102,7 @@ value gwtp_invalid_request str env = gwtp_error "Invalid request";
 
 (* Login and tokens *)
 
-value tokens_file_name () = Filename.concat gwtp_dir.val "token";
+value tokens_file_name () = Filename.concat gwtp_usr.val "token";
 
 value read_tokens fname =
   match try Some (open_in fname) with [ Sys_error _ -> None ] with
@@ -125,7 +142,7 @@ value mk_passwd () =
 
 value check_login b p =
   let line1 = b ^ ":" ^ p in
-  let ic = open_in (Filename.concat gwtp_dir.val "passwd") in
+  let ic = open_in (Filename.concat gwtp_etc.val "passwd") in
   let login_ok =
     loop () where rec loop () =
       match try Some (input_line ic) with [ End_of_file -> None ] with
@@ -207,7 +224,7 @@ value send_file str env b t f fname =
        Unix.dup2 Unix.stdout Unix.stderr;
        flush stdout;
     return
-    let bdir = Filename.concat "tmp" (b ^ ".gwb") in
+    let bdir = Filename.concat gwtp_tmp.val (b ^ ".gwb") in
     do try Unix.mkdir bdir 0o777 with
        [ Unix.Unix_error Unix.EEXIST _ _ ->
            let dh = Unix.opendir bdir in
@@ -257,23 +274,7 @@ value gwtp_send str env =
 value login_ok str env b p tok =
   do set_token b tok;
      printf "content-type: text/html\r\n\r\n";
-     printf "\
-<head><title>Gwtp - data base \"%s\"</title></head>\n<body>
-<h1>Gwtp - data base \"%s\"</h1>
-<form method=POST action=gwtp enctype=\"multipart/form-data\">
-<input type=hidden name=m value=SEND>
-<input type=hidden name=b value=%s>
-<input type=hidden name=t value=%s>
-Select the file named \"base\" in your GeneWeb data base directory:<br><p>
-<input type=file name=base size=60><br><p>
-Select the file named \"notes\" (if any) in the same directory:<br><p>
-<input type=file name=notes size=60><br><p>
-Select the file named \"patches\" (if any) in the same directory:<br><p>
-<input type=file name=patches size=60><br><p>
-<input type=submit value=Send>
-</form>
-</body>
-" b b b tok;
+     copy_file [('b', b); ('t', tok)] "send";
   return ()
 ;
 
