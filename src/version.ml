1e01411ea0746b3606d67d5d55fca45bd735dfb7
ddr <ddr@56c106c9-b585-4593-932e-8dfca04823a8>
2000-09-19 11:56:02 +0000

    -

--- src/version.ml
+++ src/version.ml
@@ -1,4 +1,4 @@
-(* $Id: version.ml,v 3.9 2000-08-19 09:39:00 ddr Exp $ *)
+(* $Id: version.ml,v 3.10 2000-09-19 11:56:02 ddr Exp $ *)
 (* Copyright (c) 2000 INRIA *)
 
-value txt = "3.08";
+value txt = "3.08-exp-1";
