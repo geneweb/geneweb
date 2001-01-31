85e22076b9af91b83284877b324078754040e71e
ddr <ddr@56c106c9-b585-4593-932e-8dfca04823a8>
2001-01-31 12:34:15 +0000

    -

--- src/ascend.ml
+++ src/ascend.ml
@@ -1,5 +1,5 @@
 (* camlp4r ./def.syn.cmo ./pa_html.cmo *)
-(* $Id: ascend.ml,v 3.57 2001-01-30 16:53:22 ddr Exp $ *)
+(* $Id: ascend.ml,v 3.58 2001-01-31 12:34:15 ddr Exp $ *)
 (* Copyright (c) 2001 INRIA *)
 
 open Config;
@@ -16,7 +16,7 @@ value limit_by_list conf =
 value limit_by_tree conf =
   match p_getint conf.base_env "max_anc_tree" with
   [ Some x -> max 1 x
-  | None -> 6 ]
+  | None -> 7 ]
 ;
 
 value niveau_max_ascendance base ip =
