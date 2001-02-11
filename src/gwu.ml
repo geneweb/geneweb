9336aaa3fa7dc7dc9b717d511b568b0d310d7514
ddr <ddr@56c106c9-b585-4593-932e-8dfca04823a8>
2001-02-11 21:22:23 +0000

    -

--- src/gwu.ml
+++ src/gwu.ml
@@ -1,4 +1,4 @@
-(* $Id: gwu.ml,v 3.41 2001-02-10 22:05:37 ddr Exp $ *)
+(* $Id: gwu.ml,v 3.42 2001-02-11 21:22:23 ddr Exp $ *)
 (* Copyright (c) 2001 INRIA *)
 
 open Def;
@@ -362,7 +362,7 @@ value print_family oc base mark (per_sel, fam_sel) fam_done notes_pl_p m =
            | Female -> 'f'
            | Neuter -> '?' ]
          in
-         Printf.fprintf oc " #sex %c%c" (c m.m_fath) (c m.m_moth) ];
+         Printf.fprintf oc " #nsck %c%c" (c m.m_fath) (c m.m_moth) ];
      print_if_no_empty oc base "#mp" fam.marriage_place;
      print_if_no_empty oc base "#ms" fam.marriage_src;
      match fam.divorce with
