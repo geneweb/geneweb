9743015d90b61900a9436181f1283717e87b5469
ddr <ddr@56c106c9-b585-4593-932e-8dfca04823a8>
2001-04-18 12:05:29 +0000

    -

--- src/lock.ml
+++ src/lock.ml
@@ -1,4 +1,4 @@
-(* $Id: lock.ml,v 4.0 2001-03-16 19:34:47 ddr Exp $ *)
+(* $Id: lock.ml,v 4.1 2001-04-18 12:05:29 ddr Exp $ *)
 (* Copyright (c) 2001 INRIA *)
 
 type choice 'a 'b = [ Left of 'a | Right of 'b ];
@@ -13,30 +13,37 @@ value control lname wait f =
       [ Unix.Unix_error _ _ _ -> None ]
     with
     [ Some fd ->
-        do try Unix.chmod lname 0o666 with _ -> (); return
-        let r =
-          try
-            do if wait then Unix.lockf fd Unix.F_LOCK 0
-               else Unix.lockf fd Unix.F_TLOCK 0;
-            return Left fd
-          with e -> Right e
-        in
-        match r with
-        [ Left fd ->
-            let r = try f () with e -> do Unix.close fd; return raise e in
-            do Unix.close fd; return Some r
-        | Right (Unix.Unix_error _ _ _) -> do Unix.close fd; return None
-        | Right exc -> do Unix.close fd; return raise exc ]
+        do {
+          try Unix.chmod lname 0o666 with _ -> ();
+          let r =
+            try
+              do {
+                if wait then Unix.lockf fd Unix.F_LOCK 0
+                else Unix.lockf fd Unix.F_TLOCK 0;
+                Left fd
+              }
+            with e ->
+              Right e
+          in
+          match r with
+          [ Left fd ->
+              let r = try f () with e -> do { Unix.close fd; raise e } in
+              do { Unix.close fd; Some r }
+          | Right (Unix.Unix_error _ _ _) -> do { Unix.close fd; None }
+          | Right exc -> do { Unix.close fd; raise exc } ]
+        }
     | None -> None ]
   else
     let r =
-      try Left (Unix.openfile lname [Unix.O_RDWR; Unix.O_CREAT] 0o666) with
-        e -> Right e
+      try
+        Left (Unix.openfile lname [Unix.O_RDWR; Unix.O_CREAT] 0o666)
+      with e ->
+        Right e
     in
     match r with
     [ Left fd ->
-        let r = try f () with e -> do Unix.close fd; return raise e in
-        do Unix.close fd; return Some r
+        let r = try f () with e -> do { Unix.close fd; raise e } in
+        do { Unix.close fd; Some r }
     | Right (Unix.Unix_error _ _ _) -> None
     | Right exc -> raise exc ]
 ;
