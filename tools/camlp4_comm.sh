2dbc1733ae07408b729e9c76eeb6ab860f572ee2
ddr <ddr@56c106c9-b585-4593-932e-8dfca04823a8>
2001-04-18 07:45:50 +0000

    -

--- tools/camlp4_comm.sh
+++ tools/camlp4_comm.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
-# $Id: camlp4_comm.sh,v 4.2 2001-04-15 05:40:56 ddr Exp $
+# $Id: camlp4_comm.sh,v 4.3 2001-04-18 07:45:50 ddr Exp $
 
 ARGS1=
 FILE=
 while test "" != "$1"; do
-        case $1 in
+	case $1 in
 	*.ml*) FILE=$1;;
 	*) ARGS1="$ARGS1 $1";;
 	esac
-        shift
+	shift
 done
 
 head -1 $FILE >/dev/null || exit 1
 
 set - `head -1 $FILE`
 if test "$2" = "camlp4r" -o "$2" = "camlp4o" -o "$2" = "camlp4"; then
-        COMM="$2"
-        shift; shift
-        ARGS2=`echo $* | sed -e "s/[()*]//g"`
+	COMM="$2"
+	shift; shift
+	ARGS2=`echo $* | sed -e "s/[()*]//g"`
 else
 	COMM=camlp4r
 	ARGS2=
