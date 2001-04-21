c9fb16f54fc39b210148813266d294823c482a57
ddr <ddr@56c106c9-b585-4593-932e-8dfca04823a8>
2001-04-21 02:37:28 +0000

    -

--- src/mergeFam.ml
+++ src/mergeFam.ml
@@ -1,5 +1,5 @@
 (* camlp4r ./pa_html.cmo *)
-(* $Id: mergeFam.ml,v 4.0 2001-03-16 19:34:48 ddr Exp $ *)
+(* $Id: mergeFam.ml,v 4.1 2001-04-21 02:37:28 ddr Exp $ *)
 (* Copyright (c) 2001 INRIA *)
 
 open Config;
@@ -10,8 +10,7 @@ open Gutil;
 value need_differences_selection conf base fam1 fam2 =
   let need_selection proj =
     let x1 = proj fam1 in
-    let x2 = proj fam2 in
-    x1 <> "" && x2 <> "" && x1 <> x2
+    let x2 = proj fam2 in x1 <> "" && x2 <> "" && x1 <> x2
   in
   need_selection
     (fun fam ->
@@ -19,40 +18,40 @@ value need_differences_selection conf base fam1 fam2 =
        [ Married -> "married"
        | NotMarried -> "not married"
        | Engaged -> "engaged"
-       | NoSexesCheck -> "no sexes check" ])
-  || need_selection
+       | NoSexesCheck -> "no sexes check" ]) ||
+  need_selection
     (fun fam ->
        match Adef.od_of_codate fam.marriage with
        [ None -> ""
-       | Some d -> Date.string_of_ondate conf d ])
-  || need_selection (fun fam -> sou base fam.marriage_place)
-  || need_selection
-       (fun fam ->
-          match fam.divorce with
-          [ NotDivorced -> "not divorced"
-          | Separated -> "separated"
-          | Divorced cod ->
-              match Adef.od_of_codate cod with
-              [ Some d -> Date.string_of_ondate conf d
-              | None -> "divorced" ] ])
+       | Some d -> Date.string_of_ondate conf d ]) ||
+  need_selection (fun fam -> sou base fam.marriage_place) ||
+  need_selection
+    (fun fam ->
+       match fam.divorce with
+       [ NotDivorced -> "not divorced"
+       | Separated -> "separated"
+       | Divorced cod ->
+           match Adef.od_of_codate cod with
+           [ Some d -> Date.string_of_ondate conf d
+           | None -> "divorced" ] ])
 ;
 
 value print_differences conf base branches fam1 fam2 =
   let string_field title name proj =
     let x1 = proj fam1 in
     let x2 = proj fam2 in
-    if x1 <> "" && x2 <> "" && x1 <> x2 then
-      do Wserver.wprint "<h4>%s</h4>\n" (capitale title);
-         tag "ul" begin
-           html_li conf;
-           Wserver.wprint "<input type=radio name=\"%s\" value=1 checked>\n"
-             name;
-           Wserver.wprint "%s\n" x1;
-           html_li conf;
-           Wserver.wprint "<input type=radio name=\"%s\" value=2>\n" name;
-           Wserver.wprint "%s\n" x2;
-         end;
-      return ()
+    if x1 <> "" && x2 <> "" && x1 <> x2 then do {
+      Wserver.wprint "<h4>%s</h4>\n" (capitale title);
+      tag "ul" begin
+        html_li conf;
+        Wserver.wprint "<input type=radio name=\"%s\" value=1 checked>\n"
+          name;
+        Wserver.wprint "%s\n" x1;
+        html_li conf;
+        Wserver.wprint "<input type=radio name=\"%s\" value=2>\n" name;
+        Wserver.wprint "%s\n" x2;
+      end;
+    }
     else ()
   in
   tag "form" "method=POST action=\"%s\"" conf.command begin
@@ -65,16 +64,19 @@ value print_differences conf base branches fam1 fam2 =
     match p_getenv conf.env "ip" with
     [ Some ip -> Wserver.wprint "<input type=hidden name=ip value=%s>\n" ip
     | None -> () ];
-    loop branches where rec loop =
+    let rec loop =
       fun
       [ [(ip1, ip2)] ->
-          do Wserver.wprint "<input type=hidden name=ini1 value=%d>\n"
-               (Adef.int_of_iper ip1);
-             Wserver.wprint "<input type=hidden name=ini2 value=%d>\n"
-               (Adef.int_of_iper ip2);
-          return ()
+          do {
+            Wserver.wprint "<input type=hidden name=ini1 value=%d>\n"
+              (Adef.int_of_iper ip1);
+            Wserver.wprint "<input type=hidden name=ini2 value=%d>\n"
+              (Adef.int_of_iper ip2);
+          }
       | [_ :: branches] -> loop branches
-      | _ -> () ];
+      | _ -> () ]
+    in
+    loop branches;
     html_p conf;
     string_field (transl_nth conf "relation/relations" 0) "relation"
       (fun fam ->
@@ -91,7 +93,7 @@ value print_differences conf base branches fam1 fam2 =
          | Some d -> Date.string_of_ondate conf d ]);
     string_field
       (nominative (transl_nth conf "marriage/marriages" 0) ^ " / " ^
-       transl conf "place")
+         transl conf "place")
       "marriage_place" (fun fam -> sou base fam.marriage_place);
     string_field (transl conf "divorce") "divorce"
       (fun fam ->
@@ -115,10 +117,11 @@ value merge_fam1 conf base fam1 fam2 =
     let s = transl_nth conf "family/families" 1 in
     Wserver.wprint "%s" (capitale (transl_decline conf "merge" s))
   in
-  do header conf title;
-     print_differences conf base [] fam1 fam2;
-     trailer conf;
-  return ()
+  do {
+    header conf title;
+    print_differences conf base [] fam1 fam2;
+    trailer conf;
+  }
 ;
 
 value merge_fam conf base fam1 fam2 =
@@ -127,8 +130,7 @@ value merge_fam conf base fam1 fam2 =
   if cpl1.father = cpl2.father && cpl1.mother = cpl2.mother then
     if need_differences_selection conf base fam1 fam2 then
       merge_fam1 conf base fam1 fam2
-    else
-      MergeFamOk.print_merge conf base
+    else MergeFamOk.print_merge conf base
   else incorrect_request conf
 ;
 
@@ -136,7 +138,6 @@ value print conf base =
   match (p_getint conf.env "f1", p_getint conf.env "f2") with
   [ (Some f1, Some f2) ->
       let fam1 = base.data.families.get f1 in
-      let fam2 = base.data.families.get f2 in
-      merge_fam conf base fam1 fam2
+      let fam2 = base.data.families.get f2 in merge_fam conf base fam1 fam2
   | _ -> incorrect_request conf ]
 ;
