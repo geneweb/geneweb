71f5b624aaaf5351ee9488207787d7310ddad2d6
ddr <ddr@56c106c9-b585-4593-932e-8dfca04823a8>
2001-04-22 18:58:19 +0000

    -

--- src/i18n_check.ml
+++ src/i18n_check.ml
@@ -1,11 +1,12 @@
-(* $Id: i18n_check.ml,v 4.0 2001-03-16 19:34:45 ddr Exp $ *)
+(* $Id: i18n_check.ml,v 4.1 2001-04-22 18:55:22 ddr Exp $ *)
 (* Copyright (c) 2001 INRIA *)
 
 value usage () =
-  do Printf.eprintf "Usage: i18n_check lang lexicon\n";
-     flush stderr;
-     exit 2;
-  return ()
+  do {
+    Printf.eprintf "Usage: i18n_check lang lexicon\n";
+    flush stderr;
+    exit 2;
+  }
 ;
 
 value main () =
@@ -15,18 +16,22 @@ value main () =
     let file = Sys.argv.(2) in
     let ic = open_in_bin file in
     try
-      while True do
+      while True do {
         let line_ref = input_line ic in
-        loop (input_line ic) where rec loop line =
+        let rec loop line =
           if line = "" then ()
-          else
-            do if String.sub line 0 3 = lang ^ ":" then
-                 Printf.printf "%s\n"
-                   (String.sub line_ref 4 (String.length line_ref - 4))
-               else ();
-            return loop (input_line ic);
-      done
-    with [ End_of_file -> () ]
+          else do {
+            if String.sub line 0 3 = lang ^ ":" then
+              Printf.printf "%s\n"
+                (String.sub line_ref 4 (String.length line_ref - 4))
+            else ();
+            loop (input_line ic)
+          }
+        in
+        loop (input_line ic)
+      }
+    with
+    [ End_of_file -> () ]
 ;
 
 Printexc.catch main ();
