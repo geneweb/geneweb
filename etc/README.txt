46789bb4c6362a8b55712c882e00f60d30e6e63f
ddr <ddr@56c106c9-b585-4593-932e-8dfca04823a8>
2002-03-08 17:16:11 +0000

    -

--- etc/README.txt
+++ etc/README.txt
@@ -1,4 +1,4 @@
-GeneWeb 4.05
+GeneWeb 4.06
 
 If you are under Windows:
    Double-click on the file "index.htm" of the directory "doc",
--- src/version.ml
+++ src/version.ml
@@ -1,7 +1,7 @@
-(* $Id: version.ml,v 4.12 2002-02-28 08:53:43 ddr Exp $ *)
+(* $Id: version.ml,v 4.13 2002-03-08 17:16:11 ddr Exp $ *)
 (* Copyright (c) 2001 INRIA *)
 
-value txt = "4.06-exp";
+value txt = "4.06";
 
 value available_languages =
   ["af"; "ca"; "cs"; "da"; "de"; "en"; "es"; "eo"; "et"; "fi";
