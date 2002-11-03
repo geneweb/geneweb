caa228f8d0f869a522b27a84555de1ccb14918ac
ddr <ddr@56c106c9-b585-4593-932e-8dfca04823a8>
2002-11-03 20:16:09 +0000

    Added macro %L in setup for languages names

--- src/translate.mli
+++ src/translate.mli
@@ -1,6 +1,9 @@
 (* camlp4r *)
-(* $Id: translate.mli,v 4.1 2002-02-14 10:19:42 ddr Exp $ *)
+(* $Id: translate.mli,v 4.2 2002-11-03 20:16:09 ddr Exp $ *)
 (* Copyright (c) 2002 INRIA *)
 
 value inline : string -> char -> (char -> string) -> string -> string;
     (* [Translate.inline lang macro_char macro str] *)
+
+value language_name : string -> string -> string;
+    (* [Translate.language_name lang lang_def] *)
