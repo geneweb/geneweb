1b3be5dd3a33b52f382020ef616558cfc41727ab
ddr <ddr@56c106c9-b585-4593-932e-8dfca04823a8>
2002-11-12 12:32:40 +0000

    added Bulgarian

--- src/version.ml
+++ src/version.ml
@@ -1,10 +1,10 @@
-(* $Id: version.ml,v 4.17 2002-07-21 09:05:01 ddr Exp $ *)
+(* $Id: version.ml,v 4.18 2002-11-12 12:32:40 ddr Exp $ *)
 (* Copyright (c) 2001 INRIA *)
 
 value txt = "4.08-exp";
 
 value available_languages =
-  ["af"; "ca"; "cs"; "da"; "de"; "en"; "es"; "eo"; "et"; "fi";
+  ["af"; "bg"; "ca"; "cs"; "da"; "de"; "en"; "es"; "eo"; "et"; "fi";
    "fr"; "he"; "is"; "it"; "lv"; "nl"; "no"; "pl"; "pt"; "pt-br";
    "ru"; "sl"; "sv"; "zh"]
 ;
