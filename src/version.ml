f4bf97cd9e72811e9987c3e922654a9912b3085b
ddr <ddr@56c106c9-b585-4593-932e-8dfca04823a8>
2002-12-30 22:24:19 +0000

    -

--- src/version.ml
+++ src/version.ml
@@ -1,7 +1,7 @@
-(* $Id: version.ml,v 4.22 2002-12-30 18:40:06 ddr Exp $ *)
+(* $Id: version.ml,v 4.23 2002-12-30 22:24:19 ddr Exp $ *)
 (* Copyright (c) 2001 INRIA *)
 
-value txt = "4.09";
+value txt = "4.10-exp";
 
 value available_languages =
   ["af"; "bg"; "ca"; "cs"; "da"; "de"; "en"; "es"; "eo"; "et"; "fi";
