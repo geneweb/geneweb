bfa3b8873c0d514a442c10b1269ebe95b85a3a73
ddr <ddr@56c106c9-b585-4593-932e-8dfca04823a8>
2004-12-29 22:27:18 +0000

    -

---
+++ src/relationLink.mli
@@ -0,0 +1,35 @@
+(* camlp4r ./pa_html.cmo *)
+(* $Id: relationLink.mli,v 4.1 2004-12-29 22:27:18 ddr Exp $ *)
+(* Copyright (c) 1998-2005 INRIA *)
+
+open Def;
+open Config;
+
+type info =
+  { ip : iper;
+    sp : sex;
+    ip1 : iper;
+    ip2 : iper;
+    b1 : list (iper * sex);
+    b2 : list (iper * sex);
+    c1 : int;
+    c2 : int;
+    pb1 : option (list (iper * sex));
+    pb2 : option (list (iper * sex));
+    nb1 : option (list (iper * sex));
+    nb2 : option (list (iper * sex));
+    sp1 : option person;
+    sp2 : option person;
+    bd : int;
+    td_prop : string }
+;
+
+value threshold : ref int;
+value make_dist_tab :
+  config -> base -> iper -> int -> (iper -> int * iper -> int);
+value find_first_branch :
+  config -> base -> (iper -> int * iper -> int) ->
+    iper -> int -> iper -> sex -> option (list (iper * sex));
+value print_relation_path : config -> base -> info -> unit;
+
+value print : config -> base -> unit;
