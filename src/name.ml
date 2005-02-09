0746e76b9e4b6280ade319f7999fb0efc6637601
ddr <ddr@56c106c9-b585-4593-932e-8dfca04823a8>
2005-02-09 22:04:33 +0000

    -

--- src/name.ml
+++ src/name.ml
@@ -1,32 +1,5 @@
-(* $Id: name.ml,v 4.18 2005-02-08 19:49:29 ddr Exp $ *)
+(* $Id: name.ml,v 4.19 2005-02-09 22:04:33 ddr Exp $ *)
 (* Copyright (c) 1998-2005 INRIA *)
-
-module Buff =
-  struct
-    value buff = ref (String.create 80);
-    value store len x =
-      do {
-        if len >= String.length buff.val then
-          buff.val := buff.val ^ String.create (String.length buff.val)
-        else ();
-        buff.val.[len] := x;
-        succ len
-      }
-    ;
-    value mstore len s =
-      add_rec len 0 where rec add_rec len i =
-        if i == String.length s then len
-        else add_rec (store len s.[i]) (succ i)
-    ;
-    value gstore len s si slen =
-      let iend = si + slen in
-      add_rec len si where rec add_rec len i =
-        if i == iend || i == String.length s then len
-        else add_rec (store len s.[i]) (succ i)
-    ;
-    value get len = String.sub buff.val 0 len;
-  end
-;
 
 value utf_8_db = ref True;
 
