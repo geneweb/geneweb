47359c4c49e08c4cc2757565f087e225ecd2204a
ddr <ddr@56c106c9-b585-4593-932e-8dfca04823a8>
2005-03-01 10:38:27 +0000

    -

--- src/ppdef.ml
+++ src/ppdef.ml
@@ -1,9 +1,11 @@
 (* camlp4r *)
-(* $Id: ppdef.ml,v 4.1 2005-03-01 05:50:43 ddr Exp $ *)
+(* $Id: ppdef.ml,v 4.2 2005-03-01 10:38:27 ddr Exp $ *)
 
 open Pa_macro;
 
-defined.val := [("OCAML_307", None) :: defined.val];
+if Sys.ocaml_version >= "3.07" then
+  defined.val := [("OCAML_307", None) :: defined.val]
+else ();
 
 if Sys.ocaml_version >= "3.08" then
   defined.val := [("OCAML_308", None) :: defined.val]
