d42a95be2b6dbb161c363bdaf5266596f05e21c8
ddr <ddr@56c106c9-b585-4593-932e-8dfca04823a8>
2005-05-19 19:39:03 +0000

    -

--- src/ascend.ml
+++ src/ascend.ml
@@ -1,5 +1,5 @@
 (* camlp4r ./def.syn.cmo ./pa_html.cmo *)
-(* $Id: ascend.ml,v 4.63 2005-05-19 12:22:49 ddr Exp $ *)
+(* $Id: ascend.ml,v 4.64 2005-05-19 19:39:03 ddr Exp $ *)
 (* Copyright (c) 1998-2005 INRIA *)
 
 open Config;
@@ -211,7 +211,7 @@ value print_generation_person conf base cnt gp =
   | _ -> () ]
 ;
 
-(*
+(* ... short and long display by list ... begin *)
 value will_print =
   fun
   [ GP_person _ _ _ -> True
@@ -794,7 +794,7 @@ value display_ancestors_with_numbers_long conf base max_level ws wn p =
     trailer conf
   }
 ;
-*)
+(* ... short and long display by list ... end *)
 
 value print_ancestors_same_time_descendants conf base p a =
   let maxlen =
@@ -2031,6 +2031,9 @@ value print_surnames_list conf base v p =
 ;
 
 value ancmenu_print = Perso.interp_templ "ancmenu";
+(*
+value anclist_print = Perso.interp_templ "anclist";
+*)
 
 value print conf base p =
   match (p_getenv conf.env "t", p_getint conf.env "v") with
@@ -2039,13 +2042,13 @@ value print conf base p =
       if p_getenv conf.env "only" = Some "on" then
         display_ancestors_level conf base v p
       else
-(*
-        display_ancestors_with_numbers conf base v p
-*)
-        Perso.interp_templ "anclist" conf base p
 (**)
-  | (Some "G", Some v) ->
+        display_ancestors_with_numbers conf base v p
 (*
+        anclist_print conf base p
+*)
+  | (Some "G", Some v) ->
+(**)
       let ws =
         match p_getenv conf.env "siblings" with
         [ Some "on" -> True
@@ -2057,9 +2060,9 @@ value print conf base p =
         | _ -> False ]
       in
       display_ancestors_with_numbers_long conf base v ws wn p
+(*
+      anclist_print conf base p
 *)
-      Perso.interp_templ "anclist" conf base p
-(**)
   | (Some "M", Some v) ->
       let al =
         match p_getenv conf.env "al" with
