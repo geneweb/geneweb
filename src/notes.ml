d203ea27c19d9c753aa814d5da0f141c50e559a9
ddr <ddr@56c106c9-b585-4593-932e-8dfca04823a8>
2005-06-02 07:52:37 +0000

    -

--- src/notes.ml
+++ src/notes.ml
@@ -1,5 +1,5 @@
 (* camlp4r ./pa_html.cmo *)
-(* $Id: notes.ml,v 4.13 2005-06-02 04:34:11 ddr Exp $ *)
+(* $Id: notes.ml,v 4.14 2005-06-02 07:52:37 ddr Exp $ *)
 
 open Config;
 open Def;
@@ -45,7 +45,7 @@ value linkify conf s =
     else loop (i + 1) (Buff.store len s.[i])
 ;
 
-value level_of_line s len =
+value section_level s len =
   if len > 4 && s.[1] = '=' && s.[len-2] = '=' then
     if len > 6 && s.[2] = '=' && s.[len-3] = '=' then
       if len > 8 && s.[3] = '=' && s.[len-4] = '=' then 4
@@ -68,7 +68,7 @@ value insert_sub_part s v sub_part =
       [ [s :: sl] ->
           let len = String.length s in
           if len > 2 && s.[0] = '=' && s.[len-1] = '=' then
-            let nlev = level_of_line s len in
+            let nlev = section_level s len in
             if cnt = v then loop [""; sub_part :: lines] nlev (cnt + 1) sl
             else if cnt > v then
               if nlev > lev then loop lines lev (cnt + 1) sl
@@ -95,7 +95,7 @@ value extract_sub_part s v =
       [ [s :: sl] ->
           let len = String.length s in
           if len > 2 && s.[0] = '=' && s.[len-1] = '=' then
-            let nlev = level_of_line s len in
+            let nlev = section_level s len in
             if cnt = v then loop [s :: lines] nlev (cnt + 1) sl
             else if cnt > v then
               if nlev > lev then loop [s :: lines] lev (cnt + 1) sl
@@ -116,57 +116,64 @@ value html_of_structure conf s =
       else if s.[i] = '\n' then loop [Buff.get len :: lines] 0 (i + 1)
       else loop lines (Buff.store len s.[i]) (i + 1)
   in
-  let (_, _, _, _, index) =
+  let tab lev = String.make (2 * lev) ' ' in
+  let (rev_index, lev, _, _, _, _, _) =
     List.fold_left
-      (fun (prev_lev, num, nums, cnt, index) s ->
-         let len = String.length s in
-         if len > 2 && s.[0] = '=' && s.[len-1] = '=' then
-           let lev = level_of_line s len in
-           let (num, nums) =
-             if lev > prev_lev then (0, [num :: nums])
-             else if lev < prev_lev then (List.hd nums + 1, List.tl nums)
-             else (num + 1, nums)
-           in
-           let s =
-             let inx =
-               List.tl
-                 (List.rev_map (fun x -> string_of_int (x + 1)) [num :: nums])
-             in
-             Printf.sprintf "<li><a href=\"#a_%d\">%s %s</a></li>" cnt
-               (String.concat "." inx) (String.sub s lev (len - 2 * lev))
-           in
-           let index =
-             let ul = "<ul style=\"list-style:none\">" in
-             if lev > prev_lev then
-               let sl =
-                 loop (lev - 1) [ul :: index] where rec loop lev sl =
-                   if lev <= prev_lev then sl
-                   else loop (lev - 1) [ul; "<li>" :: sl]
-               in
-               [s :: sl]
-             else if lev < prev_lev then
-               let sl =
-                 loop (lev + 1) index where rec loop lev sl =
-                   if lev >= prev_lev then ["</ul>" :: sl]
-                   else loop (lev + 1) ["</li>"; "</ul>" :: sl]
-               in
-               [s :: sl]
-             else [s :: index]
-           in
-           (lev, num, nums, cnt + 1, index)
-         else (prev_lev, num, nums, cnt, index))
-      (0, 0, [], 0, []) lines
+      (fun (index, lev, prev_slev, slev_stack, num, nums, cnt) s ->
+        let len = String.length s in
+        if len > 2 && s.[0] = '=' && s.[len-1] = '=' then
+          let slev = section_level s len in
+          let fs nums =
+            Printf.sprintf "<a href=\"#a_%d\">%s %s</a>"
+              cnt (String.concat "." (List.rev_map string_of_int nums))
+              (String.sub s slev (len - 2 * slev))
+          in
+          if slev > prev_slev then
+            let index = [tab (lev + 1) ^ "<ul>" :: index] in
+            let index = [tab (lev + 1) ^ "<li>" ^ fs [1 :: nums] :: index] in
+            (index, lev + 1, slev, [prev_slev :: slev_stack], 1,
+             [num :: nums], cnt + 1)
+          else if slev < prev_slev then
+            loop index lev num nums [prev_slev :: slev_stack]
+            where rec loop index lev num nums =
+              fun
+              [ [prev_slev :: slev_stack] ->
+                  if slev < prev_slev then
+                    if lev > 1 then
+                      let index = [tab lev ^ "</li>" :: index] in
+                      let index = [tab lev ^ "</ul>" :: index] in
+                      loop index (lev - 1) (List.hd nums) (List.tl nums) slev_stack
+                    else loop index lev num nums slev_stack
+                  else
+                    let index = [tab lev ^ "</li>" :: index] in
+                    let index = [tab lev ^ "<li>arg" ^ fs [num + 1 :: nums] :: index] in
+                    (index, lev, prev_slev, slev_stack, num + 1, nums, cnt + 1)
+              | [] ->
+                  (["not implem 3" :: index], lev, prev_slev, slev_stack, 1,
+                   [], cnt + 1) ]
+          else
+            let index = [tab lev ^ "</li>" :: index] in
+            let index = [tab lev ^ "<li>" ^ fs [num + 1 :: List.tl nums] :: index] in
+            (index, lev, prev_slev, slev_stack, num + 1, [num + 1 :: List.tl nums], cnt + 1)
+        else (index, lev, prev_slev, slev_stack, num, nums, cnt))
+      ([], 0, 0, [], 1, [], 0) lines
+  in
+  let rev_index =
+    loop lev rev_index where rec loop lev index =
+      if lev > 0 then
+        loop (lev - 1) [tab lev ^ "</ul>"; tab lev ^ "</li>" :: index]
+      else index
   in
   let index =
-    if index <> [] then
+    if rev_index <> [] then
       ["<dl><dd>";
        "<table border=\"1\"><tr><td>";
        "<table><tr>";
        "<td align=\"center\"><b>" ^ capitale (transl conf "summary") ^
          "</b></td>";
        "</tr><tr><td>" ::
-       List.rev_append index
-         ["</ul>"; "</td><td>";
+       List.rev_append rev_index
+         ["</td><td>";
           "<ul style=\"list-style:none\"><li>&nbsp;</li></ul>";
           "</td></tr></table"; "</td></tr></table>";
           "</dd></dl>"]]
@@ -185,7 +192,7 @@ value html_of_structure conf s =
       (fun (lines, cnt) s ->
          let len = String.length s in
          if len > 2 && s.[0] = '=' && s.[len-1] = '=' then
-           let lev = level_of_line s len in
+           let lev = section_level s len in
            let s =
              Printf.sprintf "<h%d>%s%s</h%d>" lev
                (String.sub s lev (len-2*lev))
