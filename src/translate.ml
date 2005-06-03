44e3098287c976d8f9c841450700d39e69276726
ddr <ddr@56c106c9-b585-4593-932e-8dfca04823a8>
2005-06-03 11:34:48 +0000

    -

--- src/translate.ml
+++ src/translate.ml
@@ -1,27 +1,8 @@
 (* camlp4r *)
-(* $Id: translate.ml,v 4.4 2004-12-14 09:30:17 ddr Exp $ *)
+(* $Id: translate.ml,v 4.5 2005-06-03 11:34:48 ddr Exp $ *)
 (* Copyright (c) 1998-2005 INRIA *)
 
-module Buff =
-  struct
-    value buff = ref (String.create 80);
-    value store len x =
-      do {
-        if len >= String.length buff.val then
-          buff.val := buff.val ^ String.create (String.length buff.val)
-        else ();
-        buff.val.[len] := x;
-        succ len
-      }
-    ;
-    value mstore len s =
-      add_rec len 0 where rec add_rec len i =
-        if i == String.length s then len
-        else add_rec (store len s.[i]) (succ i)
-    ;
-    value get len = String.sub buff.val 0 len;
-  end
-;
+module Buff = Buff.Make (struct value buff = ref (String.create 80); end);
 
 value skip_lang s =
   loop where rec loop i =
