885b6ada84d2e73f83acb8d0c3c4b5e0e85eb820
ddr <ddr@56c106c9-b585-4593-932e-8dfca04823a8>
2005-06-21 09:27:22 +0000

    -

--- src/gwd.ml
+++ src/gwd.ml
@@ -1,5 +1,5 @@
 (* camlp4r pa_extend.cmo ./pa_html.cmo ./pa_lock.cmo *)
-(* $Id: gwd.ml,v 4.82 2005-06-18 21:15:36 ddr Exp $ *)
+(* $Id: gwd.ml,v 4.83 2005-06-21 09:27:22 ddr Exp $ *)
 (* Copyright (c) 1998-2005 INRIA *)
 
 open Config;
@@ -1087,8 +1087,8 @@ value conf_and_connection cgi from (addr, request) script_name contents env =
             match mode with
             [ Some "DOC" -> Doc.print conf
             | Some "WDOC" -> Doc.print_wdoc conf
-            | Some "MOD_WDOC" when conf.wizard -> Doc.print_mod_wdoc conf
-            | Some "MOD_WDOC_OK" when conf.wizard -> Doc.print_mod_wdoc_ok conf
+            | Some "MOD_WDOC" -> Doc.print_mod_wdoc conf
+            | Some "MOD_WDOC_OK" -> Doc.print_mod_wdoc_ok conf
             | _ ->
                if conf.bname = "" then general_welcome conf
                else
