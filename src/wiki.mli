9233566aefe622764685ae12b0b3336c70cae360
ddr <ddr@56c106c9-b585-4593-932e-8dfca04823a8>
2005-07-07 19:33:53 +0000

    -

--- src/wiki.mli
+++ src/wiki.mli
@@ -1,5 +1,5 @@
 (* camlp4r *)
-(* $Id: wiki.mli,v 4.1 2005-07-07 18:32:27 ddr Exp $ *)
+(* $Id: wiki.mli,v 4.2 2005-07-07 19:30:23 ddr Exp $ *)
 (* Copyright (c) 1998-2005 INRIA *)
 
 open Config;
@@ -7,12 +7,12 @@ open Config;
 value lines_list_of_string : string -> (list string * bool);
 
 value syntax_links : config -> string -> (string -> string) -> string -> string;
-value html_of_tlsw_lines :
-  config -> string -> string -> int -> bool -> list string ->
-    list string -> list string;
+
+value html_of_tlsw : config -> string -> list string;
 value html_with_summary_of_tlsw :
   config -> string -> (string -> string) -> string -> string -> string;
-value rev_extract_sub_part : string -> int -> list string;
+
+value extract_sub_part : string -> int -> list string;
 value insert_sub_part : string -> int -> string -> string;
 value split_title_and_text : string -> (string * string);
 
