6be1f96b2907bf29930259d136192b81bc6fe315
ddr <ddr@56c106c9-b585-4593-932e-8dfca04823a8>
2005-08-18 15:11:36 +0000

    -

--- src/notesLinks.ml
+++ src/notesLinks.ml
@@ -1,16 +1,17 @@
 (* camlp4r *)
-(* $Id: notesLinks.ml,v 1.9 2005-08-18 08:18:26 ddr Exp $ *)
+(* $Id: notesLinks.ml,v 1.10 2005-08-18 15:11:36 ddr Exp $ *)
 
 open Def;
 
-value magic_notes_links = "GWNL0004";
+value magic_notes_links = "GWNL0006";
 type page =
   [ PgInd of iper
   | PgNotes
   | PgMisc of string
   | PgWizard of string ]
 ;
-type notes_links_db = list (page * list string);
+type key = (string * string * int);
+type notes_links_db = list (page * (list string * list key));
 
 value char_dir_sep = ':';
 
@@ -32,33 +33,80 @@ value check_file_name s =
           else None ]
 ;
 
+type wiki_link =
+  [ WLpage of int and (list string * string) and string and string and string
+  | WLperson of int and key and string
+  | WLnone ]
+;
+
 value misc_notes_link s i =
   let slen = String.length s in
-  if i < slen - 2 && s.[i] = '[' && s.[i+1] = '[' && s.[i+2] = '[' then
-    let j =
-      loop (i + 3) where rec loop j =
-        if j = slen then j
-        else if
-          j < slen - 2 && s.[j] = ']' && s.[j+1] = ']' && s.[j+2] = ']'
-        then j + 3
-        else loop (j + 1)
-    in
-    if j > i + 6 then
-      let b = String.sub s (i + 3) (j - i - 6) in
-      let (fname, anchor, text) =
-        try
-          let k = String.rindex b '/' in
-          let j = try String.index b '#' with [ Not_found -> k ] in
-          (String.sub b 0 j, String.sub b j (k - j),
-           String.sub b (k + 1) (String.length b - k - 1))
-        with
-        [ Not_found -> (b, "", b) ]
+  if i < slen - 2 && s.[i] = '[' && s.[i+1] = '[' then
+    if s.[i+2] = '[' then
+      let j =
+        loop (i + 3) where rec loop j =
+          if j = slen then j
+          else if
+            j < slen - 3 && s.[j] = ']' && s.[j+1] = ']' && s.[j+2] = ']'
+          then j + 3
+          else loop (j + 1)
       in
-      match check_file_name fname with
-      [ Some pg_path -> Some (j, pg_path, fname, anchor, text)
-      | None -> None ]
-    else None
-  else None
+      if j > i + 6 then
+        let b = String.sub s (i + 3) (j - i - 6) in
+        let (fname, anchor, text) =
+          try
+            let k = String.rindex b '/' in
+            let j = try String.index b '#' with [ Not_found -> k ] in
+            (String.sub b 0 j, String.sub b j (k - j),
+             String.sub b (k + 1) (String.length b - k - 1))
+          with
+          [ Not_found -> (b, "", b) ]
+        in
+        match check_file_name fname with
+        [ Some pg_path -> WLpage j pg_path fname anchor text
+        | None -> WLnone ]
+      else WLnone
+    else
+      let j =
+        loop (i + 2) where rec loop j =
+          if j = slen then j
+          else if j < slen -2 && s.[j] = ']' && s.[j+1] = ']' then j + 2
+          else loop (j + 1)
+      in
+      let b = String.sub s (i + 2) (j - i - 4) in
+      try
+        let k = 0 in
+        let l = String.index_from b k '/' in
+        let fn = String.sub b k (l - k) in
+        let k = l + 1 in
+        let (fn, sn, oc, name) =
+          try
+            let l = String.index_from b k '/' in
+            let sn = String.sub b k (l - k) in
+            let (oc, name) =
+              try
+                let k = l + 1 in
+                let l = String.index_from b k '/' in
+                let x = String.sub b k (l - k) in
+                (x, String.sub b (l + 1) (String.length b - l - 1))
+              with
+              [ Not_found ->
+                  ("", String.sub b (l + 1) (String.length b - l - 1)) ]
+            in
+            let oc = try int_of_string oc with [ Failure _ -> 0 ] in
+            (fn, sn, oc, name)
+          with
+          [ Not_found ->
+              let sn = String.sub b k (String.length b - k) in
+              let name = fn ^ " " ^ sn in
+              (fn, sn, 0, name) ]
+        in
+        let fn = Name.lower fn in
+        let sn = Name.lower sn in
+        WLperson j (fn, sn, oc) name
+      with
+      [ Not_found -> WLnone ]
+  else WLnone
 ;
 
 value read_db_from_file fname =
@@ -85,23 +133,26 @@ value share ht s =
 value share_strings db =
   let ht = Hashtbl.create 103 in
   List.map
-    (fun (page, list) ->
+    (fun (page, (list_nt, list_ind)) ->
        let page =
          match page with
          [ PgMisc s -> PgMisc (share ht s)
          | PgWizard s -> PgWizard (share ht s)
          | x -> x ]
        in
-       let list = List.map (share ht) list in
-       (page, list))
+       let list_nt = List.map (share ht) list_nt in
+       (page, (list_nt, list_ind)))
     db
 ;
 
-value update_db bdir who list =
+value update_db bdir who (list_nt, list_ind) =
   let fname = Filename.concat bdir "notes_links" in
   let notes_links_db = read_db_from_file fname in
   let db = List.remove_assoc who notes_links_db in
-  let new_db = if list = [] then db else [(who, list) :: db] in
+  let new_db =
+    if list_nt = [] && list_ind = [] then db
+    else [(who, (list_nt, list_ind)) :: db]
+  in
   let new_db = share_strings new_db in
   let oc = open_out_bin fname in
   do {
