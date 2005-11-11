70fccecea537ae0f43362b70690298bc2be36922
ddr <ddr@56c106c9-b585-4593-932e-8dfca04823a8>
2005-11-11 22:59:09 +0000

    -

--- src/name.ml
+++ src/name.ml
@@ -1,4 +1,4 @@
-(* $Id: name.ml,v 4.24 2005-09-05 02:21:17 ddr Exp $ *)
+(* $Id: name.ml,v 4.25 2005-11-11 22:59:09 ddr Exp $ *)
 (* Copyright (c) 1998-2005 INRIA *)
 
 value utf_8_db = ref True;
@@ -21,6 +21,166 @@ value unaccent_iso_8859_1 =
   | c -> c ]
 ;
 
+value nbc c =
+  if Char.code c < 0b10000000 then 1
+  else if Char.code c < 0b10000000 then -1
+  else if Char.code c < 0b11100000 then 2
+  else if Char.code c < 0b11110000 then 3
+  else if Char.code c < 0b11111000 then 4
+  else if Char.code c < 0b11111100 then 5
+  else if Char.code c < 0b11111110 then 6
+  else -1
+;
+
+value unaccent_utf_8 s i =
+  let nbc = nbc s.[i] in
+  if nbc = 1 || nbc < 0 || i + nbc > String.length s then
+    (String.make 1 (Char.lowercase s.[i]), i + 1)
+  else
+    let c = Char.code s.[i] in
+    let s =
+      match c with
+      [ 0xC2 -> String.make 1 (Char.lowercase s.[i+1])
+      | 0xC3 ->
+          match Char.code s.[i+1] with
+          [ 0x80 | 0x81 | 0x82 | 0x83 | 0x84 | 0x85 | 0x86 -> "a"
+          | 0x87 -> "c"
+          | 0x88 | 0x89 | 0x8A | 0x8B -> "e"
+          | 0x8C | 0x8D | 0x8E | 0x8F -> "i"
+          | 0x91 -> "n"
+          | 0x92 | 0x93 | 0x94 | 0x95 | 0x96 | 0x98 -> "o"
+          | 0x99 | 0x9A | 0x9B | 0x9C -> "u"
+          | 0x9F -> "z"
+          | 0xA0 | 0xA1 | 0xA2 | 0xA3 | 0xA4 | 0xA5 | 0xA6 -> "a"
+          | 0xA7 -> "c"
+          | 0xA8 | 0xA9 | 0xAA | 0xAB -> "e"
+          | 0xAC | 0xAD | 0xAE | 0xAF -> "i"
+          | 0xB1 -> "n"
+          | 0xB2 | 0xB3 | 0xB4 | 0xB5 | 0xB6 | 0xB8 -> "o"
+          | 0xB9 | 0xBA | 0xBB | 0xBC -> "u"
+          | 0xBF -> "y"
+          | _ ->
+              let c = Char.lowercase (Char.chr (Char.code s.[i+1] + 0x40)) in
+              String.make 1 c ]
+      | 0xC4 ->
+          match Char.code s.[i+1] with
+          [ 0x80 | 0x81 | 0x82 | 0x83 | 0x84 | 0x85 -> "a"
+          | 0x86 | 0x87 | 0x88 | 0x89
+          | 0x8A | 0x8B | 0x8C | 0x8D -> "c"
+          | 0x8E | 0x8F | 0x90 | 0x91 -> "d"
+          | 0x92 | 0x93 | 0x94 | 0x95 | 0x96
+          | 0x97 | 0x98 | 0x99 | 0x9A | 0x9B -> "e"
+          | 0x9C | 0x9D | 0x9E | 0x9F
+          | 0xA0 | 0xA1 | 0xA2 | 0xA3 -> "g"
+          | 0xA4 | 0xA5 | 0xA6 | 0xA7 -> "h"
+          | 0xA8 | 0xA9 | 0xAA | 0xAB | 0xAC
+          | 0xAD | 0xAE | 0xAF | 0xB0 | 0xB1 -> "i"
+          | 0xB2 | 0xB3 -> "ij"
+          | 0xB4 | 0xB5 -> "i"
+          | 0xB6 | 0xB7 | 0xB8 -> "k"
+          | 0xB9 | 0xBA | 0xBB | 0xBC
+          | 0xBD | 0xBE | 0xBF -> "l"
+          | _ -> String.sub s i nbc ]
+      | 0xC5 ->
+          match Char.code s.[i+1] with
+          [ 0x80 | 0x81 | 0x82 -> "l"
+          | 0x83 | 0x84 | 0x85 | 0x86 | 0x87
+          | 0x88 | 0x89 | 0x8A | 0x8B -> "n"
+          | 0x8C | 0x8D | 0x8E | 0x8F | 0x90 | 0x91 -> "o"
+          | 0x92 | 0x93 -> "oe"
+          | 0x94 | 0x95 | 0x96 | 0x97 | 0x98 | 0x99 -> "r"
+          | 0x9A | 0x9B | 0x9C | 0x9D
+          | 0x9E | 0x9F | 0xA0 | 0xA1 -> "s"
+          | 0xA2 | 0xA3 | 0xA4 | 0xA5 | 0xA6 | 0xA7 -> "t"
+          | 0xA8 | 0xA9 | 0xAA | 0xAB | 0xAC | 0xAD
+          | 0xAE | 0xAF | 0xB0 | 0xB1 | 0xB2 | 0xB3 -> "u"
+          | 0xB4 | 0xB5 -> "w"
+          | 0xB6 | 0xB7 | 0xB8 -> "y"
+          | 0xB9 | 0xBA | 0xBB | 0xBC | 0xBD | 0xBE -> "z"
+          | _ -> String.sub s i nbc ]
+      | 0xCE ->
+          match Char.code s.[i+1] with
+          [ 0x91 | 0xB1 -> "a"
+          | 0x92 | 0xB2 -> "b"
+          | 0x93 | 0xB3 -> "g"
+          | 0x94 | 0xB4 -> "d"
+          | 0x95 | 0xB5 -> "e"
+          | 0x96 | 0xB6 -> "dz"
+          | 0x97 | 0xB7 -> "e"
+          | 0x98 | 0xB8 -> "th"
+          | 0x99 | 0xB9 -> "i"
+          | 0x9A | 0xBA -> "k"
+          | 0x9B | 0xBB -> "l"
+          | 0x9C | 0xBC -> "m"
+          | 0x9D | 0xBD -> "n"
+          | 0x9E | 0xBE -> "x"
+          | 0x9F | 0xBF -> "o"
+          | 0xA0 -> "p"
+          | 0xA1 -> "r"
+          | 0xA2 | 0xA3 -> "s"
+          | 0xA4 -> "t"
+          | 0xA5 -> "u"
+          | 0xA6 -> "ph"
+          | 0xA7 -> "kh"
+          | 0xA8 -> "ps"
+          | 0xA9 -> "o"
+          | _ -> String.sub s i nbc ]
+      | 0xCF ->
+          match Char.code s.[i+1] with
+          [ 0x80 -> "p"
+          | 0x81 -> "r"
+          | 0x82 | 0x83 -> "s"
+          | 0x84 -> "t"
+          | 0x85 -> "u"
+          | 0x86 -> "ph"
+          | 0x87 -> "kh"
+          | 0x88 -> "ps"
+          | 0x89 -> "o"
+          | _ -> String.sub s i nbc ]
+      | 0xD0 ->
+          match Char.code s.[i+1] with
+          [ 0x90 | 0xB0 -> "a"
+          | 0x91 | 0xB1 -> "b"
+          | 0x92 | 0xB2 -> "v"
+          | 0x93 | 0xB3 -> "g"
+          | 0x94 | 0xB4 -> "d"
+          | 0x95 | 0xB5 -> "e"
+          | 0x96 | 0xB6 -> "j"
+          | 0x97 | 0xB7 -> "z"
+          | 0x98 | 0xB8 -> "i"
+          | 0x9A | 0xBA -> "k"
+          | 0x9B | 0xBB -> "l"
+          | 0x9C | 0xBC -> "m"
+          | 0x9D | 0xBD -> "n"
+          | 0x9E | 0xBE -> "o"
+          | 0x9F | 0xBF -> "p"
+          | 0xA0 -> "r"
+          | 0xA1 -> "s"
+          | 0xA2 -> "t"
+          | 0xA3 -> "u"
+          | 0xA4 -> "f"
+          | 0xA7 -> "tch"
+          | 0xAE -> "you"
+          | 0xAF -> "ya"
+          | _ -> String.sub s i nbc ]
+      | 0xD1 ->
+          match Char.code s.[i+1] with
+          [ 0x80 -> "r"
+          | 0x81 -> "s"
+          | 0x82 -> "t"
+          | 0x83 -> "u"
+          | 0x84 -> "f"
+          | 0x87 -> "tch"
+          | 0x8E -> "you"
+          | 0x8F -> "ya"
+          | _ -> String.sub s i nbc ]
+      | c ->
+          String.sub s i nbc ]
+    in
+    (s, i + nbc)
+;
+
+(*
 value char_iso_8859_1_of_utf_8 s i =
   if Char.code s.[i] < 0x80 then Some (s.[i], i + 1)
   else if Char.code s.[i] = 0xC2 then
@@ -31,10 +191,12 @@ value char_iso_8859_1_of_utf_8 s i =
     Some (c, i + 2)
   else None
 ;
+*)
 
 value next_chars_if_equiv s i t j =
   if i >= String.length s || j >= String.length t then None
   else if utf_8_db.val then
+(*
     match (char_iso_8859_1_of_utf_8 s i, char_iso_8859_1_of_utf_8 t j) with
     [ (Some (c1, i), Some (c2, j)) ->
         if unaccent_iso_8859_1 (Char.lowercase c1) =
@@ -43,6 +205,11 @@ value next_chars_if_equiv s i t j =
     | (None, None) ->
         if s.[i] = t.[j] then Some (i + 1, j + 1) else None
     | _ -> None ]
+*)
+    let (s1, i1) = unaccent_utf_8 s i in
+    let (t1, j1) = unaccent_utf_8 t j in
+    if s1 = t1 then Some (i1, j1) else None
+(**)
   else if s.[i] = t.[j] then Some (i + 1, j + 1)
   else if
     unaccent_iso_8859_1 (Char.lowercase s.[i]) =
@@ -64,161 +231,12 @@ value lower s =
       | c ->
           copy (len <> 0) (i + 1) len ]
     else (* start of utf-8 multi-byte char *)
-      let c = Char.code s.[i] in
-      let nbc =
-        if c < 0b11100000 then 2
-        else if c < 0b11110000 then 3
-        else if c < 0b11111000 then 4
-        else if c < 0b11111100 then 5
-        else if c < 0b11111110 then 6
-        else (* bad utf-8 *) 1
-      in
-      if i + nbc > String.length s then (* bad utf-8 *) Buff.get len
-      else
-        let len = if special then Buff.store len ' ' else len in
-        match c with
-        [ 0xC2 | 0xC3 ->
-            let c1 =
-              if c = 0xC2 then Char.chr (Char.code s.[i+1])
-              else Char.chr (Char.code s.[i+1] + 0x40)
-            in
-            let c2 = unaccent_iso_8859_1 (Char.lowercase c1) in
-            match c2 with
-            [ 'a'..'z' | 'A'..'Z' | '0'..'9' | '.' ->
-                copy False (i + 2) (Buff.store len c2)
-            | _ -> copy (len <> 0) (i + 2) len ]
-        | 0xC4 ->
-            let len =
-              match Char.code s.[i+1] with
-              [ 0x80 | 0x81 | 0x82 | 0x83 | 0x84 | 0x85 -> Buff.store len 'a'
-              | 0x86 | 0x87 | 0x88 | 0x89
-              | 0x8A | 0x8B | 0x8C | 0x8D -> Buff.store len 'c'
-              | 0x8E | 0x8F | 0x90 | 0x91 -> Buff.store len 'd'
-              | 0x92 | 0x93 | 0x94 | 0x95 | 0x96
-              | 0x97 | 0x98 | 0x99 | 0x9A | 0x9B -> Buff.store len 'e'
-              | 0x9C | 0x9D | 0x9E | 0x9F
-              | 0xA0 | 0xA1 | 0xA2 | 0xA3 -> Buff.store len 'g'
-              | 0xA4 | 0xA5 | 0xA6 | 0xA7 -> Buff.store len 'h'
-              | 0xA8 | 0xA9 | 0xAA | 0xAB | 0xAC
-              | 0xAD | 0xAE | 0xAF | 0xB0 | 0xB1 -> Buff.store len 'i'
-              | 0xB2 | 0xB3 -> Buff.mstore len "ij"
-              | 0xB4 | 0xB5 -> Buff.store len 'j'
-              | 0xB6 | 0xB7 | 0xB8 -> Buff.store len 'k'
-              | 0xB9 | 0xBA | 0xBB | 0xBC
-              | 0xBD | 0xBE | 0xBF -> Buff.store len 'l'
-              | _ -> Buff.gstore len s i nbc ]
-            in
-            copy False (i + nbc) len
-        | 0xC5 ->
-            let len =
-              match Char.code s.[i+1] with
-              [ 0x80 | 0x81 | 0x82 -> Buff.store len 'l'
-              | 0x83 | 0x84 | 0x85 | 0x86 | 0x87
-              | 0x88 | 0x89 | 0x8A | 0x8B -> Buff.store len 'n'
-              | 0x8C | 0x8D | 0x8E | 0x8F | 0x90 | 0x91 -> Buff.store len 'o'
-              | 0x92 | 0x93 -> Buff.mstore len "oe"
-              | 0x94 | 0x95 | 0x96 | 0x97 | 0x98 | 0x99 -> Buff.store len 'r'
-              | 0x9A | 0x9B | 0x9C | 0x9D
-              | 0x9E | 0x9F | 0xA0 | 0xA1 -> Buff.store len 's'
-              | 0xA2 | 0xA3 | 0xA4 | 0xA5 | 0xA6 | 0xA7 -> Buff.store len 't'
-              | 0xA8 | 0xA9 | 0xAA | 0xAB | 0xAC | 0xAD
-              | 0xAE | 0xAF | 0xB0 | 0xB1 | 0xB2 | 0xB3 -> Buff.store len 'u'
-              | 0xB4 | 0xB5 -> Buff.store len 'w'
-              | 0xB6 | 0xB7 | 0xB8 -> Buff.store len 'y'
-              | 0xB9 | 0xBA | 0xBB | 0xBC | 0xBD | 0xBE -> Buff.store len 'z'
-              | _ -> Buff.gstore len s i nbc ]
-            in
-            copy False (i + nbc) len
-        | 0xCE ->
-            let len =
-              match Char.code s.[i+1] with
-              [ 0x91 | 0xB1 -> Buff.store len 'a'
-              | 0x92 | 0xB2 -> Buff.store len 'b'
-              | 0x93 | 0xB3 -> Buff.store len 'g'
-              | 0x94 | 0xB4 -> Buff.store len 'd'
-              | 0x95 | 0xB5 -> Buff.store len 'e'
-              | 0x96 | 0xB6 -> Buff.mstore len "dz"
-              | 0x97 | 0xB7 -> Buff.store len 'e'
-              | 0x98 | 0xB8 -> Buff.mstore len "th"
-              | 0x99 | 0xB9 -> Buff.store len 'i'
-              | 0x9A | 0xBA -> Buff.store len 'k'
-              | 0x9B | 0xBB -> Buff.store len 'l'
-              | 0x9C | 0xBC -> Buff.store len 'm'
-              | 0x9D | 0xBD -> Buff.store len 'n'
-              | 0x9E | 0xBE -> Buff.store len 'x'
-              | 0x9F | 0xBF -> Buff.store len 'o'
-              | 0xA0 -> Buff.store len 'p'
-              | 0xA1 -> Buff.store len 'r'
-              | 0xA2 | 0xA3 -> Buff.store len 's'
-              | 0xA4 -> Buff.store len 't'
-              | 0xA5 -> Buff.store len 'u'
-              | 0xA6 -> Buff.mstore len "ph"
-              | 0xA7 -> Buff.mstore len "kh"
-              | 0xA8 -> Buff.mstore len "ps"
-              | 0xA9 -> Buff.store len 'o'
-              | _ -> Buff.gstore len s i nbc ]
-            in
-            copy False (i + nbc) len
-        | 0xCF ->
-            let len =
-              match Char.code s.[i+1] with
-              [ 0x80 -> Buff.store len 'p'
-              | 0x81 -> Buff.store len 'r'
-              | 0x82 | 0x83 -> Buff.store len 's'
-              | 0x84 -> Buff.store len 't'
-              | 0x85 -> Buff.store len 'u'
-              | 0x86 -> Buff.mstore len "ph"
-              | 0x87 -> Buff.mstore len "kh"
-              | 0x88 -> Buff.mstore len "ps"
-              | 0x89 -> Buff.store len 'o'
-              | _ -> Buff.gstore len s i nbc ]
-            in
-            copy False (i + nbc) len
-        | 0xD0 ->
-            let len =
-              match Char.code s.[i+1] with
-              [ 0x90 | 0xB0 -> Buff.store len 'a'
-              | 0x91 | 0xB1 -> Buff.store len 'b'
-              | 0x92 | 0xB2 -> Buff.store len 'v'
-              | 0x93 | 0xB3 -> Buff.store len 'g'
-              | 0x94 | 0xB4 -> Buff.store len 'd'
-              | 0x95 | 0xB5 -> Buff.store len 'e'
-              | 0x96 | 0xB6 -> Buff.store len 'j'
-              | 0x97 | 0xB7 -> Buff.store len 'z'
-              | 0x98 | 0xB8 -> Buff.store len 'i'
-              | 0x9A | 0xBA -> Buff.store len 'k'
-              | 0x9B | 0xBB -> Buff.store len 'l'
-              | 0x9C | 0xBC -> Buff.store len 'm'
-              | 0x9D | 0xBD -> Buff.store len 'n'
-              | 0x9E | 0xBE -> Buff.store len 'o'
-              | 0x9F | 0xBF -> Buff.store len 'p'
-              | 0xA0 -> Buff.store len 'r'
-              | 0xA1 -> Buff.store len 's'
-              | 0xA2 -> Buff.store len 't'
-              | 0xA3 -> Buff.store len 'u'
-              | 0xA4 -> Buff.store len 'f'
-              | 0xA7 -> Buff.mstore len "tch"
-              | 0xAE -> Buff.mstore len "you"
-              | 0xAF -> Buff.mstore len "ya"
-              | _ -> Buff.gstore len s i nbc ]
-            in
-            copy False (i + nbc) len
-        | 0xD1 ->
-            let len =
-              match Char.code s.[i+1] with
-              [ 0x80 -> Buff.store len 'r'
-              | 0x81 -> Buff.store len 's'
-              | 0x82 -> Buff.store len 't'
-              | 0x83 -> Buff.store len 'u'
-              | 0x84 -> Buff.store len 'f'
-              | 0x87 -> Buff.mstore len "tch"
-              | 0x8E -> Buff.mstore len "you"
-              | 0x8F -> Buff.mstore len "ya"
-              | _ -> Buff.gstore len s i nbc ]
-            in
-            copy False (i + nbc) len
-        | c ->
-            copy False (i + nbc) (Buff.gstore len s i nbc) ]
+      let len = if special then Buff.store len ' ' else len in
+      let (t, j) = unaccent_utf_8 s i in
+      match t.[0] with
+      [ 'a'..'z' | 'A'..'Z' | '0'..'9' | '.' ->
+          copy False j (Buff.mstore len t)
+      | _ -> copy (len <> 0) j len ]
 ;
 
 (* Name.abbrev *)
