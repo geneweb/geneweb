814343dfa066521011b16deb4580e9e79a78a409
ddr <ddr@56c106c9-b585-4593-932e-8dfca04823a8>
2006-08-25 09:54:30 +0000

    -

--- src/utf82old.sh
+++ src/utf82old.sh
@@ -14,36 +14,35 @@ function disp() {
         system("echo \"" $0 "\" | sed -e 's/Ä\\214/È/g' -e 's/Ä\\201/â/g' -e 's/Å\\206/ò/g' -e 's/Ä¼/ï/g' -e 's/Ä\\215/è/g' -e 's/Å¡/ð/g' -e 's/Å«/û/g' -e 's/Ä\\223/ç/g' -e 's/Ä¢/Ì/g' -e 's/Ä£/ì/g' -e 's/Ä¶/Í/g' -e 's/Ä«/î/g' -e 's/Ä·/í/g' -e 's/Ã¾/þ/g'")
     else system("echo \"" $0 "\" | iconv -f utf-8 -t " enc);
     fflush(stdout);
-    next;
 }
 function conv(t) { enc=t; disp() }
-/^af: / { conv("iso-8859-1") }
+/^af: / { conv("iso-8859-1"); next }
 /^ar: / { enc="ar"; next }
-/^bg: / { conv("windows-1251") }
-/^br: / { conv("iso-8859-1") }
-/^ca: / { conv("iso-8859-1") }
-/^cs: / { conv("iso-8859-2") }
-/^da: / { conv("iso-8859-1") }
-/^eo: / { conv("iso-8859-3") }
-/^de: / { conv("iso-8859-1") }
-/^es: / { conv("iso-8859-1") }
-/^et: / { conv("iso-8859-15") }
-/^fi: / { conv("iso-8859-1") }
-/^fr: / { conv("iso-8859-1") }
-/^fr-cr: / { conv("iso-8859-1") }
-/^he: / { conv("iso-8859-8") }
-/^is: / { conv("iso-8859-1") }
-/^it: / { conv("iso-8859-1") }
-/^lv: / { conv("lv") }
-/^nl: / { conv("iso-8859-1") }
-/^no: / { conv("iso-8859-1") }
-/^pl: / { conv("iso-8859-2") }
-/^pt: / { conv("iso-8859-1") }
-/^pt-br: / { conv("iso-8859-1") }
-/^ru: / { conv("windows-1251") }
-/^sl: / { conv("iso-8859-2") }
-/^sv: / { conv("iso-8859-1") }
-/^zh: / { conv("gb2312") }
+/^bg: / { conv("windows-1251"); next }
+/^br: / { conv("iso-8859-1"); next }
+/^ca: / { conv("iso-8859-1"); next }
+/^cs: / { conv("iso-8859-2"); next }
+/^da: / { conv("iso-8859-1"); next }
+/^eo: / { conv("iso-8859-3"); next }
+/^de: / { conv("iso-8859-1"); next }
+/^es: / { conv("iso-8859-1"); next }
+/^et: / { conv("iso-8859-15"); next }
+/^fi: / { conv("iso-8859-1"); next }
+/^fr: / { conv("iso-8859-1"); next }
+/^fr-cr: / { conv("iso-8859-1"); next }
+/^he: / { conv("iso-8859-8"); next }
+/^is: / { conv("iso-8859-1"); next }
+/^it: / { conv("iso-8859-1"); next }
+/^lv: / { conv("lv"); next }
+/^nl: / { conv("iso-8859-1"); next }
+/^no: / { conv("iso-8859-1"); next }
+/^pl: / { conv("iso-8859-2"); next }
+/^pt: / { conv("iso-8859-1"); next }
+/^pt-br: / { conv("iso-8859-1"); next }
+/^ru: / { conv("windows-1251"); next }
+/^sl: / { conv("iso-8859-2"); next }
+/^sv: / { conv("iso-8859-1"); next }
+/^zh: / { conv("gb2312"); next }
 enc== "" { print; next }
 /^  / { disp(); next }
 { enc=""; print; next }
