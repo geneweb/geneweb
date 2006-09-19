0e198d7d0af05d2fe2c4ce137379f8b1831481a5
ddr <ddr@56c106c9-b585-4593-932e-8dfca04823a8>
2006-09-19 10:31:24 +0000

    -

--- src/merge.ml
+++ src/merge.ml
@@ -1,15 +1,17 @@
 (* camlp4r ./pa_html.cmo *)
-(* $Id: merge.ml,v 5.2 2006-05-12 11:48:22 ddr Exp $ *)
+(* $Id: merge.ml,v 5.3 2006-09-19 10:31:24 ddr Exp $ *)
 (* Copyright (c) 1998-2006 INRIA *)
 
-open Def;
 open Config;
+open Def;
 open Gutil;
+open Gwdb;
 open Util;
 
 value print_someone conf base p =
   Wserver.wprint "%s%s %s" (p_first_name base p)
-    (if p.occ == 0 then "" else "." ^ string_of_int p.occ) (p_surname base p)
+    (if get_occ p == 0 then "" else "." ^ string_of_int (get_occ p))
+    (p_surname base p)
 ;
 
 value print conf base p =
@@ -23,7 +25,8 @@ value print conf base p =
   let list = Gutil.find_same_name base p in
   let list =
     List.fold_right
-      (fun p1 pl -> if p1.cle_index = p.cle_index then pl else [p1 :: pl])
+      (fun p1 pl ->
+         if get_cle_index p1 = get_cle_index p then pl else [p1 :: pl])
       list []
   in
   do {
@@ -34,7 +37,7 @@ value print conf base p =
         Util.hidden_env conf;
         xtag "input" "type=\"hidden\" name=\"m\" value=\"MRG_IND\"";
         xtag "input" "type=\"hidden\" name=\"i\" value=\"%d\""
-          (Adef.int_of_iper p.cle_index);
+          (Adef.int_of_iper (get_cle_index p));
         Wserver.wprint "%s " (capitale (transl_decline conf "with" ""));
         if list <> [] then do {
           Wserver.wprint ":";
@@ -58,25 +61,25 @@ type=\"radio\" name=\"select\" value=\"input\" checked=\"checked\"";
            tag "tr" "align=\"%s\"" conf.left begin
              tag "td" "valign=\"top\"" begin
                xtag "input" "type=\"radio\" name=\"select\" value=\"%d\""
-                 (Adef.int_of_iper p.cle_index);
+                 (Adef.int_of_iper (get_cle_index p));
              end;
              tag "td" begin
                stag "a" "href=\"%s%s\"" (commd conf) (acces conf base p) begin
-                 Wserver.wprint "%s.%d %s" (sou base p.first_name) p.occ
-                   (sou base p.surname);
+                 Wserver.wprint "%s.%d %s" (sou base (get_first_name p))
+                   (get_occ p) (sou base (get_surname p));
                end;
                Wserver.wprint "%s" (Date.short_dates_text conf base p);
                match main_title conf base p with
                [ Some t -> Wserver.wprint "%s" (one_title_text conf base p t)
                | None -> () ];
-               match parents (aoi base p.cle_index) with
+               match parents (aoi base (get_cle_index p)) with
                [ Some ifam ->
                    let cpl = coi base ifam in
                    Wserver.wprint ",\n%s"
                      (Util.translate_eval
                        (transl_a_of_b conf
                           (transl_nth conf "son/daughter/child"
-                             (index_of_sex p.sex))
+                             (index_of_sex (get_sex p)))
                           (person_title_text conf base
                              (poi base (father cpl)) ^
                              " " ^ transl_nth conf "and" 0 ^ " " ^
