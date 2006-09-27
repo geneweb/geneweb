b7eb4e71b5c1a1ab45b27ef0aba6dee1e2ab98a7
ddr <ddr@56c106c9-b585-4593-932e-8dfca04823a8>
2006-09-27 18:21:40 +0000

    -

--- src/wiznotes.ml
+++ src/wiznotes.ml
@@ -1,5 +1,5 @@
 (* camlp4r ./pa_html.cmo *)
-(* $Id: wiznotes.ml,v 5.12 2006-09-27 12:31:06 ddr Exp $ *)
+(* $Id: wiznotes.ml,v 5.13 2006-09-27 18:21:40 ddr Exp $ *)
 (* Copyright (c) 1998-2006 INRIA *)
 
 open Config;
@@ -30,17 +30,17 @@ value read_auth_file base fname =
                   with
                   [ Not_found -> "" ]
                 in
-                let (wizname, wizorder) =
+                let (wizname, wizorder, islash) =
                   try
                     let i = String.index wizname '/' in
                     let w1 = String.sub wizname 0 i in
                     let l = String.length wizname in
                     let w2 = String.sub wizname (i + 1) (l - i - 1) in
-                    (w1 ^ w2, w2 ^ w1)
+                    (w1 ^ w2, w2 ^ w1, i)
                   with
-                  [ Not_found -> (wizname, "~") ]
+                  [ Not_found -> (wizname, "~", -1) ]
                 in
-                [(String.sub line 0 i, (wizname, wizorder)) :: data]
+                [(String.sub line 0 i, (wizname, (wizorder, islash))) :: data]
               with
               [ Not_found -> data ]
             in
@@ -107,6 +107,228 @@ value wiznote_date wfile =
   | None -> ("", 0.) ]
 ;
 
+type item 'a =
+  [ ItemChar of string
+  | ItemRule of int
+  | ItemWiz of 'a ]
+;
+
+value dispatch_in_columns ncol list =
+  let (rlist, len, _) =
+    List.fold_left
+      (fun (rlist, len, prev) wiz ->
+         let (_, (_, (ord, _)), _, _) = wiz in
+         let (rlist, len) =
+           match prev with
+           [ Some prev_ord when ord.[0] = prev_ord.[0] -> (rlist, len)
+           | _ ->
+               ([ItemRule 1; ItemRule 2; ItemChar ord :: rlist],
+                len + 3) ]
+         in
+         let rlist = [ItemWiz wiz :: rlist] in
+         (rlist, len + 1, Some ord))
+      ([], 0, None) list
+  in
+  let list = List.rev rlist in
+  loop [] len 0 ncol where rec loop len_list len cum_len ncol =
+    if ncol = 1 then (List.rev [len :: len_list], list)
+    else
+      let len_i =
+        let len_i = (len + ncol / 2) / ncol in
+        match List.nth list (cum_len + len_i - 1) with
+        [ ItemChar c -> len_i + 3
+        | ItemRule n -> len_i + n
+        | ItemWiz (wz, _, _, _) -> len_i ]
+      in
+      loop [len_i :: len_list] (len - len_i) (cum_len + len_i) (ncol - 1)
+;
+
+value print_wizards_by_alphabetic_order conf list =
+if p_getenv conf.env "old" <> Some "on" then
+  let (len_list, list) =
+    let ncols =
+      match p_getint conf.env "ncols" with
+      [ Some n -> max 1 n
+      | None -> 3 ]
+    in
+    dispatch_in_columns ncols list
+  in
+  tag "table" "width=\"100%%\"" begin
+    tag "tr" "valign=\"top\"" begin
+      Wserver.wprint "<td>\n";
+      let _ =
+        List.fold_left
+          (fun list len ->
+             loop True len list where rec loop top n list =
+               if n = 0 then do {
+                 Wserver.wprint "</ul>\n</td>\n<td>\n";
+                 list
+               }
+               else
+                 match list with
+                 [ [item :: list] -> do {
+                     match item with
+                     [ ItemChar c -> do {
+                         if not top then Wserver.wprint "</ul>\n" else ();
+                         Wserver.wprint "<h3 style=\"border-bottom: \
+                           solid 1px\">%c</h3>\n" c.[0];
+                         Wserver.wprint "<ul>\n";
+                       }
+                     | ItemRule _ -> ()
+                     | ItemWiz (wz, (wname, (_, islash)), wfile, stm) -> do {
+                         let tm = Unix.localtime stm in
+                         if top then Wserver.wprint "<ul>\n" else ();
+                         let with_link =
+                           conf.wizard && conf.user = wz || wfile <> ""
+                         in
+                         stag "li" begin
+                           if with_link then
+                             Wserver.wprint
+                               "<a href=\"%sm=WIZNOTES;f=%s%t\">"
+                               (commd conf) (Util.code_varenv wz)
+                               (fun _ ->
+                                  Wserver.wprint
+                                   ";d=%d-%02d-%02d,%02d:%02d:%02d"
+                                   (tm.Unix.tm_year + 1900)
+                                   (tm.Unix.tm_mon + 1) tm.Unix.tm_mday
+                                   tm.Unix.tm_hour tm.Unix.tm_min
+                                   tm.Unix.tm_sec)
+                           else ();
+                           if islash > 0 then
+                             let s1 =
+                               let islash =
+                                 if wname.[islash-1] = ' ' then
+                                   islash - 1
+                                 else islash
+                               in
+                               String.sub wname 0 islash
+                             in
+                             let s2 =
+                               String.sub wname islash
+                                 (String.length wname - islash)
+                             in
+                             Wserver.wprint "%s (%s)" s2 s1
+                           else
+                             Wserver.wprint "%s" wname;
+                           if with_link then Wserver.wprint "</a>"
+                           else ();
+                         end;
+                         Wserver.wprint "\n";
+                       } ];
+                     loop False (n - 1) list
+                   }
+                 | [] -> [] ])
+          list len_list
+      in
+      Wserver.wprint "</ul>\n</td>\n";
+    end;
+  end
+else
+  tag "p" begin
+    let _ =
+      List.fold_left
+        (fun prev (wz, (wname, (worder, _)), wfile, stm) ->
+           let tm = Unix.localtime stm in
+           let wname = if wname = "" then wz else wname in
+           do {
+             Wserver.wprint "%s" (if prev = None then "" else ",\n");
+             match prev with
+             [ Some prev_worder when worder.[0] = prev_worder.[0] -> ()
+             | _ ->
+                 if worder.[0] = '~' then ()
+                 else Wserver.wprint "<b>(%c)</b>\n" worder.[0] ];
+             if conf.wizard && conf.user = wz || wfile <> "" then
+               Wserver.wprint "<a href=\"%sm=WIZNOTES;f=%s%t\">%s</a>"
+                 (commd conf) (Util.code_varenv wz)
+                 (fun _ ->
+                    Wserver.wprint ";d=%d-%02d-%02d,%02d:%02d:%02d"
+                      (tm.Unix.tm_year + 1900) (tm.Unix.tm_mon + 1)
+                      tm.Unix.tm_mday tm.Unix.tm_hour tm.Unix.tm_min
+                      tm.Unix.tm_sec)
+                 wname
+             else Wserver.wprint "%s" wname;
+             Some worder;
+           })
+        None list
+    in
+    Wserver.wprint "\n";
+  end
+;
+
+value print_wizards_by_date conf list = do {
+  let sep_period_list =
+    [(fun tm -> tm.Unix.tm_mon,
+      fun tm ->
+        let dmy =
+          {year = tm.Unix.tm_year + 1900; month = tm.Unix.tm_mon + 1;
+           day = 0; prec = Sure; delta = 0}
+        in
+        Wserver.wprint "%s"
+          (capitale (Date.string_of_ondate conf (Dgreg dmy Dgregorian))));
+     (fun tm -> tm.Unix.tm_year,
+      fun tm -> Wserver.wprint "%d" (tm.Unix.tm_year + 1900))]
+  in
+  let list =
+    List.sort (fun (_, _, _, mtm1) (_, _, _, mtm2) -> compare mtm2 mtm1)
+      list
+  in
+  Wserver.wprint "<dl>\n<dt>";
+  let _ =
+    List.fold_left
+      (fun (spl, prev) (wz, (wname, _), wfile, stm) -> do {
+         let tm = Unix.localtime stm in
+         let (new_item, spl) =
+           match prev with
+           [ Some prev_tm ->
+               let (sep_period, _) =
+                 match spl with
+                 [ [sp :: _] -> sp
+                 | [] -> assert False ]
+               in
+               if sep_period tm <> sep_period prev_tm then do {
+                 Wserver.wprint "</dd>\n<dt>";
+                 let spl =
+                   match spl with
+                   [ [_; (next_sp, _) :: _] ->
+                       if next_sp tm <> next_sp prev_tm then List.tl spl
+                       else spl
+                   | _ -> spl ]
+                 in
+                 (True, spl)
+               }
+               else (False, spl)
+           | None -> (True, spl) ]
+         in
+         if new_item then
+           if stm = 0.0 then Wserver.wprint "....."
+           else
+             match spl with
+             [ [(_, disp_sep_period) :: _] -> disp_sep_period tm
+             | [] -> () ]
+         else ();
+         if new_item then Wserver.wprint "</dt>\n<dd>\n" else ();
+         let wname = if wname = "" then wz else wname in
+         Wserver.wprint "%s%t"
+           (if prev = None || new_item then "" else ",\n")
+           (fun _ ->
+              if conf.wizard && conf.user = wz || wfile <> "" then
+                Wserver.wprint "<a href=\"%sm=WIZNOTES;f=%s%t\">%s</a>"
+                  (commd conf) (Util.code_varenv wz)
+                  (fun _ ->
+                     Wserver.wprint ";d=%d-%02d-%02d,%02d:%02d:%02d"
+                       (tm.Unix.tm_year + 1900) (tm.Unix.tm_mon + 1)
+                       tm.Unix.tm_mday tm.Unix.tm_hour tm.Unix.tm_min
+                       tm.Unix.tm_sec)
+                  wname
+              else Wserver.wprint "%s" wname);
+         (spl, Some tm)
+       })
+      (sep_period_list, None) list
+  in
+  ();
+  Wserver.wprint "</dd></dl>\n"
+};
+
 value print_main conf base auth_file =
   let wiztxt =
     Util.translate_eval (transl_nth conf "wizard/wizards/friend/friends" 1)
@@ -120,7 +342,9 @@ value print_main conf base auth_file =
     let list = read_auth_file base auth_file in
     if by_alphab_order then
       List.sort
-        (fun (_, (_, o1)) (_, (_, o2)) -> Gutil.alphabetic_order o1 o2) list
+        (fun (_, (_, (o1, _))) (_, (_, (o2, _))) ->
+           Gutil.alphabetic_order o1 o2)
+        list
     else list
   in
   let wddir = dir conf in
@@ -134,112 +358,8 @@ value print_main conf base auth_file =
            (wz, wname, wfile, wnote))
         wizdata
     in
-    if by_alphab_order then
-      tag "p" begin
-        let _ =
-          List.fold_left
-            (fun prev (wz, (wname, worder), wfile, stm) ->
-               let tm = Unix.localtime stm in
-               let wname = if wname = "" then wz else wname in
-               do {
-                 Wserver.wprint "%s" (if prev = None then "" else ",\n");
-                 match prev with
-                 [ Some prev_worder when worder.[0] = prev_worder.[0] -> ()
-                 | _ ->
-                     if worder.[0] = '~' then ()
-                     else Wserver.wprint "<b>(%c)</b>\n" worder.[0] ];
-                 if conf.wizard && conf.user = wz || wfile <> "" then
-                   Wserver.wprint "<a href=\"%sm=WIZNOTES;f=%s%t\">%s</a>"
-                     (commd conf) (Util.code_varenv wz)
-                     (fun _ ->
-                        Wserver.wprint ";d=%d-%02d-%02d,%02d:%02d:%02d"
-                          (tm.Unix.tm_year + 1900) (tm.Unix.tm_mon + 1)
-                          tm.Unix.tm_mday tm.Unix.tm_hour tm.Unix.tm_min
-                          tm.Unix.tm_sec)
-                     wname
-                 else Wserver.wprint "%s" wname;
-                 Some worder;
-               })
-            None list
-        in
-        Wserver.wprint "\n";
-      end
-    else do {
-      let sep_period_list =
-        [(fun tm -> tm.Unix.tm_mon,
-          fun tm ->
-            let dmy =
-              {year = tm.Unix.tm_year + 1900; month = tm.Unix.tm_mon + 1;
-               day = 0; prec = Sure; delta = 0}
-            in
-            Wserver.wprint "%s"
-              (capitale (Date.string_of_ondate conf (Dgreg dmy Dgregorian))));
-         (fun tm -> tm.Unix.tm_year,
-          fun tm -> Wserver.wprint "%d" (tm.Unix.tm_year + 1900))]
-      in
-      let list =
-        List.sort (fun (_, _, _, mtm1) (_, _, _, mtm2) -> compare mtm2 mtm1)
-          list
-      in
-      Wserver.wprint "<dl>\n<dt>";
-      let _ =
-        List.fold_left
-          (fun (spl, prev) (wz, (wname, _), wfile, stm) ->
-             let tm = Unix.localtime stm in
-             let (new_item, spl) =
-               match prev with
-               [ Some prev_tm ->
-                   let (sep_period, _) =
-                     match spl with
-                     [ [sp :: _] -> sp
-                     | [] -> assert False ]
-                   in
-                   if sep_period tm <> sep_period prev_tm then do {
-                     Wserver.wprint "</dd>\n<dt>";
-                     let spl =
-                       match spl with
-                       [ [_; (next_sp, _) :: _] ->
-                           if next_sp tm <> next_sp prev_tm then List.tl spl
-                           else spl
-                       | _ -> spl ]
-                     in
-                     (True, spl)
-                   }
-                   else (False, spl)
-               | None -> (True, spl) ]
-             in
-             do {
-               if new_item then
-                 if stm = 0.0 then Wserver.wprint "....."
-                 else
-                   match spl with
-                   [ [(_, disp_sep_period) :: _] -> disp_sep_period tm
-                   | [] -> () ]
-               else ();
-               if new_item then Wserver.wprint "</dt>\n<dd>\n" else ();
-                let () =
-                 let wname = if wname = "" then wz else wname in
-                 Wserver.wprint "%s%t"
-                   (if prev = None || new_item then "" else ",\n")
-                   (fun _ ->
-                      if conf.wizard && conf.user = wz || wfile <> "" then
-                        Wserver.wprint "<a href=\"%sm=WIZNOTES;f=%s%t\">%s</a>"
-                          (commd conf) (Util.code_varenv wz)
-                          (fun _ ->
-                             Wserver.wprint ";d=%d-%02d-%02d,%02d:%02d:%02d"
-                               (tm.Unix.tm_year + 1900) (tm.Unix.tm_mon + 1)
-                               tm.Unix.tm_mday tm.Unix.tm_hour tm.Unix.tm_min
-                               tm.Unix.tm_sec)
-                          wname
-                      else Wserver.wprint "%s" wname)
-               in
-               (spl, Some tm)
-             })
-          (sep_period_list, None) list
-      in
-      ();
-      Wserver.wprint "</dd></dl>\n"
-    };
+    if by_alphab_order then print_wizards_by_alphabetic_order conf list
+    else print_wizards_by_date conf list;
     tag "p" begin
       Wserver.wprint "%d %s\n" (List.length wizdata) wiztxt;
     end;
