a24c56f92463aec7b22eae729ddbefe3f22e8e4f
ddr <ddr@56c106c9-b585-4593-932e-8dfca04823a8>
2006-10-04 11:56:35 +0000

    - Trying to limit the usage of Gwdb.apply_base...

--- src/check.mli
+++ src/check.mli
@@ -1,13 +1,10 @@
-(* $Id: check.mli,v 5.1 2006-10-01 11:30:07 ddr Exp $ *)
+(* $Id: check.mli,v 5.2 2006-10-04 11:56:35 ddr Exp $ *)
 (* Copyright (c) 2006 INRIA *)
 
 open Gwdb;
 
-value print_base_error :
-  out_channel -> base -> Def.error person -> unit;
-value print_base_warning :
-  out_channel -> base ->
-    Def.warning person descend (Def.gen_title Adef.istr) -> unit;
+value print_base_error : out_channel -> base -> Gutil.base_error -> unit;
+value print_base_warning : out_channel -> base -> Gutil.base_warning -> unit;
 
 value check_base :
   base -> (Gutil.base_error -> unit) -> (Gutil.base_warning -> unit) ->
