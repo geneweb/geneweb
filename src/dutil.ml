3143632ee2a11026c954799b5f17230a0e36f554
ddr <ddr@56c106c9-b585-4593-932e-8dfca04823a8>
2006-10-21 02:24:57 +0000

    - Internal: made the function "person_misc_names" usable everywhere.

--- src/dutil.ml
+++ src/dutil.ml
@@ -1,4 +1,4 @@
-(* $Id: dutil.ml,v 5.8 2006-10-20 05:29:43 ddr Exp $ *)
+(* $Id: dutil.ml,v 5.9 2006-10-21 02:24:57 ddr Exp $ *)
 (* Copyright (c) 2006 INRIA *)
 
 open Dbdisk;
@@ -21,9 +21,11 @@ value sou base i = base.data.strings.get (Adef.int_of_istr i);
 value p_first_name base p = nominative (sou base p.first_name);
 value p_surname base p = nominative (sou base p.surname);
 
-value dsk_person_misc_names base p nobtit =
-  let first_name = p_first_name base p in
-  let surname = p_surname base p in
+value gen_person_misc_names first_name surname public_name qualifiers aliases
+    first_names_aliases surnames_aliases titles husbands
+    father_titles_places =
+  let first_name = nominative first_name in
+  let surname = nominative surname in
   if first_name = "?" || surname = "?" then []
   else
     let public_names =
@@ -35,50 +37,35 @@ value dsk_person_misc_names base p nobtit =
                match t.t_name with
                [ Tmain | Tnone -> ()
                | Tname x -> tnl.val := [x :: tnl.val] ])
-            (nobtit p);
+            titles;
           tnl.val
         }
       in
-      if sou base p.public_name = "" || nobtit p = [] then titles_names
-      else [p.public_name :: titles_names]
+      if public_name = "" || titles = [] then titles_names
+      else [public_name :: titles_names]
     in
     let first_names =
       let pn =
-        if sou base p.public_name <> "" && nobtit p = [] then
-          [p.public_name :: public_names]
+        if public_name <> "" && titles = [] then [public_name :: public_names]
         else public_names
       in
-      [first_name :: List.map (sou base) (p.first_names_aliases @ pn)]
+      [first_name :: first_names_aliases @ pn]
     in
     let surnames =
-      [surname ::
-       surnames_pieces surname @
-         List.map (sou base) (p.surnames_aliases @ p.qualifiers)]
+      [surname :: surnames_pieces surname @ surnames_aliases @ qualifiers]
     in
     let surnames =
-      if p.sex = Female then
-        let u = uoi base p.key_index in
-        List.fold_left
-          (fun list ifam ->
-             let cpl = coi base ifam in
-             let husband = poi base (Adef.father cpl) in
-             let husband_surname = p_surname base husband in
-             let husband_surnames_aliases =
-               List.map (sou base) husband.surnames_aliases
-             in
-             if p_surname base husband = "?" then
-               husband_surnames_aliases @ list
-             else
-               [husband_surname ::
-                surnames_pieces husband_surname @ husband_surnames_aliases @
-                  list])
-          surnames (Array.to_list u.family)
-      else surnames
+      List.fold_left
+        (fun list (husband_surname, husband_surnames_aliases) ->
+           let husband_surname = nominative husband_surname in
+           if husband_surname = "?" then husband_surnames_aliases @ list
+           else
+             [husband_surname ::
+              surnames_pieces husband_surname @ husband_surnames_aliases @
+                list])
+        surnames husbands
     in
-    let list = [] in
-    let list =
-      List.fold_left (fun list s -> [sou base s :: list]) list public_names
-    in
+    let list = public_names in
     let list =
       List.fold_left
         (fun list f ->
@@ -86,52 +73,75 @@ value dsk_person_misc_names base p nobtit =
         list first_names
     in
     let list =
-      let first_names =
-        [first_name :: List.map (sou base) (p.first_names_aliases)]
-      in
+      let first_names = [first_name :: first_names_aliases] in
       List.fold_left
         (fun list t ->
-           let s = sou base t.t_place in
+           let s = t.t_place in
            if s = "" then list
            else
              let first_names =
                match t.t_name with
-               [ Tname f -> [sou base f :: first_names]
+               [ Tname f -> [f :: first_names]
                | Tmain | Tnone ->
-                   let f = sou base (p.public_name) in
+                   let f = public_name in
                    if f = "" then first_names else [f :: first_names] ]
              in
              List.fold_left (fun list f -> [f ^ " " ^ s :: list]) list
                first_names)
-        list (nobtit p)
+        list titles
     in
     let list =
-      match (aoi base p.key_index).parents with
-      [ Some ifam ->
-          let cpl = coi base ifam in
-          let fath = poi base (Adef.father cpl) in
-          let first_names =
-            [first_name :: List.map (sou base) (p.first_names_aliases)]
-          in
-          List.fold_left
-            (fun list t ->
-               let s = sou base t.t_place in
-               if s = "" then list
-               else
-                 List.fold_left (fun list f -> [f ^ " " ^ s :: list]) list
-                   first_names)
-            list (nobtit fath)
-      | _ -> list ]
+      if father_titles_places = [] then list
+      else
+        let first_names = [first_name :: first_names_aliases] in
+        List.fold_left
+          (fun list s ->
+             if s = "" then list
+             else
+               List.fold_left (fun list f -> [f ^ " " ^ s :: list]) list
+                 first_names)
+          list father_titles_places
     in
-    let list =
-      List.fold_left (fun list s -> [sou base s :: list]) list p.aliases
-    in
+    let list = List.rev_append aliases list in
     let fn = Name.lower (first_name ^ " " ^ surname) in
     List.fold_left
       (fun list s ->
          let s = Name.lower s in
          if s = fn || List.mem s list then list else [s :: list])
       [] list
+;
+
+value husbands base p =
+  let u = uoi base p.key_index in
+  List.map
+    (fun ifam ->
+       let cpl = coi base ifam in
+       let husband = poi base (Adef.father cpl) in
+       let husband_surname = p_surname base husband in
+       let husband_surnames_aliases =
+         List.map (sou base) husband.surnames_aliases
+       in
+       (husband_surname, husband_surnames_aliases))
+    (Array.to_list u.family)
+;
+
+value father_titles_places base p nobtit =
+  match (aoi base p.key_index).parents with
+  [ Some ifam ->
+      let cpl = coi base ifam in
+      let fath = poi base (Adef.father cpl) in
+      List.map (fun t -> sou base t.t_place) (nobtit fath)
+  | None -> [] ]
+;
+
+value dsk_person_misc_names base p nobtit =
+  let sou = sou base in
+  gen_person_misc_names (sou p.first_name) (sou p.surname) (sou p.public_name)
+    (List.map sou p.qualifiers) (List.map sou p.aliases)
+    (List.map sou p.first_names_aliases) (List.map sou p.surnames_aliases)
+    (List.map (Futil.map_title_strings sou) (nobtit p))
+    (if p.sex = Female then husbands base p else [])
+    (father_titles_places base p nobtit)
 ;
 
 value check_magic =
