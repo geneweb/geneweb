9eb3aea31b2dbc63d2a8f966e78a59d5ab82e285
ddr <ddr@56c106c9-b585-4593-932e-8dfca04823a8>
2006-11-06 13:16:17 +0000

    - Added program "check_base" checking a database like the end of "gwc" does.

---
+++ src/check_base.ml
@@ -0,0 +1,34 @@
+(* $Id: check_base.ml,v 5.1 2006-11-06 13:16:17 ddr Exp $ *)
+
+open Printf;
+
+value set_error base x = do {
+  printf "\nError: ";
+  Check.print_base_error stdout base x;
+};
+
+value set_warning base =
+  fun
+  [ Def.UndefinedSex _ -> ()
+  | x -> do {
+      printf "\nWarning: ";
+      Check.print_base_warning stdout base x;
+    } ]
+;
+
+value check_base bname = do {
+  let base = Gwdb.open_base bname in
+  let changed_p (ip, p) = do {
+    let fn = Gwdb.p_first_name base p in
+    let sn = Gwdb.p_surname base p in
+    printf "%s.%d %s not changed" fn (Gwdb.get_occ p) sn;
+  }
+  in
+  Check.check_base base (set_error base) (set_warning base)
+    (fun _ -> True) changed_p False;
+  flush stdout;
+};
+
+value main () = check_base Sys.argv.(1);
+
+main ();
