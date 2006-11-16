2b7ce21d7589ad79e93af56cf3d4e4a29ab11684
ddr <ddr@56c106c9-b585-4593-932e-8dfca04823a8>
2006-11-16 09:08:04 +0000

    - Internal: small improvements of dependencies computing.

--- tools/camlp4_depend.sh
+++ tools/camlp4_depend.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -e
-# $Id: camlp4_depend.sh,v 5.1 2006-11-01 10:48:29 ddr Exp $
+# $Id: camlp4_depend.sh,v 5.2 2006-11-16 09:08:04 ddr Exp $
 
 FILES=
 DEPARGS=
@@ -9,40 +9,40 @@ for i in $*; do
     *) DEPARGS="$DEPARGS $i";;
     esac
 done
-PR_DEP=../src/pr_dep.cmo
+PR_DEP=$TOP/src/pr_dep.cmo
 
 for FILE in $FILES; do
     head -1 $FILE >/dev/null || exit 1
-    set - `head -1 $FILE`
+    set - $(head -1 $FILE)
     if test "$2" = "camlp4r" -o "$2" = "camlp4o" -o "$2" = "camlp4"; then
         COMM=$2
         shift; shift
-        ARGS=`echo $* | sed -e "s/[()*]//g"`
+        ARGS=$(echo $* | sed -e "s/[()*]//g")
     else
         COMM=camlp4r
         ARGS=
     fi
     ARGS2="$DEPARGS"
-    echo $COMM $PR_DEP ../src/ppdef.cmo $ARGS -- $ARGS2 $FILE >&2
-    $COMM $PR_DEP ../src/ppdef.cmo $ARGS -- $ARGS2 $FILE
+    echo $COMM $PR_DEP $TOP/src/ppdef.cmo $ARGS -- $ARGS2 $FILE >&2
+    $COMM $PR_DEP $TOP/src/ppdef.cmo $ARGS -- $ARGS2 $FILE
 done
 
 for FILE in $FILES; do
     head -1 $FILE >/dev/null || exit 1
-    set - `head -1 $FILE`
+    set - $(head -1 $FILE)
     if test "$2" = "camlp4r" -o "$2" = "camlp4o" -o "$2" = "camlp4"; then
         COMM=$2
 	shift; shift
-        ARGS=`echo $* | sed -e "s/[()*]//g"`
+        ARGS=$(echo $* | sed -e "s/[()*]//g")
 	DEPS=
 	for i in $ARGS; do
-	    if test `echo $i | sed "s/^\(..\).*$/\1/"` = "./"; then
-		DEP=`echo $i | sed "s/^..\(.*\)$/\1/"`
+	    if test $(echo $i | sed "s/^\(..\).*$/\1/") = "./"; then
+		DEP=$(echo $i | sed "s/^..\(.*\)$/\1/")
 		DEPS="$DEPS $DEP"
 	    fi
 	done
         if test "$DEPS" != ""; then
-	    BASE=`basename $FILE .ml`
+	    BASE=$(basename $FILE .ml)
 	    echo $BASE.cmo $BASE.cmx: $DEPS
         fi
     fi
