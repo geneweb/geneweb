ecf075303c313c08aec078d3828a6972fe98ba55
ddr <ddr@56c106c9-b585-4593-932e-8dfca04823a8>
2006-11-29 10:16:41 +0000

    - Fixed bug: if starting with a person named e.g. "Daniel de Rauglaudre" as   *first name* and *nothing* as surname (by mistake), if edited to change   "Daniel" as first name and "de Rauglaudre" as surname, this person had   no key and it was possible to create another "Daniel.0 de Rauglaudre".

--- src/updateIndOk.ml
+++ src/updateIndOk.ml
@@ -1,5 +1,5 @@
 (* camlp4r ./pa_html.cmo *)
-(* $Id: updateIndOk.ml,v 5.37 2006-11-18 13:32:27 ddr Exp $ *)
+(* $Id: updateIndOk.ml,v 5.38 2006-11-29 10:16:41 ddr Exp $ *)
 (* Copyright (c) 1998-2006 INRIA *)
 
 open Config;
@@ -474,9 +474,7 @@ value effective_mod conf base sp =
         Name.lower (nominative sp.surname) <> Name.lower osn)
     then do {
       patch_key base pi sp.first_name sp.surname sp.occ;
-      if Name.crush_lower (ofn ^ " " ^ osn) <> Name.crush_lower key then
-        person_ht_add base key pi
-      else ();
+      person_ht_add base key pi
     }
     else ();
     check_sex_married conf base sp op;
