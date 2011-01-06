1fb5c8269b42e1f4f5f729a06d256fa736d68abd
ddr <ddr@56c106c9-b585-4593-932e-8dfca04823a8>
2011-01-06 09:59:58 +0000

    -

--- src/version.ml
+++ src/version.ml
@@ -1,7 +1,7 @@
-(* $Id: version.ml,v 5.7 2011-01-06 09:28:17 ddr Exp $ *)
+(* $Id: version.ml,v 5.8 2011-01-06 09:59:58 ddr Exp $ *)
 (* Copyright (c) 1998-2007 INRIA *)
 
-value txt = "5.02";
+value txt = "5.03-exp";
 
 value available_languages =
   ["af"; "bg"; "br"; "ca"; "cs"; "da"; "de"; "en"; "es"; "eo"; "et"; "fi";
