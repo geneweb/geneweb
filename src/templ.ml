61026cd3e90bd6e9fcb9e74373bdb56f45e27c3a
fabien <fabien@56c106c9-b585-4593-932e-8dfca04823a8>
2011-08-31 10:31:23 +0000

    suppression de la lecture rtl pour les css

--- src/templ.ml
+++ src/templ.ml
@@ -1058,11 +1058,7 @@ value print_body_prop conf =
 ;
 
 value print_css_prop conf =
-  let s =
-    try " dir=\"" ^ Hashtbl.find conf.lexicon " !dir" ^ "\"" with
-    [ Not_found -> "" ]
-  in
-  Wserver.wprint "%s" (s ^ Util.css_prop conf)
+  Wserver.wprint "%s" (Util.css_prop conf)
 ;
 
 type vother 'a =
