74e567f9b184b9e25d622f73120c487cee8d4371
fabien <fabien@56c106c9-b585-4593-932e-8dfca04823a8>
2012-10-03 10:32:02 +0000

    Dans la recherche avancée, on cherche le lieu de mariage et la date (#177)

--- src/advSearchOk.ml
+++ src/advSearchOk.ml
@@ -122,18 +122,7 @@ value advanced_search conf base max_answers =
         | _ -> False ]
     | _ -> True ]
   in
-  let test_date_fam p x =
-    let test_date df =
-      List.exists
-        (fun ifam ->
-           let fam = foi base ifam in
-           let father = poi base (get_father fam) in
-           let mother = poi base (get_mother fam) in
-           if fast_auth_age conf father && fast_auth_age conf mother then 
-             df (Adef.od_of_codate (get_marriage fam))
-           else False)
-        (Array.to_list (get_family p))
-    in
+  let test_marriage p x y =
     let (d1, d2) =
       try Hashtbl.find hd x with
       [ Not_found ->
@@ -143,9 +132,24 @@ value advanced_search conf base max_answers =
           in
           do { Hashtbl.add hd x v; v } ]
     in
+    let y = gets y in
+    let test_date_place df =
+      List.exists
+        (fun ifam ->
+           let fam = foi base ifam in
+           let father = poi base (get_father fam) in
+           let mother = poi base (get_mother fam) in
+           if fast_auth_age conf father && fast_auth_age conf mother then 
+             if y = "" then df (Adef.od_of_codate (get_marriage fam))
+             else
+               name_incl y (sou base (get_marriage_place fam)) &&
+                 df (Adef.od_of_codate (get_marriage fam))
+           else False)
+        (Array.to_list (get_family p))
+    in
     match (d1, d2) with
     [ (Some d1, Some d2) ->
-        test_date 
+        test_date_place 
           (fun
           [ Some d ->
               if CheckItem.strictly_before d d1 then False
@@ -153,32 +157,18 @@ value advanced_search conf base max_answers =
               else True
           | _ -> False ])
     | (Some d1, _) ->
-        test_date 
+        test_date_place 
           (fun
           [ Some d when fast_auth_age conf p ->
               if CheckItem.strictly_before d d1 then False else True
           | _ -> False ])
     | (_, Some d2) ->
-        test_date
+        test_date_place
           (fun
           [ Some d when fast_auth_age conf p ->
               if CheckItem.strictly_after d d2 then False else True
           | _ -> False ])
     | _ -> True ]
-  in
-  let test_auth_fam p x =
-    let y = gets x in
-    if y = "" then True
-    else
-      List.exists
-        (fun ifam ->
-           let fam = foi base ifam in
-           let father = poi base (get_father fam) in
-           let mother = poi base (get_mother fam) in
-           if fast_auth_age conf father && fast_auth_age conf mother then
-             name_incl y (sou base (get_marriage_place fam))
-           else False)
-        (Array.to_list (get_family p))
   in
   let list = ref [] in
   let len = ref 0 in
@@ -216,7 +206,7 @@ value advanced_search conf base max_answers =
           [ "Y" -> get_family p <> [| |]
           | "N" -> get_family p = [| |]
           | _ -> True ]) &&
-       test_date_fam p "marriage" &&
+       test_marriage p "marriage" "marriage_place" &&
        test_auth p "birth_place"
          (fun x -> name_incl x (sou base (get_birth_place p))) &&
        test_auth p "bapt_place"
@@ -226,8 +216,7 @@ value advanced_search conf base max_answers =
        test_auth p "burial_place"
          (fun x -> name_incl x (sou base (get_burial_place p))) &&
        test_auth p "occu" 
-         (fun x -> name_incl x (sou base (get_occupation p))) &&
-       test_auth_fam p "marriage_place"
+         (fun x -> name_incl x (sou base (get_occupation p)))
     then do {
       list.val := [p :: list.val]; incr len;
     }
