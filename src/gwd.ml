70c7397262118d8943fd8b079b5e60aca050b462
fabien <fabien@56c106c9-b585-4593-932e-8dfca04823a8>
2012-12-18 20:25:03 +0000

    Add the option no_note_for_visitor (same as no_image_for_visitor) (#134)

--- src/gwd.ml
+++ src/gwd.ml
@@ -1301,6 +1301,11 @@ value make_conf cgi from_addr (addr, request) script_name contents env = do {
        else
          try List.assoc "no_image_for_visitor" base_env = "yes" with
          [ Not_found -> False ];
+     no_note =
+       if ar.ar_wizard || ar.ar_friend then False
+       else
+         try List.assoc "no_note_for_visitor" base_env = "yes" with
+         [ Not_found -> False ];
      bname = base_file; env = env; senv = [];
      henv =
        (if not cgi then []
