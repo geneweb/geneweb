da5b13f092bdde7b94c793aefe456ccc6fadf670
ipfix <fabien.geneweb@gmail.com>
2014-07-01 22:24:35 +0200

    Fix #249: change parameter from v to n in the search function

--- src/request.ml
+++ src/request.ml
@@ -537,7 +537,7 @@ value family_m conf base =
             match p_getenv conf.env "t" with
             [ Some "P" -> [("fn", n) :: conf.env]
             | Some "N" -> [("sn", n) :: conf.env]
-            | _ -> [("v", n) :: conf.env] ]
+            | _ -> [("n", n) :: conf.env] ]
         | None -> conf.env ]
       in
       let conf = {(conf) with env = env} in
@@ -569,7 +569,7 @@ value family_m conf base =
                 else specify conf base n pl
             | pl -> specify conf base n pl ]
           in
-          match real_input "v" with
+          match real_input "n" with
           [ Some n -> search n
           | None ->
               match (real_input "fn", real_input "sn") with
