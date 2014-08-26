8db39fc9d4efbcb447215ae4249a5a71987d12cd
ipfix <fabien.geneweb@gmail.com>
2014-08-26 16:57:51 +0200

    Add a function to find the sosa (debuter son arbre)

--- api/api_request.ml
+++ api/api_request.ml
@@ -80,6 +80,7 @@ value family_m conf base =
     | Some "API_GRAPH_ASC" -> Api_graph.print_graph_asc conf base
     | Some "API_GRAPH_DESC" -> Api_graph.print_graph_desc conf base
     | Some "API_GRAPH_REL" -> Api_graph.print_graph_rel conf base
+    | Some "API_FIND_SOSA" -> Api.print_find_sosa conf base
     | Some "API_INFO_BASE" -> Api.print_info_base conf base
     | Some "API_INFO_IND" -> Api.print_info_ind conf base
     | Some "API_IMAGE" -> Api.print_img conf base
