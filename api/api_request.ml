33a4eadbf1d0ce646fd5c2d5324cdcb6902a0b35
ipfix <fabien.geneweb@gmail.com>
2014-09-30 16:05:33 +0200

    Add function to find the person with the more ancestors

--- api/api_request.ml
+++ api/api_request.ml
@@ -89,6 +89,7 @@ value family_m conf base =
   | Some "API_IMAGE_UPDATE" when conf.wizard -> Api.print_updt_image conf base
   | Some "API_LIST_PERSONS" -> Api.print_list_ref_person conf base
   | Some "API_LOOP_BASE" -> Api.print_loop conf base
+  | Some "API_MAX_ANCESTORS" when conf.wizard -> Api.print_max_ancestors conf base
   | Some "API_NOTIFICATION_BIRTHDAY" -> Api.print_notification_birthday conf base
   | Some "API_PRINT_INDEX" -> Api.print_all_full_person conf base
   | Some "API_PRINT_EXPORT" -> Api.print_export conf base
