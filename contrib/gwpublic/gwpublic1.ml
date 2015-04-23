f08d125f7f0f851db8aca2c4c7dfcda650034b22
ipfix <fabien.geneweb@gmail.com>
2015-04-23 12:36:32 +0200

    Fix 217c4f3: of course it's the contrary.

--- contrib/gwpublic/gwpublic1.ml
+++ contrib/gwpublic/gwpublic1.ml
@@ -79,13 +79,13 @@ value mark_descendants base scanned old lim_year =
   else ()
 ;
 
-value mark_ancestors base scanned lim_year check_titled is_quest_string =
+value mark_ancestors base scanned lim_year titled is_quest_string =
   loop where rec loop p =
     if not scanned.(Adef.int_of_iper (get_key_index p)) then do {
       scanned.(Adef.int_of_iper (get_key_index p)) := True;
       (* si pas de date ou date > lim_year *)
       if not (is_old lim_year p) && get_access p <> Public &&
-         (check_titled && get_titles p = []) &&
+         (titled || get_titles p = []) &&
          not (is_quest_string (get_first_name p)) &&
          not (is_quest_string (get_surname p))
       then do {
@@ -128,7 +128,7 @@ value public_everybody bname =
   }
 ;
 
-value public_all bname lim_year check_titled = do {
+value public_all bname lim_year titled = do {
   let base = Gwdb.open_base bname in
   let () = load_ascends_array base in
   let () = load_couples_array base in
@@ -155,7 +155,7 @@ value public_all bname lim_year check_titled = do {
     for i = 0 to nb_of_persons base - 1 do {
       if old.(i) && not scanned.(i) then do {
         let p = poi base (Adef.iper_of_int i) in
-        mark_ancestors base scanned lim_year check_titled is_quest_string p
+        mark_ancestors base scanned lim_year titled is_quest_string p
       }
       else ();
     };
@@ -163,7 +163,7 @@ value public_all bname lim_year check_titled = do {
   }
 };
 
-value public_some bname lim_year check_titled key =
+value public_some bname lim_year titled key =
   let base = Gwdb.open_base bname in
   match Gutil.person_ht_find_all base key with
   [ [ip] ->
@@ -172,7 +172,7 @@ value public_some bname lim_year check_titled key =
       let () = load_ascends_array base in
       let () = load_couples_array base in
       do {
-        mark_ancestors base scanned lim_year check_titled is_quest_string p;
+        mark_ancestors base scanned lim_year titled is_quest_string p;
         if changes.val then commit_patches base else ();
       }
   | _ ->
@@ -187,12 +187,12 @@ value lim_year = ref 1900;
 value ind = ref "";
 value bname = ref "";
 value everybody = ref False;
-value check_titled = ref False;
+value titled = ref True;
 
 value speclist =
   [("-y", Arg.Int (fun i -> lim_year.val := i),
     "limit year (default = " ^ string_of_int lim_year.val ^ ")");
-   ("-ct", Arg.Set check_titled,
+   ("-ct", Arg.Clear titled,
     "check if the person has a title (default = don't check)");
    ("-everybody", Arg.Set everybody, "set flag public to everybody");
    ("-ind", Arg.String (fun x -> ind.val := x),
