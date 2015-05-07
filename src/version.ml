b1606cf6a4f4de5461a7aa6d4160138f8798e543
ipfix <fabien.geneweb@gmail.com>
2015-05-07 11:42:44 +0200

    Version 6.08

--- src/version.ml
+++ src/version.ml
@@ -1,7 +1,7 @@
 (* $Id: version.ml,v 5.8 2011-01-06 09:59:58 ddr Exp $ *)
 (* Copyright (c) 1998-2007 INRIA *)
 
-value txt = "6.08 - exp";
+value txt = "6.08";
 
 value available_languages =
   ["af"; "bg"; "br"; "ca"; "cs"; "da"; "de"; "en"; "es"; "eo"; "et"; "fi";
