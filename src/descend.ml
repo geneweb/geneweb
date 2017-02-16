0f6d646195071f6822b635309d5dd5e1b023e5a5
Henri83 <henri.gouraud@laposte.net>
2017-02-16 10:45:22 +0100

    Corrections on destable.txt; 2 display modes I/H

--- src/descend.ml
+++ src/descend.ml
@@ -1470,7 +1470,7 @@ value print conf base p =
   let templ =
     match p_getenv conf.env "t" with
     [ Some ("F" | "L" | "M") -> "deslist"
-    | Some ("H") -> "destable"
+    | Some ("H" | "I" ) -> "destable"
     | Some _ -> ""
     | _ -> "desmenu" ]
   in
