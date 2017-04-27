7e0cabfe6e48ad6784253bc46deaf6142bb6fb47
Martin POTIER <martin.potier@geneanet.org>
2017-04-27 10:12:00 +0200

    Add gwpublic command to make anybody public as gwprivate do
    Add gwaccess to refactor the code in gwprivate

--- contrib/gwpublic/gwpublic.ml
+++ contrib/gwpublic/gwpublic.ml
@@ -14,7 +14,7 @@ value speclist =
    ("-list-ind", Arg.String (fun s -> list_ind.val := s), "<file> file to the list of persons")]
 ;
 value anonfun i = bname.val := i;
-value usage = "Usage: private [-everybody] [-ind key] [-list-ind file] base";
+value usage = "Usage: public [-everybody] [-ind key] [-list-ind file] base";
 
 value main () =
   do {
@@ -23,9 +23,9 @@ value main () =
     let gcc = Gc.get () in
     gcc.Gc.max_overhead := 100;
     Gc.set gcc;
-    if everybody.val then Gwaccess.access_everybody Private bname.val
-    else if list_ind.val = "" then Gwaccess.access_some Private bname.val ind.val
-    else Gwaccess.access_some_list Private bname.val list_ind.val
+    if everybody.val then Gwaccess.access_everybody Public bname.val
+    else if list_ind.val = "" then Gwaccess.access_some Public bname.val ind.val
+    else Gwaccess.access_some_list Public bname.val list_ind.val
   }
 ;
 
