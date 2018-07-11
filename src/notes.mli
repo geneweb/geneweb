725f52d8fb21801f3976d76533f227c78bb3cf08
fablhx <ipfix.github@gmail.com>
2018-07-11 14:09:49 +0200

    Remove 'open NotesLinks'

--- src/notes.mli
+++ src/notes.mli
@@ -3,18 +3,18 @@
 
 open Config;
 open Gwdb;
-open NotesLinks;
 
 value file_path : config -> base -> string -> string;
 value read_notes : base -> string -> (list (string * string) * string);
 
 value print : config -> base -> unit;
 value print_mod : config -> base -> unit;
-value update_notes_links_db : config -> page -> string -> unit;
+value update_notes_links_db : config -> NotesLinks.page -> string -> unit;
 value print_mod_ok : config -> base -> unit;
 
 value print_misc_notes : config -> base -> unit;
 value print_misc_notes_search : config -> base -> unit;
-value print_linked_list : config -> base -> list page -> unit;
+value print_linked_list : config -> base -> list NotesLinks.page -> unit;
 
-value merge_possible_aliases : config -> notes_links_db -> notes_links_db;
+value merge_possible_aliases :
+  config -> NotesLinks.notes_links_db -> NotesLinks.notes_links_db;
