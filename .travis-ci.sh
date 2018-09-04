fca76ac5d560a2cb32c1cbda1067bfedb53c5b0e
Julien Sagot <julien.sagot@geneanet.org>
2018-09-04 15:04:55 +0200

    Travis: allow osx to fail on 4.07 (until problem with camlp4 is fixed) and include .travis-ci.sh in .travis.yml.

--- .travis-ci.sh
+++
@@ -1,8 +0,0 @@
-set -uex
-export OPAMYES=1
-eval $(opam config env)
-opam pin add geneweb . --no-action
-opam install geneweb --deps-only
-./configure && make install uninstall clean
-./configure --api && make install uninstall clean
-opam depext lablgtk && opam install lablgtk && make everything-exe
