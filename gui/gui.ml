cf64e0e8fafaf00032dbdf91f82e6bfd68578116
GuillaumeBrochu <guillaume_brochu@yahoo.ca>
2018-09-15 12:59:25 -0400

    gui.ml : Fix compilation problem with OCAML >=4.06.0 (geneweb 6)
    Following a change in Lablgtk-2.18.6:
    http://lablgtk.forge.ocamlcore.org/changes2.txt
    http://git.ocamlcore.org/cgi-bin/gitweb.cgi?p=lablgtk/lablgtk.git;a=commit;h=c37e2ef1ad4cc08958f9a49357923de0f21ec93c
    
    To compile gui, you now either need:
    - ocaml >=4.06
    - or to switch to -unsafe-string in tools/Makefile.inc.* for ocaml < 4.06
    
    Corresponds to : 8c98a5534c2939562cb24bbafc50148d94f174d1

--- gui/gui.ml
+++ gui/gui.ml
@@ -90,9 +90,9 @@ value channel_redirector channel callback = do {
     do { fun cond ->
       try
         if List.mem `IN cond then do {
-	        (* On Windows, you must use Io.read *)
-	        let len = GMain.Io.read chan ~buf ~pos:0 ~len in
-	        len >= 1 && (callback (String.sub buf 0 len))
+          (* On Windows, you must use Io.read *)
+          let len = GMain.Io.read chan ~buf ~pos:0 ~len in
+          len >= 1 && (callback (Bytes.sub buf 0 len))
         }
         else False
       with [ _ -> False ]
@@ -130,7 +130,8 @@ value exec_wait conf prog args =
     let redirect channel =
       let buffer = GText.buffer () in
       let _text = GText.view ~buffer ~editable:False ~packing:vvbox#add () in
-      channel_redirector channel (fun c -> do {buffer#insert c; True})
+      channel_redirector channel
+        (fun c -> do {buffer#insert (Bytes.to_string c); True})
     in
     ignore (redirect Unix.stderr);
     let pid = exec prog args Unix.stdout Unix.stderr in
