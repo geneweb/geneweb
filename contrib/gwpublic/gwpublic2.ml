3b22a75306aa82fdd6d7a232e865c1aa0fb8f75a
Julien Sagot <julien.sagot@geneanet.org>
2018-09-17 18:03:02 +0200

    Fixed contrib/gwpublic/gwpublic1.

--- contrib/gwpublic/gwpublic2.ml
+++ contrib/gwpublic/gwpublic2.ml
@@ -152,6 +152,7 @@ let usage = "Usage: public [-y #] [-t] base"
 let main () =
   Arg.parse speclist anonfun usage;
   if !bname = "" then begin Arg.usage speclist usage; exit 2 end;
+  Secure.set_base_dir (Filename.dirname !bname) ;
   Lock.control_retry
     (Mutil.lock_file !bname)
     ~onerror:Lock.print_error_and_exit
