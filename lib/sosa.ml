fe78bd7e1bc2ecd775ab31f7920668c5e245c82a
Benoit LECLUSE <benoit.lecluse@geneanet.org>
2019-01-22 09:36:33 +0100

    Sosa are now treated as int

--- lib/sosa.ml
+++ lib/sosa.ml
@@ -1,146 +1,35 @@
 (* Copyright (c) 1998-2007 INRIA *)
 
-type t = int array
+type t = int
 
-let base = 0x1000000
-let max_mul_base = max_int / base
-
-let zero = [| |]
-let one = [| 1 |]
+let zero = 0
+let one = 1
 let eq x y = x = y
-let gt x y =
-  if Array.length x > Array.length y then true
-  else if Array.length x < Array.length y then false
-  else
-    let rec loop i =
-      if i < 0 then false
-      else if x.(i) > y.(i) then true
-      else if x.(i) < y.(i) then false
-      else loop (i - 1)
-    in
-    loop (Array.length x - 1)
-let twice x =
-  let l =
-    let rec loop i r =
-      if i = Array.length x then if r = 0 then [] else [r]
-      else
-        let v = x.(i) lsl 1 + r in
-        v land (base - 1) :: loop (i + 1) (if v >= base then 1 else 0)
-    in
-    loop 0 0
-  in
-  Array.of_list l
-let half x =
-  let l =
-    let rec loop i r v =
-      if i < 0 then v
-      else
-        let rd = if x.(i) land 1 = 0 then 0 else base / 2 in
-        let v =
-          let d = r + x.(i) / 2 in if d = 0 && v = [] then v else d :: v
-        in
-        loop (i - 1) rd v
-    in
-    loop (Array.length x - 1) 0 []
-  in
-  Array.of_list l
-let even x = if Array.length x = 0 then true else x.(0) land 1 = 0
-let inc x n =
-  let l =
-    let rec loop i r =
-      if i = Array.length x then if r = 0 then [] else [r]
-      else let d = x.(i) + r in d mod base :: loop (i + 1) (d / base)
-    in
-    loop 0 n
-  in
-  Array.of_list l
-let add x y =
-  let l =
-    let rec loop i r =
-      if i >= Array.length x && i >= Array.length y then
-        if r = 0 then [] else [r]
-      else
-        let (d, r) =
-          let xi = if i >= Array.length x then 0 else x.(i) in
-          let yi = if i >= Array.length y then 0 else y.(i) in
-          let s = xi + yi + r in s mod base, s / base
-        in
-        d :: loop (i + 1) r
-    in
-    loop 0 0
-  in
-  Array.of_list l
-let normalize =
-  let rec loop =
-    function
-      [] -> []
-    | x :: l -> let r = loop l in if x = 0 && r = [] then r else x :: r
-  in
-  loop
-let sub x y =
-  let l =
-    let rec loop i r =
-      if i >= Array.length x && i >= Array.length y then
-        if r = 0 then [] else invalid_arg "Sosa.sub"
-      else
-        let (d, r) =
-          let xi = if i >= Array.length x then 0 else x.(i) in
-          let yi = if i >= Array.length y then 0 else y.(i) in
-          if yi + r <= xi then xi - (yi + r), 0 else base + xi - (yi + r), 1
-        in
-        d :: loop (i + 1) r
-    in
-    loop 0 0
-  in
-  Array.of_list (normalize l)
-let mul0 x n =
-  if n > max_mul_base then invalid_arg "Sosa.mul"
-  else
-    let l =
-      let rec loop i r =
-        if i = Array.length x then if r = 0 then [] else [r]
-        else let d = x.(i) * n + r in d mod base :: loop (i + 1) (d / base)
-      in
-      loop 0 0
-    in
-    Array.of_list l
-let mul x n =
-  if n < max_mul_base then mul0 x n
-  else
-    let rec loop r x n =
-      if n < max_mul_base then add r (mul0 x n)
-      else
-        loop (add r (mul0 x (n mod max_mul_base))) (mul0 x max_mul_base)
-          (n / max_mul_base)
-    in
-    loop zero x n
-let div x n =
-  if n > max_mul_base then invalid_arg "Sosa.div"
-  else
-    let l =
-      let rec loop i l r =
-        if i < 0 then l
-        else
-          let r = r mod n * base + x.(i) in
-          let d = r / n in loop (i - 1) (d :: l) r
-      in
-      loop (Array.length x - 1) [] 0
-    in
-    Array.of_list (normalize l)
-let modl x n =
-  let r = sub x (mul0 (div x n) n) in if Array.length r = 0 then 0 else r.(0)
+let gt x y = x > y
+let half x = x / 2
+let add x y = x - y
+let sub x y = x - y
+let mul x n = x * n
+let div x n = x / n
+let modl x n = x mod n
 
 let of_int i =
   if i < 0 then invalid_arg "Sosa.of_int"
   else if i = 0 then zero
-  else if i < base then [| i |]
-  else [| i mod base; i / base |]
+  else if i = 1 then one
+  else i
+
+let inc sosa increment = sosa + increment
+
+let even sosa =
+  sosa mod 2 = 0
+
+let twice sosa =
+  sosa * 2
 
 let to_int = function
-  | [||] -> 0
-  | [| i |] -> i
-  | [| m ; d |] -> d * base + m
-  | _ -> assert false
+  | 0 -> 0
+  | i -> i
 
 let rec exp_gen x1 x2 n =
   if n = 0 || x1 = zero then one
@@ -193,21 +82,13 @@ let to_string_sep_base sep base x =
   Bytes.unsafe_to_string s
 
 let to_string_sep sep = to_string_sep_base sep 10
+
 let to_string = to_string_sep_base "" 10
 
-let of_string s =
-  let rec loop n i =
-    if i = String.length s then n
-    else
-      match s.[i] with
-        '0'..'9' ->
-          loop (inc (mul0 n 10) (Char.code s.[i] - Char.code '0')) (i + 1)
-      | _ -> failwith "Sosa.of_string"
-  in
-  loop zero 0
+let of_string s = int_of_string s
 
 let gen x =
-  let s = to_string_sep_base "" 2 x in
+  let s =  to_string_sep_base "" 2 x in
   String.length s (* coherent with %sosa.lvl *)
 
 let branch x =
