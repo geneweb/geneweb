163052dc1e02e033fa587012842c5ddfcd7a8b73
Julien Sagot <julien.sagot@geneanet.org>
2019-04-11 20:17:17 +0200

    Used Zarith lib for Sosa module.

--- lib/sosa.ml
+++ lib/sosa.ml
@@ -1,42 +1,59 @@
 (* Copyright (c) 1998-2007 INRIA *)
 
-type t = int
+type t = Z.t
 
-let zero = 0
-let one = 1
-let eq x y = x = y
-let gt x y = x > y
-let half x = x / 2
-let add x y = x + y
-let sub x y = x - y
-let mul x n = x * n
-let div x n = x / n
-let modl x n = x mod n
+let of_int i = if i < 0 then invalid_arg "Sosa.of_int" else Z.of_int i
 
-let of_int i = if i < 0 then invalid_arg "Sosa.of_int" else i
-let inc sosa increment = sosa + increment
-let even sosa = sosa mod 2 = 0
-let twice sosa = sosa * 2
-let to_int x = x
+let zero = Z.zero
+let one = Z.one
+let eq = Z.equal
+let gt = Z.gt
+let half = let two = Z.succ one in fun x -> Z.div x two
 
-let rec exp_gen x1 x2 n =
-  if n = 0 || x1 = zero then one
-  else if n = 1 then x1
-  else exp_gen (mul x1 (to_int x2)) x2 (n-1)
+let add = Z.add
+let sub = Z.sub
+let mul x y = Z.mul x (Z.of_int y)
+let div x y = Z.div x (Z.of_int y)
+let modl x y = Z.rem x (Z.of_int y)
+let exp = Z.pow
 
-let exp x n =
-  exp_gen x x n
+let inc sosa increment = Z.add sosa (of_int increment)
+let even = Z.is_even
+let twice = let two = Z.succ Z.one in fun sosa -> Z.mul sosa two
 
-let to_string = string_of_int
+(* Mutil.string_of_int_sep adapted to type [t] *)
+let to_string_sep =
+  let ten = Z.of_int 10 in
+  fun sep x ->
+  let digits, len =
+    let rec loop (d, l) x =
+      if x = Z.zero then (d, l)
+      else loop (Char.chr (Char.code '0' + Z.to_int (Z.rem x ten)) :: d, l + 1) (Z.div x ten)
+    in
+    loop ([], 0) x
+  in
+  let digits, len = if digits = [] then ['0'], 1 else digits, len in
+  let slen = String.length sep in
+  let s = Bytes.create (len + (len - 1) / 3 * slen) in
+  let _ =
+    List.fold_left
+      (fun (i, j) c ->
+         Bytes.set s j c ;
+         if i < len - 1 && (len - 1 - i) mod 3 = 0 then
+           begin String.blit sep 0 s (j + 1) slen; i + 1, j + 1 + slen end
+         else i + 1, j + 1)
+      (0, 0) digits
+  in
+  Bytes.unsafe_to_string s
 
-let to_string_sep = Mutil.string_of_int_sep
+let to_string = Z.to_string
 
-let of_string = int_of_string
+let of_string = Z.of_string
 
-let gen x = int_of_float (log (float_of_int x) /. log 2.) + 1
+let gen x = Z.log2 x + 1
 
 let branches x =
   let rec aux acc d =
-    if d = 0 then acc else aux (d land 1 :: acc) (d lsr 1)
+    if d = Z.zero then acc else aux (Z.to_int (Z.logand d one) :: acc) (Z.shift_right d 1)
   in
   List.tl (aux [] x)
