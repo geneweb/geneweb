657d311da9af58ffb502baaa0125250c6dcb20a1
Julien Sagot <julien.sagot@geneanet.org>
2019-04-30 14:11:07 +0200

    Benchmark: added case for Place.normalize function

--- benchmark/bench.ml
+++ benchmark/bench.ml
@@ -1,5 +1,5 @@
 let bench name n fn arg =
-  ignore @@ Benchmark.latency1 ~name n fn arg
+  ignore @@ Benchmark.latency1 ~name n (List.map fn) arg
 
 let list =
   [1;2;10;100;1000;10000;100000;1000000;10000000;100000000;1000000000]
@@ -8,8 +8,10 @@ let sosa_list =
   List.map Sosa.of_int list
 
 let () =
-  bench "Sosa.gen" 1000000L (List.map Sosa.gen) sosa_list
-; bench "Sosa.to_string_sep" 1000000L (List.map @@ Sosa.to_string_sep ",") sosa_list
-; bench "Sosa.to_string" 1000000L (List.map Sosa.to_string) sosa_list
-; bench "Sosa.of_string" 1000000L (List.map Sosa.of_string) (List.map string_of_int list)
-; bench "Sosa.branches" 1000000L (List.map Sosa.branches) sosa_list
+  bench "Sosa.gen" 1000000L (List.map Sosa.gen) [ sosa_list ]
+; bench "Sosa.to_string_sep" 1000000L (List.map @@ Sosa.to_string_sep ",") [ sosa_list ]
+; bench "Sosa.to_string" 1000000L (List.map Sosa.to_string) [ sosa_list ]
+; bench "Sosa.of_string" 1000000L (List.map Sosa.of_string) [ List.map string_of_int list ]
+; bench "Sosa.branches" 1000000L (List.map Sosa.branches) [ sosa_list ]
+; bench "Place.normalize" 10000000L Geneweb.Place.normalize
+    [ "[foo-bar] - boobar (baz)" ; "[foo-bar] – boobar (baz)" ; "[foo-bar] — boobar (baz)" ]
