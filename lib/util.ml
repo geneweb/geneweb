04bfbde7e61e2af414821c8f443cd4718c31f929
Ludovic Ledieu <lledieu@free.fr>
2019-06-10 19:04:01 +0200

    Rework surname_by_branch / m=N;v=
    This page wasn't compliant with HTML5.
    - dl/dt/dd only used on first level
    - ul/li + CSS for others
    - added class posterity to be able to inline "..."
    
    Signed-off-by: Ludovic Ledieu <lledieu@free.fr>

--- lib/util.ml
+++ lib/util.ml
@@ -909,14 +909,18 @@ let geneweb_link conf href s =
 let wprint_geneweb_link conf href s =
   Wserver.printf "%s" (geneweb_link conf href s)
 
-let reference conf base p s =
+let reference_flags with_id conf base p s =
   let iper = get_iper p in
   if conf.cancel_links || is_hidden p then s
   else
     String.concat ""
-      ["<a href=\""; commd conf; acces conf base p; "\" id=\"i";
-       string_of_iper iper; "\">"; s; "</a>"]
+      ["<a href=\""; commd conf; acces conf base p;
+       if with_id then "\" id=\"i" else "";
+       string_of_int iper; "\">"; s; "</a>"]
 
+let reference = reference_flags true
+
+let reference_noid = reference_flags false
 
 (* ************************************************************************* *)
 (*  [Fonc] update_family_loop : config -> base -> person -> string -> string *)
