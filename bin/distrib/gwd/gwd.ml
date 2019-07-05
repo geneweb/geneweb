3e79e8f0317d254a39d1ce07605a295601112db8
Julien Sagot <julien.sagot@geneanet.org>
2019-07-05 08:33:55 +0200

    Split wserver lib into wserver and wserver.mli

--- bin/distrib/gwd/gwd.ml
+++ bin/distrib/gwd/gwd.ml
@@ -1,7 +1,7 @@
 open Geneweb
 
 module Request = Request.Make (struct let handler = RequestHandler.defaultHandler end)
-module Main = GwDaemon.Make (Wserver) (Request)
+module Main = GwDaemon.Make (Request)
 
 let _ =
   Main.run ()
