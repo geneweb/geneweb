92cf0f1388ca6bf140c8315821b3f564ddd0d99a
Julien Sagot <julien.sagot@geneanet.org>
2019-07-17 15:01:19 +0200

    Added benchmark for UpdateData.get_all_data.

--- benchmark/bench.ml
+++ benchmark/bench.ml
@@ -1,3 +1,5 @@
+open Geneweb
+
 let bench name n fn arg =
   ignore @@ Benchmark.latency1 ~name n (List.map fn) arg
 
@@ -19,3 +21,38 @@ let () =
     [ "aaaaaaaaaa" ; "bbbbbbbbbb" ; "abbbbbbbb" ; "bbbbbbbbba" ; "ababababab" ]
 ; bench "Mutil.tr" 100000000L (fun s -> Mutil.tr 'a' 'b' @@ "a" ^ s)
     [ "aaaaaaaaaa" ; "bbbbbbbbbb" ; "abbbbbbbb" ; "bbbbbbbbba" ; "ababababab" ]
+
+; begin match Sys.getenv "BENCH_BASE" with
+  | exception Not_found -> ()
+  | bname when bname <> "" ->
+    let conf =
+      {Config.from="";manitou=false;supervisor=false;wizard=false;
+       is_printed_by_template=false;friend=false;
+       just_friend_wizard=false;user="";username="";
+       auth_scheme=Config.NoAuth;pure_xhtml=false;command="";
+       indep_command="";highlight="";lang="";default_lang="";
+       default_sosa_ref=Gwdb.dummy_iper, None;multi_parents=false;
+       can_send_image=false;authorized_wizards_notes=false;
+       public_if_titles=false;public_if_no_date=false;
+       cancel_links=false;setup_link=false;access_by_key=false;
+       private_years=0;hide_names=false;use_restrict=false;
+       no_image=false;no_note=false;bname="";cgi_passwd="";
+       env=[];senv=[];henv=[];base_env=[];allowed_titles=lazy[];
+       denied_titles=lazy[];xhs="";request=[];
+       lexicon=Hashtbl.create 16;charset="";is_rtl=false;left="";
+       right="";auth_file="";border=0;n_connect=None;
+       today={Def.day=0;month=0;year=0;delta=0;prec=Def.Sure};
+       today_wd=0;time=0,0,0;ctime=0.;image_prefix="";
+       b_arg_for_basename=false}
+    in
+    bench
+      "UpdateData.get_all_data" 500L
+      begin fun conf ->
+            let base = Gwdb.open_base bname in
+            let data = UpdateData.get_all_data conf base in
+            Gwdb.close_base base ;
+            data
+      end
+      [ { conf with Config.env = ["data","place"] } ]
+  | _ -> ()
+end
