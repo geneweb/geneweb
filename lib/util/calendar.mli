63b987d88961ce8764efee1817ae91c9c181e924
Julien Sagot <julien.sagot@geneanet.org>
2019-10-31 07:59:29 +0100

    Exported Calendar module as external lib

--- lib/util/calendar.mli
+++ lib/util/calendar.mli
@@ -1,23 +1,19 @@
-(* $Id: calendar.mli,v 5.0 2005-12-13 11:51:27 ddr Exp $ *)
+val gregorian_of_sdn : Def.precision -> int -> Def.dmy
+val julian_of_sdn : Def.precision -> int -> Def.dmy
+val french_of_sdn : Def.precision -> int -> Def.dmy
+val hebrew_of_sdn : Def.precision -> int -> Def.dmy
 
-open Def
+val sdn_of_gregorian : Def.dmy -> int
+val sdn_of_julian : Def.dmy -> int
+val sdn_of_french : Def.dmy -> int
+val sdn_of_hebrew : Def.dmy -> int
 
-val gregorian_of_sdn : precision -> int -> dmy
-val julian_of_sdn : precision -> int -> dmy
-val french_of_sdn : precision -> int -> dmy
-val hebrew_of_sdn : precision -> int -> dmy
-
-val sdn_of_gregorian : dmy -> int
-val sdn_of_julian : dmy -> int
-val sdn_of_french : dmy -> int
-val sdn_of_hebrew : dmy -> int
-
-val gregorian_of_julian : dmy -> dmy
-val julian_of_gregorian : dmy -> dmy
-val gregorian_of_french : dmy -> dmy
-val french_of_gregorian : dmy -> dmy
-val gregorian_of_hebrew : dmy -> dmy
-val hebrew_of_gregorian : dmy -> dmy
+val gregorian_of_julian : Def.dmy -> Def.dmy
+val julian_of_gregorian : Def.dmy -> Def.dmy
+val gregorian_of_french : Def.dmy -> Def.dmy
+val french_of_gregorian : Def.dmy -> Def.dmy
+val gregorian_of_hebrew : Def.dmy -> Def.dmy
+val hebrew_of_gregorian : Def.dmy -> Def.dmy
 
 type moon_phase = NewMoon | FirstQuarter | FullMoon | LastQuarter
 
