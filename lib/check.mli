8d6f9aa8c66c664cd42289ac86b36cca886a8a2d
Julien Sagot <julien.sagot@geneanet.org>
2019-12-02 15:16:02 +0100

    Check.check_base: added ?verbose parameter

--- lib/check.mli
+++ lib/check.mli
@@ -8,7 +8,8 @@ val print_base_error : out_channel -> base -> CheckItem.base_error -> unit
 val print_base_warning : out_channel -> base -> CheckItem.base_warning -> unit
 
 val check_base
-  : base
+  : ?verbose:bool
+  -> base
   -> (CheckItem.base_error -> unit)
   -> (CheckItem.base_warning -> unit)
   -> (iper * person * Def.sex option * relation list option -> unit)
