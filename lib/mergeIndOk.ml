geneweb/geneweb

commit 2e19605c5f10fd769aef2db59af3ddeec107cfd0

Fixed merge_ind operations order (witness informations were lost)

lib/mergeIndOk.ml
@@ -429,14 +429,14 @@ let effective_mod_merge o_conf base o_p1 o_p2 sp print_mod_merge_ok =
       let p2_family = get_family p2 in
       let warning _ = () in
       MergeInd.reparent_ind base warning sp.key_index ip2;
+      let p = UpdateIndOk.effective_mod conf base sp in
+      let p = redirect_relations_of_added_related base p ip2 rel_chil in
+      redirect_added_families base p ip2 p2_family;
       let warning _ = () in
       let p2 = UpdateIndOk.effective_del base warning p2 in
       patch_person base p2.key_index p2;
       let u2 = {family = [| |]} in
       patch_union base p2.key_index u2;
-      let p = UpdateIndOk.effective_mod conf base sp in
-      let p = redirect_relations_of_added_related base p ip2 rel_chil in
-      redirect_added_families base p ip2 p2_family;
       patch_person base p.key_index p;
       patch_cache_info conf Util.cache_nb_base_persons
         (fun v -> let v = int_of_string v - 1 in string_of_int v);

commit 6468da68a212df89dcc6fc0c7c297cbfc6216a56

mergeIndOk: fixed a bug introduced in 2e19605c5f10fd769aef2db59af3ddeec107cfd0

lib/mergeIndOk.ml
@@ -429,7 +429,7 @@ let effective_mod_merge o_conf base o_p1 o_p2 sp print_mod_merge_ok =
       let p2_family = get_family p2 in
       let warning _ = () in
       MergeInd.reparent_ind base warning sp.key_index ip2;
-      let p = UpdateIndOk.effective_mod conf base sp in
+      let p = UpdateIndOk.effective_mod ~skip_conflict:ip2 conf base sp in
       let p = redirect_relations_of_added_related base p ip2 rel_chil in
       redirect_added_families base p ip2 p2_family;
       let warning _ = () in

lib/updateIndOk.ml
@@ -928,7 +928,7 @@ let pwitnesses_of pevents =
     [] pevents
 
 (* sp.death *)
-let effective_mod conf base sp =
+let effective_mod ?skip_conflict conf base sp =
   let pi = sp.key_index in
   let op = poi base pi in
   let key = sp.first_name ^ " " ^ sp.surname in
@@ -937,8 +937,13 @@ let effective_mod conf base sp =
   let oocc = get_occ op in
   if ofn = sp.first_name && osn = sp.surname && oocc = sp.occ then ()
   else
-    begin let ipl = Gutil.person_ht_find_all base key in
-      check_conflict conf base sp ipl; rename_image_file conf base op sp
+    begin
+      let ipl = match skip_conflict with
+        | Some i -> List.filter ((<>) i) @@ Gutil.person_ht_find_all base key
+        | None -> Gutil.person_ht_find_all base key
+      in
+      check_conflict conf base sp ipl ;
+      rename_image_file conf base op sp
     end;
   (* Si on modifie la personne pour lui ajouter un nom/prénom, alors *)
   (* il faut remettre le compteur du nombre de personne à jour.      *)

lib/updateIndOk.mli
@@ -7,8 +7,14 @@ open Gwdb
 val effective_del :
   base -> (CheckItem.base_warning -> unit) -> person ->
     (iper, iper, istr) gen_person
-val effective_mod :
-  config -> base -> (iper, Update.key, string) gen_person -> (iper, iper, istr) gen_person
+
+val effective_mod
+  : ?skip_conflict:iper
+  -> config
+  -> base
+  -> (iper, Update.key, string) gen_person
+  -> (iper, iper, istr) gen_person
+
 val all_checks_person :
   base -> (iper, iper, istr) gen_person -> ifam gen_ascend ->
     ifam gen_union -> CheckItem.base_warning list