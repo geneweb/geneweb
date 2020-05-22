f30bf97dc57ffccc2cbd38d1f39cf29c49347016
Julien Sagot <julien.sagot@geneanet.org>
2020-05-22 14:52:38 +0200

    gwd: default handler now handles m=SANDBOX

--- bin/gwd/gwd.ml
+++ bin/gwd/gwd.ml
@@ -1,7 +1,17 @@
 open Geneweb
 open Gwdlib
 
-module Request = Request.Make (struct let handler = RequestHandler.defaultHandler end)
+let handler =
+  { RequestHandler.defaultHandler
+    with fallback = begin fun mode self conf base ->
+        if mode = "SANDBOX" then begin
+        let models = Gwxjg.Data.default_env conf base in
+        JgInterp.render ~conf ~file:"SANDBOX.html.jingoo" ~models
+        end else self.incorrect_request self conf base
+    end
+}
+
+module Request = Request.Make (struct let handler = handler end)
 module Main = GwDaemon.Make (Request)
 
 let _ =
