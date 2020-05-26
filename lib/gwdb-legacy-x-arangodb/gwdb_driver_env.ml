5c0ca795cba8af6a41e65a5714720eddf268190b
Julien Sagot <julien.sagot@geneanet.org>
2020-05-26 09:00:59 +0200

    gwdb-legacy-x-arangodb: error message when a required env var is missing

--- lib/gwdb-legacy-x-arangodb/gwdb_driver_env.ml
+++ lib/gwdb-legacy-x-arangodb/gwdb_driver_env.ml
@@ -1,7 +1,14 @@
-let user = Sys.getenv "GW_ARANGODB_USER"
-let password = Sys.getenv "GW_ARANGODB_PASSWORD"
-let server = Sys.getenv "GW_ARANGODB_SERVER"
-let port = Sys.getenv "GW_ARANGODB_PORT"
-let database = Sys.getenv "GW_ARANGODB_DATABASE"
+let aux s =
+  try Sys.getenv s
+  with Not_found ->
+    prerr_endline @@
+    "error: environment variable \"" ^ s ^ "\" not defined" ;
+    raise Not_found
+
+let user = aux "GW_ARANGODB_USER"
+let password = aux "GW_ARANGODB_PASSWORD"
+let server = aux "GW_ARANGODB_SERVER"
+let port = aux "GW_ARANGODB_PORT"
+let database = aux "GW_ARANGODB_DATABASE"
 let url =
   "http://" ^ user ^ ":" ^ password ^ "@" ^ server ^ ":" ^ port ^ "/_db/" ^ database ^ "/"
