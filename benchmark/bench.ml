9000ff608a9d076a5b2d1dd5a230dd8adfca5593
Julien Sagot <julien.sagot@geneanet.org>
2020-06-23 09:11:42 +0200

    Config: added an empty (uninitialized) config value.

--- benchmark/bench.ml
+++ benchmark/bench.ml
@@ -39,29 +39,7 @@ let bench () =
   in
   match Sys.getenv_opt "BENCH_BASE" with
   | Some bname when bname <> "" ->
-    let conf =
-      {Config.from="";manitou=false;supervisor=false;wizard=false;
-       #ifdef API
-         api_host="";api_port=0;
-       #endif
-         is_printed_by_template=false;friend=false;
-       just_friend_wizard=false;user="";username="";
-       auth_scheme=Config.NoAuth;pure_xhtml=false;command="";
-       indep_command="";highlight="";lang="";default_lang="";
-       default_sosa_ref=Gwdb.dummy_iper, None;multi_parents=false;
-       can_send_image=false;authorized_wizards_notes=false;
-       public_if_titles=false;public_if_no_date=false;
-       cancel_links=false;setup_link=false;access_by_key=false;
-       private_years=0;hide_names=false;use_restrict=false;
-       no_image=false;no_note=false;bname="";cgi_passwd="";
-       env=[];senv=[];henv=[];base_env=[];allowed_titles=lazy[];
-       denied_titles=lazy[];xhs="";request=[];
-       lexicon=Hashtbl.create 16;charset="";is_rtl=false;left="";
-       right="";auth_file="";border=0;n_connect=None;
-       today={Def.day=0;month=0;year=0;delta=0;prec=Def.Sure};
-       today_wd=0;time=0,0,0;ctime=0.;image_prefix="";
-       b_arg_for_basename=false}
-    in
+    let conf = Config.empty in
     let bench_w_base ?t name fn args =
       let base = Gwdb.open_base bname in
       let r = bench ?t name (fn base) args in
