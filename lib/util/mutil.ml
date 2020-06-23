bc2b11cb24f81279c6e03395fe8a8efa6ca22a5e
Julien Sagot <julien.sagot@geneanet.org>
2020-06-23 15:14:23 +0200

    Moved normalize_utf_8 to Mutil, used it in gwdb-legacy (Gwdb.insert_string).

--- lib/util/mutil.ml
+++ lib/util/mutil.ml
@@ -511,3 +511,18 @@ let input_file_ic ic =
           loop ()
         | exception End_of_file -> Buffer.contents buffer
       in loop ()
+
+let normalize_utf_8 s =
+  let b = Buffer.create (String.length s * 3) in
+  let n = Uunf.create `NFC in
+  let rec add v = match Uunf.add n v with
+    | `Uchar u -> Uutf.Buffer.add_utf_8 b u; add `Await
+    | `Await | `End -> ()
+  in
+  let add_uchar _ _ = function
+    | `Malformed _ -> add (`Uchar Uutf.u_rep)
+    | `Uchar _ as u -> add u
+  in
+  Uutf.String.fold_utf_8 add_uchar () s ;
+  add `End ;
+  Buffer.contents b
