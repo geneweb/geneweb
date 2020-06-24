ca83c66650ca5d859761e9dd3e81ed356ef5f6a3
Julien Sagot <julien.sagot@geneanet.org>
2020-06-24 12:30:50 +0200

    jgInterp: update to new jingoo interface

--- bin/gwd/jgInterp.ml
+++ bin/gwd/jgInterp.ml
@@ -12,5 +12,8 @@ let render ~conf ~file ~models =
     }
   in
   let output x = Wserver.print_string @@ Jg_runtime.string_of_tvalue x in
-  let ctx = Jg_interp.init_context ~env ~models ~output () in
+  let ctx =
+    let models = fun x -> List.assoc x models in
+    Jg_interp.init_context ~env ~models ~output ()
+  in
   Jg_interp.from_file ~env ~ctx ~models ~output file
