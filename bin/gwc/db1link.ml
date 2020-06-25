84a3ade9985cb0e006f458e1a4fbf97761005626
Julien Sagot <julien.sagot@geneanet.org>
2020-06-25 17:30:00 +0200

    bin/gwc/db1link.ml: tweak error messages

--- bin/gwc/db1link.ml
+++ bin/gwc/db1link.ml
@@ -67,7 +67,7 @@ type gen =
 let check_error gen = gen.g_errored <- true
 
 let set_error base gen x =
-  Printf.printf "\nError: " ;
+  Printf.printf "Error: " ;
   Check.print_base_error stdout base x ;
   check_error gen
 
@@ -82,6 +82,7 @@ let coi base i = base.c_couples.(i)
 let sou base i = base.c_strings.(i)
 let p_first_name base p = Mutil.nominative (sou base p.m_first_name)
 let p_surname base p = Mutil.nominative (sou base p.m_surname)
+
 let designation base p =
   let prenom = p_first_name base p in
   let nom = p_surname base p in
@@ -270,6 +271,18 @@ let find_first_available_occ gen fn sn occ =
   in
   loop occ
 
+let print_oc_aux = function 0 -> "" | n -> "." ^ string_of_int n
+
+let print_two_spellings n1 p1 oc1 n2 p2 oc2 =
+  Printf.printf "Person defined with two spellings: \"%s%s %s\" & \"%s%s %s\"\n"
+    p1 (print_oc_aux oc1) n1 p2 (print_oc_aux oc2) n2
+
+let print_person_already_defined n1 p1 oc1 n2 p2 oc2 =
+  Printf.printf "Person already defined: \"%s%s %s\"" p1 (print_oc_aux oc1) n1 ;
+  if p1 <> p2 || n1 <> n2
+  then Printf.printf "as name: \"%s%s %s\"\n" p2 (print_oc_aux oc2) n2
+  else Printf.printf "\n"
+
 let insert_undefined gen key =
   let occ = key.pk_occ + gen.g_file_info.f_shift in
   let (x, ip) =
@@ -318,17 +331,9 @@ let insert_undefined gen key =
        sou gen.g_base x.m_surname <> key.pk_surname
     then
       begin
-        Printf.printf "\nPerson defined with two spellings:\n";
-        Printf.printf "  \"%s%s %s\"\n" key.pk_first_name
-          (match x.m_occ with
-             0 -> ""
-           | n -> "." ^ string_of_int n)
-          key.pk_surname;
-        Printf.printf "  \"%s%s %s\"\n" (p_first_name gen.g_base x)
-          (match occ with
-             0 -> ""
-           | n -> "." ^ string_of_int n)
-          (p_surname gen.g_base x);
+        print_two_spellings
+          key.pk_surname key.pk_first_name x.m_occ
+          (p_surname gen.g_base x) (p_first_name gen.g_base x) occ ;
         gen.g_def.(ip) <- true;
         check_error gen
       end;
@@ -372,20 +377,10 @@ let insert_person gen so =
   in
   if gen.g_def.(ip) then
     begin
-      Printf.printf "\nPerson already defined: \"%s%s %s\"\n" so.first_name
-        (match x.m_occ with
-           0 -> ""
-         | n -> "." ^ string_of_int n)
-        so.surname;
-      if p_first_name gen.g_base x <> so.first_name ||
-         p_surname gen.g_base x <> so.surname
-      then
-        Printf.printf "as name: \"%s%s %s\"\n" (p_first_name gen.g_base x)
-          (match occ with
-             0 -> ""
-           | n -> "." ^ string_of_int n)
-          (p_surname gen.g_base x);
-      flush stdout;
+      print_person_already_defined
+        so.surname so.first_name x.m_occ
+        (p_surname gen.g_base x) (p_first_name gen.g_base x) occ ;
+      flush stdout ;
       check_error gen
     end
   else gen.g_def.(ip) <- true;
@@ -394,17 +389,9 @@ let insert_person gen so =
        sou gen.g_base x.m_surname <> so.surname
     then
       begin
-        Printf.printf "\nPerson defined with two spellings:\n";
-        Printf.printf "  \"%s%s %s\"\n" so.first_name
-          (match x.m_occ with
-             0 -> ""
-           | n -> "." ^ string_of_int n)
-          so.surname;
-        Printf.printf "  \"%s%s %s\"\n" (p_first_name gen.g_base x)
-          (match occ with
-             0 -> ""
-           | n -> "." ^ string_of_int n)
-          (p_surname gen.g_base x);
+        print_two_spellings
+          so.surname so.first_name x.m_occ
+          (p_surname gen.g_base x) (p_first_name gen.g_base x) occ ;
         gen.g_def.(ip) <- true;
         check_error gen
       end;
@@ -459,12 +446,7 @@ let check_parents_not_already_defined gen ix fath moth =
     let cpl = coi gen.g_base int in
     let p = Adef.father cpl in
     let m = Adef.mother cpl in
-    Printf.printf "I cannot add \"%s\", child of\n    \
-                   - \"%s\"\n    \
-                   - \"%s\",\n\
-                   because this persons still exists as child of\n    \
-                   - \"%s\"\n    \
-                   - \"%s\"."
+    Printf.printf "I cannot add \"%s\", child of \"%s\" & \"%s\", because this persons still exists as child of \"%s\" & \"%s\".\n"
       (designation gen.g_base x) (designation gen.g_base fath)
       (designation gen.g_base moth)
       (designation gen.g_base (poi gen.g_base p))
@@ -480,7 +462,7 @@ let check_parents_not_already_defined gen ix fath moth =
 let notice_sex gen p s =
   if p.m_sex = Neuter then p.m_sex <- s
   else if p.m_sex <> s && s <> Neuter then
-    Printf.printf "\nInconsistency about the sex of\n  %s %s\n"
+    Printf.printf "Inconsistency about the sex of %s %s\n"
       (p_first_name gen.g_base p) (p_surname gen.g_base p)
 
 let fevent_name_unique_string gen =
