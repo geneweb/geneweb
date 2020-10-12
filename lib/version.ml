a1682615e6322422669d7dbd5adb94bf68e79a5b
Henri83 <henri.gouraud@laposte.net>
2020-10-12 08:55:36 +0200

    Set version to 7.00

--- lib/version.ml
+++ lib/version.ml
@@ -1,7 +1,7 @@
 (* $Id: version.ml,v 5.8 2011-01-06 09:59:58 ddr Exp $ *)
 (* Copyright (c) 1998-2007 INRIA *)
 
-let txt = "7.00-exp"
+let txt = "7.0.0"
 
 let available_languages =
   ["af"; "bg"; "br"; "ca"; "cs"; "da"; "de"; "en"; "es"; "eo"; "et"; "fi";
