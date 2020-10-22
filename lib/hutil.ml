3ff7a6752e9ae7724860870d7922dc90e5315a5d
Henri83 <henri.gouraud@laposte.net>
2020-10-22 20:30:59 +0200

    Revert (partially) commit 32c1ecd33 which had broken hed/trl include

--- lib/hutil.ml
+++ lib/hutil.ml
@@ -57,7 +57,10 @@ let header_without_http conf title =
   Wserver.printf "  <meta name=\"viewport\" content=\"width=device-width, \
                     initial-scale=1, shrink-to-fit=no\">\n";
   Util.include_template conf [] "css" (fun () -> ());
-  Templ.include_hed_trl conf "hed";
+  begin match Util.open_templ conf "hed" with
+    Some ic -> Templ.copy_from_templ conf [] ic
+  | None -> ()
+  end;
   Wserver.printf "\n</head>\n";
   let s =
     try " dir=\"" ^ Hashtbl.find conf.lexicon "!dir" ^ "\"" with
@@ -107,7 +110,10 @@ let rheader conf title =
 
 let gen_trailer with_logo conf =
   let conf = {conf with is_printed_by_template = false} in
-  Templ.include_hed_trl conf "trl";
+  begin match Util.open_templ conf "trl" with
+    Some ic -> Templ.copy_from_templ conf [] ic
+  | None -> ()
+  end;
   if with_logo then Templ.print_copyright_with_logo conf
   else Templ.print_copyright conf;
   Util.include_template conf [] "js" (fun () -> ());
