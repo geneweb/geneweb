7823d1f68a367da1e02ebd556d9b57fc7dd4d4dd
Julien Sagot <julien.sagot@geneanet.org>
2020-11-18 10:58:18 +0100

    gwexport: set default desc option to 0

--- bin/gwexport/gwexport.ml
+++ bin/gwexport/gwexport.ml
@@ -265,7 +265,7 @@ let select opts ips =
       if (opts.asc <> None && opts.ascdesc <> None) || opts.desc <> None then begin
         assert (opts.censor = 0) ;
         let asc = Opt.default max_int opts.asc in
-        let desc = - (Opt.default max_int opts.desc) in
+        let desc = - (Opt.default 0 opts.desc) in
         let ht =
           match opts.ascdesc with
           | Some ascdesc ->
