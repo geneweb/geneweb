bbaddf13bf6327551c3370cf353d8aba4540ca87
Julien Sagot <julien.sagot@geneanet.org>
2020-12-01 10:42:51 +0100

    gwd: removed default SANDBOX mode fallback

--- bin/gwd/gwd.ml
+++ bin/gwd/gwd.ml
@@ -1,19 +1,6 @@
 open Geneweb
-open Gwdlib
 
-
-let handler =
-  { RequestHandler.defaultHandler
-    with fallback = begin fun mode self conf base ->
-        match mode with
-        | "SANDBOX" ->
-          let models = Gwxjg.Data.default_env conf base in
-          JgInterp.render ~conf ~file:"SANDBOX.html.jingoo" ~models
-        | _ -> self.incorrect_request self conf base
-      end
-  }
-
-module Request = Request.Make (struct let handler = handler end)
+module Request = Request.Make (struct let handler = RequestHandler.defaultHandler end)
 module Main = GwDaemon.Make (Request)
 
 let _ = Main.run ()
