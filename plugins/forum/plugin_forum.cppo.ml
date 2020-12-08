4a3dcd26226cbc9d5de88358dcc3ea7c450d574a
Julien Sagot <julien.sagot@geneanet.org>
2020-12-08 10:30:32 +0100

    Converted FORUM feature into a plugin

---
+++ plugins/forum/plugin_forum.cppo.ml
@@ -0,0 +1,32 @@
+module Forum = struct
+  #include "forum.ml"
+end
+module ForumDisplay = struct
+  #include "forumDisplay.ml"
+end
+
+open Geneweb
+open Config
+
+let ns = "forum"
+
+let wrapper fn _ conf base =
+  match base with
+  | Some b when Util.p_getenv conf.base_env "disable_forum" <> Some "yes" ->
+    fn conf b ;
+    true
+  | _ ->
+    Hutil.incorrect_request conf ;
+    true
+
+let () =
+  Gwd_lib.GwdPlugin.register ~ns
+    [ "FORUM", wrapper ForumDisplay.print
+    ; "FORUM_ADD", wrapper ForumDisplay.print_add
+    ; "FORUM_ADD_OK", wrapper ForumDisplay.print_add_ok
+    ; "FORUM_DEL", wrapper ForumDisplay.print_del
+    ; "FORUM_P_P", wrapper ForumDisplay.print_access_switch
+    ; "FORUM_SEARCH", wrapper ForumDisplay.print_search
+    ; "FORUM_VAL", wrapper ForumDisplay.print_valid
+    ; "FORUM_VIEW", wrapper ForumDisplay.print
+    ]
