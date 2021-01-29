aedb45b287b161158d4208cb8f1041e4c0e92a60
Henri83 <henri.gouraud@laposte.net>
2021-01-29 14:15:36 +0100

    Some more updates for gwsetup and version.ml

--- lib/version.ml
+++ lib/version.ml
@@ -3,6 +3,6 @@
 let txt = "7.0.0"
 
 let available_languages =
-  ["af"; "bg"; "br"; "ca"; "cs"; "da"; "de"; "en"; "es"; "eo"; "et"; "fi";
+  ["af"; "bg"; "br"; "ca"; "co"; "cs"; "da"; "de"; "en"; "es"; "eo"; "et"; "fi";
    "fr"; "he"; "is"; "it"; "lv"; "nl"; "no"; "pl"; "pt"; "pt-br"; "ro"; "ru";
-   "sk"; "sl"; "sv"; "zh"]
+   "sk"; "sl"; "sv"; "tr"; "zh"]
