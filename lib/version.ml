4a5d0619c5cbb40cd78163022809b70f1d066ddb
TitiFix <github.titifix@gmail.com>
2021-04-29 19:08:16 +0200

    Maj droits

--- lib/version.ml
+++ lib/version.ml
@@ -1,6 +1,6 @@
 (* Copyright (c) 1998-2007 INRIA *)
 
-let txt = "7.1.0.TitiFix_fixwserver_20210428" (* from TitiFix_fixwserver commit 0bef4953 Apr_28_10:25:54_2021 *)
+let txt = "7.1.0.TitiFix_fixwserver_20210429" (* from TitiFix_fixwserver commit 216a1045 Apr_28_20:04:55_2021 *)
 
 let available_languages =
   ["af"; "bg"; "br"; "ca"; "cs"; "da"; "de"; "en"; "es"; "eo"; "et"; "fi";
