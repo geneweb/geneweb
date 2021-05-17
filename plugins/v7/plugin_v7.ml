9d83b7bb4f9aa15bb46ac2ea766a16b7aa04566d
Julien Sagot <julien.sagot@geneanet.org>
2021-05-17 11:19:43 +0200

    v7 plugin: switch to embed_in_plugin_libraries instead of cppo includes

--- plugins/v7/plugin_v7.ml
+++ plugins/v7/plugin_v7.ml
@@ -1,21 +1,9 @@
-open Geneweb
-open Config
-open Util
-
-module Cousins = struct
-  #include "cousins.ml"
-end
-module Perso = struct
-  #include "perso.ml"
-end
-module Some = struct
-  #include "some.ml"
-end
-module Place = struct
-  #include "place.ml"
-end
-
+open Geneweb.Config
+module Gwdb = Geneweb.Gwdb
+module Util = Geneweb.Util
 module Request = Gwd_lib.Request
+
+open Plugin_v7_lib
 
 let w_base =
   Request.w_base
@@ -25,10 +13,10 @@ let w_person =
     ~none:(fun c b -> Gwd_lib.Request.very_unknown c b ; true)
 
 let person_selected conf base p =
-  match p_getenv conf.senv "em" with
+  match Util.p_getenv conf.senv "em" with
   | Some "R" -> false
   | Some _ -> false
-  | None -> Perso.print conf base p ; true
+  | None -> V7_perso.print conf base p ; true
 
 let home conf base : bool =
   if base <> None
@@ -36,8 +24,8 @@ let home conf base : bool =
     w_base begin fun conf base : bool ->
       if Request.only_special_env conf.env then false
       else w_person begin fun conf base p ->
-          match p_getenv conf.env "ptempl" with
-          | Some t when p_getenv conf.base_env "ptempl" = Some "yes" -> false
+          match Util.p_getenv conf.env "ptempl" with
+          | Some t when Util.p_getenv conf.base_env "ptempl" = Some "yes" -> false
           | _ -> person_selected conf base p
         end conf base
     end conf base
@@ -47,19 +35,19 @@ let l =
   w_base begin fun conf base ->
     Gwdb.dummy_iper
     |> Gwdb.empty_person base
-    |> Perso.interp_templ "list" conf base
+    |> !V7_interp.templ "list" conf base
     |> fun () -> true
     end
 
 let p =
-  w_base begin fun conf base -> match p_getenv conf.env "v" with
-    | Some v -> Some.first_name_print conf base v ; true
+  w_base begin fun conf base -> match Util.p_getenv conf.env "v" with
+    | Some v -> V7_some.first_name_print conf base v ; true
     | None -> false
   end
 
 let ps =
   w_base begin fun conf base ->
-    Place.print_all_places_surnames conf base ;
+    V7_place.print_all_places_surnames conf base ;
     true
   end
 
