e01ccd48204629f567c3e967b9d4cdb9e8e74b81
Julien Sagot <julien.sagot@geneanet.org>
2021-05-28 15:15:34 +0200

    gwd: log to stderr as default instead of gwd.syslog file

--- bin/gwd/gwdLog.ml
+++ bin/gwd/gwdLog.ml
@@ -47,39 +47,39 @@ let syslog (level : level) msg =
 #endif
 
 #ifndef SYSLOG
-let syslog =
-  let fname =
-    try Sys.getenv "GW_SYSLOG_FILE"
-    with Not_found -> Filename.basename Sys.executable_name ^ ".syslog"
-  in
-  fun (level : level) msg ->
-    if !verbosity
-       >=
-       match level with
-       | `LOG_EMERG -> 0
-       | `LOG_ALERT -> 1
-       | `LOG_CRIT -> 2
-       | `LOG_ERR -> 3
-       | `LOG_WARNING -> 4
-       | `LOG_NOTICE -> 5
-       | `LOG_INFO -> 6
-       | `LOG_DEBUG -> 7
-    then begin
-      let tm = Unix.(time () |> localtime) in
-      let level =
-        match level with
-        | `LOG_EMERG -> "EMERGENCY"
-        | `LOG_ALERT -> "ALERT"
-        | `LOG_CRIT -> "CRITICAL"
-        | `LOG_ERR -> "ERROR"
-        | `LOG_WARNING -> "WARNING"
-        | `LOG_NOTICE -> "NOTICE"
-        | `LOG_INFO -> "INFO"
-        | `LOG_DEBUG -> "DEBUG"
-      in
-      let oc = open_out_gen [ Open_wronly ; Open_creat ; Open_append ] 0o644 fname in
-      Printf.fprintf oc "[%s]: %s %s\n" (Mutil.sprintf_date tm) level msg ;
-      close_out oc ;
+let syslog (level : level) msg =
+  if !verbosity
+     >=
+     match level with
+     | `LOG_EMERG -> 0
+     | `LOG_ALERT -> 1
+     | `LOG_CRIT -> 2
+     | `LOG_ERR -> 3
+     | `LOG_WARNING -> 4
+     | `LOG_NOTICE -> 5
+     | `LOG_INFO -> 6
+     | `LOG_DEBUG -> 7
+  then begin
+    let tm = Unix.(time () |> localtime) in
+    let level =
+      match level with
+      | `LOG_EMERG -> "EMERGENCY"
+      | `LOG_ALERT -> "ALERT"
+      | `LOG_CRIT -> "CRITICAL"
+      | `LOG_ERR -> "ERROR"
+      | `LOG_WARNING -> "WARNING"
+      | `LOG_NOTICE -> "NOTICE"
+      | `LOG_INFO -> "INFO"
+      | `LOG_DEBUG -> "DEBUG"
+    in
+    let print oc = Printf.fprintf oc "[%s]: %s %s\n" (Mutil.sprintf_date tm) level msg in
+    begin match Sys.getenv_opt "GW_SYSLOG_FILE" with
+      | Some fn ->
+        let oc = open_out_gen [ Open_wronly ; Open_creat ; Open_append ] 0o644 fn in
+        print oc ;
+        close_out oc
+      | None -> print stderr
+    end ;
 #ifdef DEBUG
     Printexc.print_backtrace stderr ;
 #endif
