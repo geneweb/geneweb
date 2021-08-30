5d22e9bff4a90598279e1c156579a675e25e0cb3
Julien SAGOT <1826552+sagotch@users.noreply.github.com>
2021-08-30 22:16:42 +0200

    gwrepl: added GWREPL_PPF parameter

--- bin/gwrepl/gwrepl.ml
+++ bin/gwrepl/gwrepl.ml
@@ -28,7 +28,7 @@ let unpack ~force_unpack ~verbose =
     Array.iter (output_file ~verbose) Data.shared ;
   end
 
-let run ~verbose ~noprompt =
+let run ~ppf ~verbose ~noprompt =
   Clflags.noversion := true ;
   Clflags.noinit := true ;
   if Array.length Sys.argv <> 1 || noprompt then Clflags.noprompt := true ;
@@ -38,17 +38,27 @@ let run ~verbose ~noprompt =
   end Data.directories ;
   Array.iter begin fun (file, _) ->
     if verbose then print_endline ("load: " ^ file) ;
-    path file |> Topdirs.dir_load Format.std_formatter
+    path file |> Topdirs.dir_load ppf
   end Data.cmas ;
-  Toploop.loop Format.std_formatter
+  Toploop.loop ppf
 
 (** For script execution, run:
-    cat <script.ml> | [ GWREPL_VERBOSE=1 ] [ GWREPL_FORCE_UNPACK=1 ] [ GWREPL_NOPROMPT=1 ] gwrepl.exe [scrip_arg1] ...  *)
+    cat <script.ml> | [ GWREPL_PPF=/dev/null ] [ GWREPL_VERBOSE=1 ] [ GWREPL_FORCE_UNPACK=1 ] [ GWREPL_NOPROMPT=1 ] gwrepl.exe [scrip_arg1] ...  *)
 (** For interactive toplevel, run:
     gwrepl.exe *)
 let () =
+  let ppf =
+    match Sys.getenv_opt "GWREPL_PPF" with
+    | None | Some ("STD"|"std") -> Format.std_formatter
+    | Some ("ERR"|"err") -> Format.err_formatter
+    | Some path ->
+      let oc = open_out path in
+      Format.make_formatter
+        (Stdlib.output_substring oc)
+        (fun () -> Stdlib.flush oc)
+  in
   let verbose = Sys.getenv_opt "GWREPL_VERBOSE" <> None in
   let force_unpack = Sys.getenv_opt "GWREPL_FORCE_UNPACK" <> None in
   let noprompt = Sys.getenv_opt "GWREPL_NOPROMPT" <> None in
   unpack ~force_unpack ~verbose ;
-  run ~verbose ~noprompt
+  run ~ppf ~verbose ~noprompt
