b73081aedd7edbb151dd3f00ebfddac6f43a9887
Steven de Oliveira <de.oliveira.steven@gmail.com>
2021-12-21 15:14:50 +0100

    Documenting UpdateDataDisplay

---
+++ lib/updateDataDisplay.mli
@@ -0,0 +1,7 @@
+
+(** Displays a menu for updating Geneweb's dictionary of names, last names,
+    locations, sources and professions. *)
+val print_mod : Config.config -> Gwdb.base -> unit
+
+(** Updates persons linked to the updated data. *)
+val print_mod_ok : Config.config -> Gwdb.base -> unit
