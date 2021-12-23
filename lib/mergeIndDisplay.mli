b3d45e3fe6ff5503bf1d55c406f13d3c05ef54d8
Steven de Oliveira <de.oliveira.steven@gmail.com>
2021-12-23 15:36:19 +0100

    Documentation of MergeIndDisplay

---
+++ lib/mergeIndDisplay.mli
@@ -0,0 +1,6 @@
+
+(** Displays a form for merging two persons *)
+val print : Config.config -> Gwdb.base -> unit
+
+(** Displays the page for killing ancestors (undocumented feature) *)
+val print_kill_ancestors : Config.config -> Gwdb.base -> unit
