f73409cd1d5a863e2e4ad5eff846118e2599558b
Julien Sagot <julien.sagot@geneanet.org>
2022-02-04 12:56:08 +0100

    Added doc for GwdPlugin module

--- bin/gwd/gwdPlugin.mli
+++ bin/gwd/gwdPlugin.mli
@@ -1,15 +1,49 @@
+(** When dynamically loading a plugin, this variable contains
+    the path of the assets directory associated to the plugin
+    being currently loaded.
+*)
 val assets : string ref
 
-val ht : (string, (string * (Geneweb.Config.config -> Gwdb.base option -> bool))) Hashtbl.t
+(** [register ~ns handlers] register modes handlers of a plugin.
 
-val register :
-  ns:string ->
-  (string * (string -> Geneweb.Config.config -> Gwdb.base option -> bool)) list ->
+    [ns] is the namespace of the plugin (i.e. its name)
+
+    [handler] is a associative list of handler.
+    The key is the mode (the `m` GET/POST parameter).
+    The value is the handler itself. The difference between a plugin
+    handler and default gwd's handlers (the ones in request.ml) is that
+    a plugin handler takes an extra (first) argument being
+    the path of the asset directory associated to this plugin
+    and returns a boolean.
+
+    If the handler returns [true], it means that it actually processed
+    the request. If is is [false], [gwd] must try another plugin handler to
+    treat the request. If no plugin is suitable, gwd's default handler
+    must be used, or fail if it does not exists.
+
+    Handlers can overwrite pre-existing modes or create new ones.
+ *)
+val register
+  : ns:string
+  -> (string * (string -> Geneweb.Config.config -> Gwdb.base option -> bool)) list ->
   unit
 
-val se : (string * (Geneweb.Config.config -> Gwdb.base option -> unit)) list ref
+(** Table of handlers registered by plugins.  *)
+val ht : (string, (string * (Geneweb.Config.config -> Gwdb.base option -> bool))) Hashtbl.t
 
+(** [register_se ~ns hook] register a plugin hook (side effect function).
+
+    If enabled, hooks are executed before the request handlers, in the
+    order of registration (first registred = first executed).
+
+    For exemple, a plugin could be to change the [conf] output to print everything
+    in a buffer and apply a transformation to the resulting document before actually
+    sending it to the client.
+  *)
 val register_se :
   ns:string ->
   (string -> Geneweb.Config.config -> Gwdb.base option -> unit) ->
   unit
+
+(** Table of hooks registered by plugins. *)
+val se : (string * (Geneweb.Config.config -> Gwdb.base option -> unit)) list ref
