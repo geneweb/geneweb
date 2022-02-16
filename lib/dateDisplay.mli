5eb340d6f9c1bde063ea8810add480e611ac106a
Julien Sagot <julien.sagot@geneanet.org>
2022-02-16 15:51:01 +0100

    Restore deleted function from DateDisplay interface

--- lib/dateDisplay.mli
+++ lib/dateDisplay.mli
@@ -8,9 +8,15 @@ open Gwdb
     Return the day of the week for this [date] *)
 val get_wday : config -> date -> string
 
-(** Returns textual representation of the date translated to the current language. Uses different encodings depending on day's,
-    month's and year's accessibility. Doesn't consider precision. *)
+(** Returns textual representation of the date translated to the current language.
+    Uses different encodings depending on day's, month's and year's accessibility.
+    Doesn't consider precision. *)
 val code_dmy : config -> dmy -> string
+
+(** Returns textual representation of a day / month / year in the hebrew calendar
+    translated to the current language.
+*)
+val code_hebrew_date : config -> int -> int -> int -> string
 
 (** Converts and translate date to the textual representation for the giving language. Considers precision. *)
 val string_of_dmy : Config.config -> Def.dmy -> Adef.safe_string
@@ -19,10 +25,19 @@ val string_of_dmy : Config.config -> Def.dmy -> Adef.safe_string
     Difference between calendars is not taken into the acount. *)
 val string_of_date : config -> date -> Adef.safe_string
 
-(** Converts and translate date with considering different calendars with prefix "on" before dates (changes for other languages).
+(** Converts and translate date with considering different calendars with prefix "on"
+    before dates (changes for other languages).
     Date precision is much more verbose then with [string_of_date]. Decline phrase if needed.
     If [link] is true then encapsulates result in HTML link to the page calendar's date converter. *)
 val string_of_ondate : ?link:bool -> config -> date -> Adef.safe_string
+
+(** Translate a date in the french calendar
+    with prefix "on" before dates (changes for other languages). *)
+val string_of_on_french_dmy : config -> dmy -> Adef.safe_string
+
+(** Translate a date in the habrew calendar
+    with prefix "on" before dates (changes for other languages). *)
+val string_of_on_hebrew_dmy : config -> dmy -> Adef.safe_string
 
 (** Returns date in format dd/mm/yyyy. Format could be different for other languages (defined by [!dates order]
     keyword in the lexicon). *)
@@ -77,3 +92,22 @@ val string_of_date_aux
   -> Config.config
   -> Def.date
   -> Adef.safe_string
+
+(**/**)
+
+(** Returns the translation of the month of French calendar
+    First month is [0]
+*)
+val hebrew_month : config -> int -> string
+
+(** Returns the translation of the month of Hebrew calendar
+    First month is [0]
+*)
+val french_month : config -> int -> string
+
+(** [string_of_prec_dmy conf s s2 d]
+    Takes two date representations (as strings) [s] and [s2] and
+    returns translated phrase according to prec of [d].
+    [d] is only used to determine the precision
+ *)
+val string_of_prec_dmy : config -> string -> string -> dmy -> Adef.safe_string
