e0863098bd33cd78b0b28bc0631f00ef0e5ef128
Julien Sagot <julien.sagot@geneanet.org>
2022-03-29 16:28:15 +0200

    Fixing safe html in wiki notes

--- plugins/gwxjg/gwxjg_data.ml
+++ plugins/gwxjg/gwxjg_data.ml
@@ -14,13 +14,9 @@ let escaped (x : Adef.escaped_string) = Tsafe (x :> string)
 
 let unbox_string = function Tsafe s | Tstr s -> s | x -> Jg_types.failwith_type_error_1 "unbox_string" x
 
-let mk_source_rs conf base str  =
-  let s = Util.string_with_macros conf [] str in
-  let lines =
-    match Wiki.html_of_tlsw conf s with
-    | "<p>" :: x :: [ "</p>" ] -> [ x ]
-    | lines -> lines
-  in
+let wiki_aux pp conf base env str =
+  let s = Util.string_with_macros conf env str in
+  let lines = pp (Wiki.html_of_tlsw conf s) in
   let wi =
     { Wiki.wi_mode = "NOTES"
     ; Wiki.wi_file_path = Notes.file_path conf base
@@ -28,23 +24,18 @@ let mk_source_rs conf base str  =
     ; Wiki.wi_always_show_link = conf.wizard || conf.friend
     }
   in
-  ( Tstr str
-  , Tsafe (Wiki.syntax_links conf wi (String.concat "\n" lines))
-  )
+  let html =
+    String.concat "\n" lines
+    |> Wiki.syntax_links conf wi
+    |> Util.safe_html
+  in
+  (Tstr str, safe html)
+
+let mk_source_rs conf base str =
+  wiki_aux (function "<p>" :: x :: [ "</p>" ] -> [ x ] | x -> x) conf base [] str
 
 let mk_note_rs conf base env str =
-  let s = Util.string_with_macros conf env str in
-  let lines = Wiki.html_of_tlsw conf s in
-  let wi =
-    { Wiki.wi_mode = "NOTES"
-    ; Wiki.wi_file_path = Notes.file_path conf base
-    ; Wiki.wi_person_exists = Util.person_exists conf base
-    ; Wiki.wi_always_show_link = conf.wizard || conf.friend
-    }
-  in
-  ( Tstr str
-  , Tsafe (Wiki.syntax_links conf wi (String.concat "\n" lines))
-  )
+  wiki_aux (fun x -> x) conf base env str
 
 let mk_person_note_rs conf base p note =
   let env = ['i', (fun () -> Util.default_image_name base p)] in
