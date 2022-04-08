6084d28ec87dd2498b8400eb3dc4f1f0475fb9e8
Elie Canonici Merle <elie.canonicimerle@geneanet.org>
2022-04-08 16:46:27 +0200

    translate wip

--- lib/gwdb-legacy/translate.mli
+++ lib/gwdb-legacy/translate.mli
@@ -1,4 +1,7 @@
 
 
-val translate_person : ('iper, 'person, 'string) Dbdisk.gen_person ->
-                       ('iper, 'person, 'string) Def.gen_person
+val as_def_person : ('iper, 'person, 'string) Dbdisk.gen_person ->
+                 ('iper, 'person, 'string) Def.gen_person
+
+val as_dsk_person : ('iper, 'person, 'string) Def.gen_person ->
+                 ('iper, 'person, 'string) Dbdisk.gen_person
