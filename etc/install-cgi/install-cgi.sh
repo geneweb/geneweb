14dae68507132348d2017f9b922018b9161ccc36
hgouraud <Henri.Gouraud@LaPoste.net>
2022-05-05 17:44:16 +0200

    Script to install GeneWeb in CGI mode (#1319)
    * Script to install GeneWeb in CGI mode
    
    * Some documentation for install-cgi
    
    * Remove static_path from test.gwf

---
+++ etc/install-cgi/install-cgi.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+# This script assumes that the distribution folder,
+# (or a link to it) is installed at the Document root
+# of the Web server (Apache or other).
+# Please update the search for DocumentRoot according to
+# your understanding of other environments.
+
+# The purpose of the script is to install
+# - gwd.cgi and test.cgi in the cgi-bin folder
+# - a test base in the bases folder
+
+OS_ENV=`uname` 
+
+# Find the document root under Apache
+
+if [ $OS_ENV = "Linux" ]; then
+  if [ -f /etc/apache2/httpd.conf ]; then
+    TMP=`grep -i "DocumentRoot" /etc/apache2/httpd.conf| sed '/#/d'`
+  elif [ -f /etc/apache2/sites-enabled/000-default.conf ]; then
+    TMP=`grep DocumentRoot /etc/apache2/sites-enabled/000-default.conf | sed '/#/d'`
+  fi
+elif [ $OS_ENV = "Darwin" ]; then
+  TMP=`grep -i "DocumentRoot" /etc/apache2/httpd.conf | sed '/#/d'`
+else
+  echo "Please find value of DocumentRoot of your server and set it manually"
+fi
+
+WEB_ROOT=`echo $TMP | sed -e "s/DocumentRoot//g" \
+        | sed "s/^[ \t]*//" | sed -e 's/"//g'`
+
+echo Document root is: $WEB_ROOT
+
+BIN_DIR=$WEB_ROOT/distribution/gw
+BASES=$WEB_ROOT/distribution/bases
+
+DIR="$(dirname "$0")"
+cd "$DIR"
+
+if [ -d $WEB_ROOT/cgi-bin ]; then
+  cp gwd.cgi $WEB_ROOT/cgi-bin 
+  cp test.cgi $WEB_ROOT/cgi-bin 
+else
+  echo "missing cgi-bin"
+  exit -1
+fi
+
+chmod +x $WEB_ROOT/cgi-bin/gwd.cgi
+
+cp test.gwf $BASES
+$BIN_DIR/gwc -f -o $BASES/test test.gw
---
+++ etc/install-cgi/test.cgi
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo 'Content-type: text/html'
+echo  
+echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"   '
+echo '   "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">  '
+echo '<html xmlns="http://www.w3.org/1999/xhtml">  '
+echo '<body>'
+echo 'This is a test for cgi commands'
+echo '</body>'
+echo '</html>'
+
