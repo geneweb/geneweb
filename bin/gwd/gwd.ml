9c7d45504a70843b678e97fef6479242199fdec1
Henri Gouraud <henri.gouraud@laposte.net>
2022-05-17 22:34:05 +0200

    Treat image_prefix same way as static_path

--- bin/gwd/gwd.ml
+++ bin/gwd/gwd.ml
@@ -1230,12 +1230,16 @@ let make_conf from_addr request script_name env =
        {day = tm.Unix.tm_mday; month = succ tm.Unix.tm_mon;
         year = tm.Unix.tm_year + 1900; prec = Sure; delta = 0};
      today_wd = tm.Unix.tm_wday;
-     time = tm.Unix.tm_hour, tm.Unix.tm_min, tm.Unix.tm_sec; ctime = utm;
-     image_prefix =
+     time = tm.Unix.tm_hour, tm.Unix.tm_min, tm.Unix.tm_sec; ctime = utm
+    ; image_prefix =
        if !images_url <> "" then !images_url
-       else if !(Wserver.cgi) then ar.ar_command ^ "?m=IM&v="
-       else "images";
-    static_path =
+       else if !(Wserver.cgi) then
+					begin match Sys.getenv_opt "GW_STATIC_PATH" with
+					| Some x -> x ^ "../images"
+					| None -> "../distribution/gw/images/"
+					end
+       else "images"
+    ; static_path =
       begin match Sys.getenv_opt "GW_STATIC_PATH" with
       | Some x -> x
       | None -> "../distribution/gw/etc/"
--- lib/templ.ml
+++ lib/templ.ml
@@ -178,7 +178,12 @@ and eval_simple_variable conf =
       in
       Util.doctype conf ^ "\n"
   | "highlight" -> conf.highlight
-  | "image_prefix" -> Util.image_prefix conf
+  | "image_prefix" ->
+      if conf.cgi then
+        match Util.p_getenv conf.base_env "image_prefix" with
+        | Some x -> x
+        | None -> conf.image_prefix
+      else conf.image_prefix
   | "lang" -> conf.lang
   | "left" -> conf.left
   | "nl" -> "\n"
