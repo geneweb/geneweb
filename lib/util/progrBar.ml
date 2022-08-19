bd28be367515f7aabe6a1b89ce6a72375ce6d963
swrup <olivier.pierre@geneanet.org>
2022-08-19 11:51:28 +0200

    fix unused for-loop index warnings

--- lib/util/progrBar.ml
+++ lib/util/progrBar.ml
@@ -12,13 +12,13 @@ let draw_len = String.length draw
 let pb_cnt = size * draw_rep * draw_len
 
 let start () =
-  for i = 1 to size do Printf.eprintf "%c" !empty done; Printf.eprintf "\013"
+  for _i = 1 to size do Printf.eprintf "%c" !empty done; Printf.eprintf "\013"
 
 let run cnt max_cnt =
   let pb_cnt = if max_cnt < pb_cnt then size * draw_len else pb_cnt in
   let already_disp = cnt * size / max_cnt in
   let to_disp = (cnt + 1) * size / max_cnt in
-  for i = already_disp + 1 to to_disp do Printf.eprintf "%c" !full done;
+  for _i = already_disp + 1 to to_disp do Printf.eprintf "%c" !full done;
   let already_disp = cnt * pb_cnt / max_cnt in
   let to_disp = (cnt + 1) * pb_cnt / max_cnt in
   if cnt = max_cnt - 1 then Printf.eprintf " \008"
