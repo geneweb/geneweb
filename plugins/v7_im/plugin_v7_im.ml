59f0e3373606a139e98dbf0e7be122972175358e
Elie Canonici Merle <elie.canonicimerle@geneanet.org>
2022-08-31 15:59:59 +0200

    fix lock before opening bases
    clean

--- plugins/v7_im/plugin_v7_im.ml
+++ plugins/v7_im/plugin_v7_im.ml
@@ -13,16 +13,16 @@ let can_send_image conf =
   List.assoc_opt "can_send_image" conf.base_env <> Some "no"
 
 let () =
-  let wrap' fn = fun conf base -> fn conf base ; true in
+  let wrap' fn = fun conf base_name -> fn conf base_name ; true in
   let wrap fn =
-    fun _ conf base ->
+    fun conf base_name ->
       if conf.wizard && can_send_image conf
-      then w_base fn conf base
+      then w_base fn conf base_name
       else false
   in
   Gwd_lib.GwdPlugin.register ~ns
-    [ "DEL_IMAGE", V7_im_sendImage.print_del |> wrap' |> wrap
-    ; "DEL_IMAGE_OK", V7_im_sendImage.print_del_ok |> wrap' |> w_lock |> wrap
-    ; "SND_IMAGE", V7_im_sendImage.print |> wrap' |> wrap
-    ; "SND_IMAGE_OK", V7_im_sendImage.print_send_ok |> wrap' |> w_lock |> wrap
+    [ "DEL_IMAGE", (fun _assets -> V7_im_sendImage.print_del |> wrap' |> wrap)
+    ; "DEL_IMAGE_OK", (fun _assets -> V7_im_sendImage.print_del_ok |> wrap' |> wrap |> w_lock)
+    ; "SND_IMAGE", (fun _assets -> V7_im_sendImage.print |> wrap' |> wrap)
+    ; "SND_IMAGE_OK", (fun _assets -> V7_im_sendImage.print_send_ok |> wrap' |> wrap |> w_lock)
     ]
