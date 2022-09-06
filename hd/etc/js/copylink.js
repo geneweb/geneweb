818356b8a63caaa8fdb58171d0f4beccf8e41e9f
A2 <a2line@gmail.com>
2022-09-06 20:40:37 +0200

    a few new features for templates
    * fancharts
    * JV compact desc. tree
    * buttons on trees
    * full cousins navigation table
    * filter on asc./desc. tree views
    * better navigation on dictionnary, better sort subplaces
    * revamp anc/desc/parente forms/menu
    * highlight missing data on tables

--- hd/etc/js/copylink.js
+++ hd/etc/js/copylink.js
@@ -23,6 +23,14 @@ function copyToClipboard(text) {
     var simpledata = simplecopybtn.dataset.wikilink;
     var fullcopybtn = document.querySelector(".full-copy");
     var fulldata = fullcopybtn.dataset.wikilink;
+    var permacopybtn = document.querySelector(".permalink-copy");
+    var permafcopybtn = document.querySelector(".permalink-friend-copy");
+    var permawcopybtn = document.querySelector(".permalink-wizard-copy");
+    var permadata = permacopybtn.dataset.wikilink;
+    var cururl = window.location;
+    var permaurl = cururl.origin + "/" + permadata;
+    var permaurlf = permaurl + "&w=f";
+    var permaurlw = permaurl + "&w=w";
 
     simplecopybtn.addEventListener("click", function(event) {
     var result = copyToClipboard(simpledata);
@@ -31,5 +39,20 @@ function copyToClipboard(text) {
 
     fullcopybtn.addEventListener("click", function(event) {
     var result = copyToClipboard(fulldata);
+    console.log("copied?", result);
+});
+
+    permacopybtn.addEventListener("click", function(event) {
+    var result = copyToClipboard(permaurl);
+    console.log("copied?", result);
+});
+
+    permafcopybtn.addEventListener("click", function(event) {
+    var result = copyToClipboard(permaurlf);
+    console.log("copied?", result);
+});
+
+    permawcopybtn.addEventListener("click", function(event) {
+    var result = copyToClipboard(permaurlw);
     console.log("copied?", result);
 });
