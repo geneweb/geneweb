b042ef623b9fe7a891c9bcb5fcbfea4af177c6f5
Elie Canonici Merle <elie.canonicimerle@geneanet.org>
2022-09-29 16:40:38 +0200

    working person update with witness notes

--- lib/updateInd.ml
+++ lib/updateInd.ml
@@ -34,6 +34,7 @@ let bool_val = Update_util.bool_val
 let str_val = Update_util.str_val
 let safe_val = Update_util.safe_val
 
+
 let rec eval_var conf base env p _loc sl =
   try eval_special_var conf base sl
   with Not_found -> eval_simple_var conf base env p sl
@@ -317,34 +318,61 @@ and eval_simple_var conf base env p = function
               | _ -> raise Not_found)
           | None -> raise Not_found)
       | _ -> raise Not_found)
-  | [ "witness_kind" ] -> (
-      match get_env "cnt" env with
-      | Vint i -> (
-          let e =
-            try Some (List.nth p.pevents (i - 1)) with Failure _ -> None
-          in
-          match e with
-          | Some e -> (
-              match get_env "wcnt" env with
-              | Vint i ->
-                  let i = i - 1 in
-                  if i >= 0 && i < Array.length e.epers_witnesses then
-                    let _, wk, _ = e.epers_witnesses.(i) in 
-                    match wk with
-                      Witness_GodParent        -> str_val "godp"
-                    | Witness_CivilOfficer     -> str_val "offi"
-                    | Witness_ReligiousOfficer -> str_val "reli"
-                    | Witness_Informant -> str_val "info"
-                    | Witness_Attending -> str_val "atte"
-                    | Witness_Mentioned -> str_val "ment"
-                    | Witness_Other -> str_val "othe"
-                    | Witness -> str_val ""
-                  else if i >= 0 && i < 2 && Array.length e.epers_witnesses < 2
-                  then str_val ""
-                  else raise Not_found
-              | _ -> raise Not_found)
-          | None -> raise Not_found)
-      | _ -> raise Not_found)
+  | ["witness_note"] ->
+     begin match get_env "cnt" env with
+       Vint i ->
+        let e =
+          try Some (List.nth p.pevents (i - 1)) with Failure _ -> None
+        in
+        begin match e with
+          Some e ->
+           begin match get_env "wcnt" env with
+             Vint i ->
+              let i = i - 1 in
+              if i >= 0 && i < Array.length e.epers_witnesses then
+                let _, _, wnote = e.epers_witnesses.(i) in 
+                str_val (Util.escape_html wnote)
+              else if
+                i >= 0 && i < 2 && Array.length e.epers_witnesses < 2
+              then
+                str_val ""
+              else raise Not_found
+           | _ -> raise Not_found
+           end
+        | None -> raise Not_found
+        end
+     | _ -> raise Not_found
+     end
+  | ["witness_kind"] ->
+    (match get_env "cnt" env with
+     | Vint i ->
+       let e =
+         try Some (List.nth p.pevents (i - 1)) with Failure _ -> None
+       in
+       (match e with
+       | Some e -> (
+           match get_env "wcnt" env with
+           | Vint i ->
+             let i = i - 1 in
+             if i >= 0 && i < Array.length e.epers_witnesses then
+               let _, wk, _ = e.epers_witnesses.(i) in 
+               match wk with
+                 Witness_GodParent        -> str_val "godp"
+               | Witness_CivilOfficer     -> str_val "offi"
+               | Witness_ReligiousOfficer -> str_val "reli"
+               | Witness_Informant -> str_val "info"
+               | Witness_Attending -> str_val "atte"
+               | Witness_Mentioned -> str_val "ment"
+               | Witness_Other -> str_val "othe"
+               | Witness -> str_val ""
+             else if i >= 0 && i < 2 && Array.length e.epers_witnesses < 2
+             then str_val ""
+             else raise Not_found
+           | _ -> raise Not_found
+         )
+       | None -> raise Not_found)
+     | _ -> raise Not_found
+    )
   | [ s ] -> Update_util.eval_default_var conf s
   | _ -> raise Not_found
 
