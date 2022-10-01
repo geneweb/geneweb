fc522fc3df79617f7da4c057b1088b18f94e6af7
Henri83 <henri.gouraud@laposte.net>
2022-10-01 15:05:56 +0200

    Install m=PPS

--- bin/gwd/request.ml
+++ bin/gwd/request.ml
@@ -579,6 +579,9 @@ let treat_request =
           w_wizard @@ w_lock @@ w_base @@ UpdateFamOk.print_inv
         | "KILL_ANC" ->
           w_wizard @@ w_lock @@ w_base @@ MergeIndDisplay.print_kill_ancestors
+        | "L" ->
+          w_base @@ fun conf base ->
+              Perso.interp_templ "list" conf base (Gwdb.empty_person base Gwdb.dummy_iper)
         | "LB" when conf.wizard || conf.friend ->
           w_base @@ BirthDeathDisplay.print_birth
         | "LD" when conf.wizard || conf.friend ->
@@ -704,6 +707,8 @@ let treat_request =
           w_base @@ BirthDeathDisplay.print_population_pyramid
         | "PS" ->
           w_base @@ PlaceDisplay.print_all_places_surnames
+        | "PPS" ->
+          w_base @@ Place_v7.print_all_places_surnames
         | "R" ->
           w_base @@ w_person @@ relation_print
         | "REQUEST" ->
