c6c98176304fcdd831ceaf88f2e4227e2dcd1e20
Henri83 <henri.gouraud@laposte.net>
2022-12-16 16:51:54 +0100

    Isolate ascendDisplay.ml; fanchart (m=FC)

--- lib/perso.ml
+++ lib/perso.ml
@@ -5011,58 +5011,6 @@ let print ?no_headers conf base p =
       Util.unauthorized conf src
   | Some _ | None -> interp_templ ?no_headers "perso" conf base p
 
-let limit_by_tree conf =
-  match
-    Option.map int_of_string (List.assoc_opt "max_anc_tree" conf.base_env)
-  with
-  | Some x -> max 1 x
-  | None -> 7
-
-let print_ancestors_dag conf base v p =
-  let v = min (limit_by_tree conf) v in
-  let set =
-    let rec loop set lev ip =
-      let set = Dag.Pset.add ip set in
-      if lev <= 1 then set
-      else
-        match get_parents (pget conf base ip) with
-        | Some ifam ->
-            let cpl = foi base ifam in
-            let set = loop set (lev - 1) (get_mother cpl) in
-            loop set (lev - 1) (get_father cpl)
-        | None -> set
-    in
-    loop Dag.Pset.empty v (get_iper p)
-  in
-  let elem_txt p = DagDisplay.Item (p, Adef.safe "") in
-  (* Récupère les options d'affichage. *)
-  let options = Util.display_options conf in
-  let vbar_txt ip =
-    let p = pget conf base ip in
-    commd conf ^^^ "m=A&t=T&dag=on&v=" ^<^ string_of_int v ^<^ "&" ^<^ options
-    ^^^ "&" ^<^ acces conf base p
-  in
-  let page_title =
-    Util.transl conf "tree" |> Utf8.capitalize_fst |> Adef.safe
-  in
-  DagDisplay.make_and_print_dag conf base elem_txt vbar_txt true set []
-    page_title (Adef.escaped "")
-
-let print_ascend conf base p =
-  match
-    (p_getenv conf.env "t", p_getenv conf.env "dag", p_getint conf.env "v")
-  with
-  | Some "T", Some "on", Some v -> print_ancestors_dag conf base v p
-  | _ ->
-      let templ =
-        match p_getenv conf.env "t" with
-        | Some ("E" | "F" | "H" | "L") -> "anclist"
-        | Some ("D" | "G" | "M" | "N" | "P" | "X" | "Y" | "Z") -> "ancsosa"
-        | Some ("A" | "C" | "T") -> "anctree"
-        | _ -> "ancmenu"
-      in
-      interp_templ templ conf base p
-
 let print_what_links conf base p =
   if authorized_age conf base p then (
     let key =
