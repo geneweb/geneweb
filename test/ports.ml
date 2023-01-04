827bd78ab8d3de86493691804cf7203adeda2262
swrup <olivier.pierre@geneanet.org>
2023-01-04 16:19:34 +0100

    add abc.ged tests

--- test/ports.ml
+++ test/ports.ml
@@ -1,4 +1,4 @@
-(* test for import/export of .ged and .gw *)
+(* tests for import/export of .ged and .gw *)
 
 let original_ged = Filename.concat "assets" "royal92.ged"
 let exported_ged = Filename.concat "assets" "royal92_exported.ged"
@@ -72,3 +72,93 @@ let () = Gwdb.close_base base_3
    let () = assert (nb_persons_1 = nb_persons_3)
 *)
 let () = assert (nb_families_1 = nb_families_3)
+
+(* -- test abc.ged -- *)
+
+(* test on a base made from a ged import -> gw export -> gw import *)
+let base =
+  let base = ged_import (Filename.concat "assets" "abc.ged") in
+  gw_export base "abc.gw";
+  gw_import "abc.gw"
+
+let () = assert (Gwdb.nb_of_persons base = 3)
+
+let a =
+  match Gwdb.person_of_key base "a" "A" 0 with
+  | None -> failwith {|person "a A" not found|}
+  | Some iper -> Gwdb.poi base iper
+
+(* check birth event *)
+let () =
+  (match Date.cdate_to_dmy_opt (Gwdb.get_birth a) with
+  | None -> failwith {|no birth date for person "a A"|}
+  | Some dmy ->
+      assert (
+        Date.compare_dmy dmy
+          { day = 1; month = 1; year = 1990; prec = Def.Sure; delta = 0 }
+        = 0));
+  assert (
+    Gwdb.sou base (Gwdb.get_birth_note a) = "This is a note on a birth event");
+  assert (
+    Gwdb.sou base (Gwdb.get_birth_src a) = "This is a source on a birth event");
+  assert (Gwdb.sou base (Gwdb.get_birth_place a) = "Paris")
+
+(* check family marriage event *)
+let () =
+  let fam = Gwdb.foi base (Gwdb.get_family a).(0) in
+  assert (Gwdb.sou base (Gwdb.get_comment fam) = "This is a comment on a family");
+  let fevents = Gwdb.get_fevents fam in
+  let marriage =
+    match
+      List.filter (fun event -> event.Def.efam_name = Def.Efam_Marriage) fevents
+    with
+    | [] -> failwith "no Efam_Marriage"
+    | e :: [] -> e
+    | _l -> failwith "duplicate Efam_Marriage"
+  in
+  (match Date.cdate_to_dmy_opt marriage.efam_date with
+  | None -> failwith "no marriage date"
+  | Some dmy ->
+      assert (
+        Date.compare_dmy dmy
+          { day = 18; month = 05; year = 2013; prec = Def.Sure; delta = 0 }
+        = 0));
+
+  assert (
+    Gwdb.sou base marriage.efam_note = "This is a note on a marriage event");
+  assert (
+    Gwdb.sou base marriage.efam_src = "This is a source on a marriage event");
+  assert (Gwdb.sou base marriage.efam_place = "Lyon");
+  (* TODO check witness note here *)
+  let witness, witness_kind =
+    assert (Array.length marriage.efam_witnesses = 1);
+    let w, wk = marriage.efam_witnesses.(0) in
+    (Gwdb.poi base w, wk)
+  in
+  assert (Gwdb.sou base (Gwdb.get_first_name witness) = "c");
+  assert (Gwdb.sou base (Gwdb.get_surname witness) = "C");
+  assert (witness_kind = Def.Witness_Attending)
+
+(* check Degree event *)
+let () =
+  let pevents = Gwdb.get_pevents a in
+  let diploma =
+    match
+      List.filter
+        (fun event -> event.Def.epers_name = Def.Epers_Diploma)
+        pevents
+    with
+    | [] -> failwith "no Epers_Diploma"
+    | e :: [] -> e
+    | _l -> failwith "duplicate Epers_Diploma"
+  in
+  assert (Gwdb.sou base diploma.epers_note = "This is a note on a diploma event");
+  assert (
+    Gwdb.sou base diploma.epers_src = "This is a source on a diploma event");
+  (* TODO WNOTE *)
+  let _witness, witness_kind =
+    let witnesses = diploma.epers_witnesses in
+    assert (Array.length witnesses = 1);
+    witnesses.(0)
+  in
+  assert (witness_kind = Def.Witness)
