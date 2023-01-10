0cc19ce2d2fc104bed483c46cd5089c9db6e7a12
Henri83 <henri.gouraud@laposte.net>
2023-01-10 21:24:18 +0100

    missing js code for query time display

--- lib/config.ml
+++ lib/config.ml
@@ -49,7 +49,7 @@ type config = {
   wizard : bool;
   is_printed_by_template : bool;
   debug : bool;
-  query_start: float;
+  query_start : float;
   friend : bool;
   just_friend_wizard : bool;
   user : string;
