a9db7be0c85d53059f398a79d6eb549160fbd4a8
Henri83 <henri.gouraud@laposte.net>
2023-01-17 18:34:41 +0100

    propose user:passwd capture if visitors_access=no

--- bin/gwd/request.ml
+++ bin/gwd/request.ml
@@ -769,10 +769,8 @@ let treat_request =
     Output.print_sstring conf conf.bname ;
     Output.print_sstring conf {|" |} ;
     transl conf "reserved to friends or wizards"
+    |> Utf8.capitalize_fst
     |> Output.print_sstring conf ;
-    let base_name =
-      if conf.cgi then (Printf.sprintf "b=%s&" conf.bname) else ""
-    in
     let user = transl_nth conf "user/password/cancel" 0 in
     let passwd = transl_nth conf "user/password/cancel" 1 in
     Output.print_sstring conf ".</li></ul>" ;
