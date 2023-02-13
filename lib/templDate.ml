c1d0d4911d32a1fcda53eb50d35dc72408bd67b0
Henri83 <henri.gouraud@laposte.net>
2023-02-13 10:00:16 +0100

    Suppress gregorian_day. Use sdn_of_gregorian in julian_day

--- lib/templDate.ml
+++ lib/templDate.ml
@@ -14,9 +14,6 @@ let rec eval_date_var conf jd = function
   | [ "julian_day" ] -> VVstring (string_of_int jd)
   | [ "julian_day"; "sep1000" ] ->
       VVstring (Mutil.string_of_int_sep (transl conf "(thousand separator)") jd)
-  | [ "gregorian_day" ] ->
-      let gd = Calendar.gregorian_of_sdn Sure jd in
-      VVstring (string_of_int (Calendar.sdn_of_gregorian gd))
   | [ "moon_age" ] -> (
       try
         let _, md = Calendar.moon_phase_of_sdn jd in
