e4dd6144f8ed9d22a5a33168fc4f294a009cddb0
Elie Canonici Merle <elie.canonicimerle@geneanet.org>
2023-03-09 15:01:56 +0100

    handle fpoi cache

--- lib/core/consangAll.ml
+++ lib/core/consangAll.ml
@@ -36,6 +36,7 @@ let consang_array base =
   (fget, cget, cset, patched)
 
 let compute ?(verbosity = 2) base from_scratch =
+  let () = Gwdb.set_fpoi_cache base false in
   let () = load_ascends_array base in
   let () = load_couples_array base in
   let fget, cget, cset, patched = consang_array base in
@@ -139,4 +140,5 @@ let compute ?(verbosity = 2) base from_scratch =
      flush stderr;
      ());
   if !patched then Gwdb.commit_patches base;
+  let () = Gwdb.set_fpoi_cache base true in
   !patched
