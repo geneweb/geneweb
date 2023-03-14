4691f9b199ea8f2441fa19e5deb580e66f8a1a25
swrup <olivier.pierre@geneanet.org>
2023-03-14 18:55:13 +0100

    fmt plugin_forum

--- plugins/forum/plugin_forum.ml
+++ plugins/forum/plugin_forum.ml
@@ -11,19 +11,23 @@ let wrapper fn conf base =
 
 let w_base =
   let none conf =
-    Hutil.incorrect_request conf ;
+    Hutil.incorrect_request conf;
     true
   in
   Gwd_lib.Request.w_base ~none
 
 let () =
   Gwd_lib.GwdPlugin.register ~ns
-    [ "FORUM", (fun _assets -> w_base @@ wrapper ForumDisplay.print)
-    ; "FORUM_ADD", (fun _assets -> w_base @@ wrapper ForumDisplay.print_add)
-    ; "FORUM_ADD_OK", (fun _assets -> w_base @@ wrapper ForumDisplay.print_add_ok)
-    ; "FORUM_DEL", (fun _assets -> w_base @@ wrapper ForumDisplay.print_del)
-    ; "FORUM_P_P", (fun _assets -> w_base @@ wrapper ForumDisplay.print_access_switch)
-    ; "FORUM_SEARCH", (fun _assets -> w_base @@ wrapper ForumDisplay.print_search)
-    ; "FORUM_VAL", (fun _assets -> w_base @@ wrapper ForumDisplay.print_valid)
-    ; "FORUM_VIEW", (fun _assets -> w_base @@ wrapper ForumDisplay.print)
+    [
+      ("FORUM", fun _assets -> w_base @@ wrapper ForumDisplay.print);
+      ("FORUM_ADD", fun _assets -> w_base @@ wrapper ForumDisplay.print_add);
+      ( "FORUM_ADD_OK",
+        fun _assets -> w_base @@ wrapper ForumDisplay.print_add_ok );
+      ("FORUM_DEL", fun _assets -> w_base @@ wrapper ForumDisplay.print_del);
+      ( "FORUM_P_P",
+        fun _assets -> w_base @@ wrapper ForumDisplay.print_access_switch );
+      ( "FORUM_SEARCH",
+        fun _assets -> w_base @@ wrapper ForumDisplay.print_search );
+      ("FORUM_VAL", fun _assets -> w_base @@ wrapper ForumDisplay.print_valid);
+      ("FORUM_VIEW", fun _assets -> w_base @@ wrapper ForumDisplay.print);
     ]
