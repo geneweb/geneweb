48a57444c6b7df099c9bcc06dcf948a4772dd43a
swrup <olivier.pierre@geneanet.org>
2023-06-02 17:07:27 +0200

    check has_access_to_images before getting them

--- lib/perso.ml
+++ lib/perso.ml
@@ -5070,13 +5070,14 @@ let print_foreach conf base print_ast eval_expr =
   in
   (* carrousel *)
   let print_foreach_img_in_keydir env al ((p, _p_auth) as ep) old =
-    let l =
-      List.sort String.compare
-        (if old then Image.get_keydir_old_files conf base p
-        else Image.get_keydir_files conf base p)
-    in
     if Image.has_access_to_images conf base p then
+      let l =
+        List.sort String.compare
+          (if old then Image.get_keydir_old_files conf base p
+          else Image.get_keydir_files conf base p)
+      in
       let rec loop first cnt = function
+        | [] -> ()
         | a :: l ->
             let env = ("keydir_img", Vstring a) :: env in
             let env = ("first", Vbool first) :: env in
@@ -5084,10 +5085,8 @@ let print_foreach conf base print_ast eval_expr =
             let env = ("img_cnt", Vint cnt) :: env in
             List.iter (print_ast env ep) al;
             loop false (cnt + 1) l
-        | [] -> ()
       in
       loop true 1 l
-    else ()
   in
   let print_simple_foreach env el al ini_ep ep efam loc = function
     | "alias" -> print_foreach_alias env al ep
