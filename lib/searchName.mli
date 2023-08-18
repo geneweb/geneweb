4b955e00dc914f7804924ff8fbe33c462871acf1
Henri83 <henri.gouraud@laposte.net>
2023-08-18 07:37:55 +0200

    partial_key: search_by_name then search (not exact) for surname, then for first_name or public_name

--- lib/searchName.mli
+++ lib/searchName.mli
@@ -17,9 +17,6 @@ val search_by_name : config -> base -> string -> person list
 (** Search persons by name that has format {i "firstname surname"}. Dublicates are possible. Empty persons, persons with private
     names or persons to which there are no rights to access are not listed. *)
 
-val search_partial_key : config -> base -> string -> person list
-(** Calls [search_key_aux] with [aux] fonction that makes calls to [search_by_name] if result is empty. *)
-
 val search_by_sosa : config -> base -> string -> person list
 
 val search_by_key : config -> base -> string -> person list
