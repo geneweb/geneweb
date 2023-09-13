3c05ac3030cb8c5c792041460316b6e380332fa2
swrup <olivier.pierre@geneanet.org>
2023-09-13 16:15:52 +0200

    update date tests

--- test/test.ml
+++ test/test.ml
@@ -2,6 +2,6 @@ let () =
   let open Alcotest in
   try
     run ~and_exit:false "Geneweb"
-      (Sosa_test.v @ Place_test.v @ Calendar_test.v @ Wiki_test.v @ Merge_test.v
+      (Sosa_test.v @ Place_test.v @ Date_test.v @ Wiki_test.v @ Merge_test.v
      @ Util_test.v)
   with Test_error -> ()
