a5a5b504fde392996a48b9357fc1105ec2a8fd42
Michel Normand <michel_mno@laposte.net>
2023-11-14 15:20:56 +0100

    Do not overwrite static_path by bvar if still present
    This is to complete the previous code changes
    about bvar.static_path superseded by GW_STATIC_PATH
    
    To cover following specific use-case:
    Use same database to test on same machine
    different versions of geneweb triggered by different cgi scripts,
    among which geneweb 7.0.0 that still need static_path from gwf,
    but the other newer versions using GW_STATIC_PATH from cgi scripts.
    
    Signed-off-by: Michel Normand <michel_mno@laposte.net>

--- lib/templ.ml
+++ lib/templ.ml
@@ -508,11 +508,7 @@ and eval_simple_variable conf = function
   | "sp" -> " "
   | "static_path" ->
       (let s =
-         if conf.cgi then
-           match List.assoc_opt "static_path" conf.base_env with
-           | Some x -> Adef.escaped x
-           | None -> Adef.escaped conf.static_path
-         else Adef.escaped ""
+         if conf.cgi then Adef.escaped conf.static_path else Adef.escaped ""
        in
        s
         :> string)
