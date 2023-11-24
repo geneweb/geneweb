7b7c73138e25fd09bc275e1a48aec3f78c291f59
A2 <a2line@gmail.com>
2023-11-24 07:42:25 +0100

    improve clean_html_tags

--- lib/util.mli
+++ lib/util.mli
@@ -38,7 +38,8 @@ val update_wf_trace : config -> string -> unit
 val get_referer : config -> Adef.escaped_string
 (** Get referer (the page you came from to the current page) page from HTTP request *)
 
-val clean_html_tags : string -> string list -> string
+val clean_html_tags : string -> string
+
 val clean_comment_tags : string -> string
 
 val html : ?content_type:string -> config -> unit
