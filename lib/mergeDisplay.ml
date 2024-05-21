3d96192ee77ffb31413a2a5387acf4bc40249956
Idir Lankri <idir.lankri@geneanet.org>
2024-05-21 12:37:52 +0200

    Cleanup: Dispose of some top-level `open`s

--- lib/mergeDisplay.ml
+++ lib/mergeDisplay.ml
@@ -1,30 +1,26 @@
 (* $Id: merge.ml, v7-exp 2018-09-26 07:34:44 ddr Exp $ *)
 (* Copyright (c) 1998-2007 INRIA *)
 
-open Config
-open Gwdb
-open Util
-
 let print_someone conf base p =
-  Output.printf conf "%s%s %s" (p_first_name base p)
-    (if get_occ p = 0 then "" else "." ^ string_of_int (get_occ p))
-    (p_surname base p)
+  Output.printf conf "%s%s %s" (Gwdb.p_first_name base p)
+    (if Gwdb.get_occ p = 0 then "" else "." ^ string_of_int (Gwdb.get_occ p))
+    (Gwdb.p_surname base p)
 
 let print conf base p =
   let title h =
     Output.print_sstring conf
-      (Utf8.capitalize_fst (transl_decline conf "merge" ""));
+      (Utf8.capitalize_fst (Util.transl_decline conf "merge" ""));
     if not h then (
       Output.print_sstring conf " ";
       print_someone conf base p;
       Output.print_sstring conf " ";
-      Output.print_sstring conf (transl_decline conf "with" "");
-      Output.print_sstring conf (transl conf ":"))
+      Output.print_sstring conf (Util.transl_decline conf "with" "");
+      Output.print_sstring conf (Util.transl conf ":"))
   in
   let list = Gutil.find_same_name base p in
   let list =
     List.fold_right
-      (fun p1 pl -> if get_iper p1 = get_iper p then pl else p1 :: pl)
+      (fun p1 pl -> if Gwdb.get_iper p1 = Gwdb.get_iper p then pl else p1 :: pl)
       list []
   in
   Perso.interp_notempl_with_menu title "perso_header" conf base p;
@@ -32,32 +28,33 @@ let print conf base p =
   title false;
   Output.print_sstring conf "</h2>";
   Output.print_sstring conf {|<form method="get" action="|};
-  Output.print_sstring conf conf.command;
+  Output.print_sstring conf conf.Config.command;
   Output.print_sstring conf {|"class="mx-3 mb-3">|};
   Util.hidden_env conf;
   Util.hidden_input conf "m" (Adef.encoded "MRG_IND");
-  Util.hidden_input conf "i" (get_iper p |> string_of_iper |> Mutil.encode);
+  Util.hidden_input conf "i"
+    (Gwdb.get_iper p |> Gwdb.string_of_iper |> Mutil.encode);
   Output.print_sstring conf
     "<span class=\"form-row align-items-center\"><span \
      class=\"col-auto\"><span class=\"custom-control custom-radio\"><input \
      type=\"radio\" class=\"custom-control-input\" name=\"select\" \
      id=\"input\" value=\"input\" checked><label \
      class=\"custom-control-label\"for=\"input\">";
-  Output.print_sstring conf (transl conf "any individual in the base");
+  Output.print_sstring conf (Util.transl conf "any individual in the base");
   Output.print_sstring conf
     "</label></span></span><span class=\"col-auto\"><input type=\"text\" \
      class=\"form-control\" name=\"n\" placeholder=\"";
-  Output.print_sstring conf (transl_nth conf "first name/first names" 0);
+  Output.print_sstring conf (Util.transl_nth conf "first name/first names" 0);
   Output.print_sstring conf ".";
-  Output.print_sstring conf (transl conf "number");
+  Output.print_sstring conf (Util.transl conf "number");
   Output.print_sstring conf " ";
-  Output.print_sstring conf (transl_nth conf "surname/surnames" 0);
+  Output.print_sstring conf (Util.transl_nth conf "surname/surnames" 0);
   Output.print_sstring conf "\" title=\"";
-  Output.print_sstring conf (transl_nth conf "first name/first names" 0);
+  Output.print_sstring conf (Util.transl_nth conf "first name/first names" 0);
   Output.print_sstring conf ".";
-  Output.print_sstring conf (transl conf "number");
+  Output.print_sstring conf (Util.transl conf "number");
   Output.print_sstring conf " ";
-  Output.print_sstring conf (transl_nth conf "surname/surnames" 0);
+  Output.print_sstring conf (Util.transl_nth conf "surname/surnames" 0);
   Output.print_sstring conf
     "\" size=\"50\" id=\"inlineinput\" autofocus></span></span>";
   if list <> [] then
@@ -67,77 +64,82 @@ let print conf base p =
         Output.print_sstring conf
           "<input type=\"radio\" class=\"custom-control-input\" \
            name=\"select\" id=\"";
-        Output.print_string conf (get_iper p |> string_of_iper |> Mutil.encode);
+        Output.print_string conf
+          (Gwdb.get_iper p |> Gwdb.string_of_iper |> Mutil.encode);
         Output.print_sstring conf "\" value=\"";
-        Output.print_string conf (get_iper p |> string_of_iper |> Mutil.encode);
+        Output.print_string conf
+          (Gwdb.get_iper p |> Gwdb.string_of_iper |> Mutil.encode);
         Output.print_sstring conf "\">\n";
         Output.print_sstring conf "<label class=\"custom-control-label\" for=\"";
-        Output.print_string conf (get_iper p |> string_of_iper |> Mutil.encode);
+        Output.print_string conf
+          (Gwdb.get_iper p |> Gwdb.string_of_iper |> Mutil.encode);
         Output.print_sstring conf "\">";
         Output.print_sstring conf "</label></div>")
       list;
   Output.print_sstring conf
     {|<button type="submit" class="btn btn-primary btn-lg mt-2">|};
   Output.print_sstring conf
-    (Utf8.capitalize_fst (transl_nth conf "validate/delete" 0));
+    (Utf8.capitalize_fst (Util.transl_nth conf "validate/delete" 0));
   Output.print_sstring conf "</button></form>\n";
   Hutil.trailer conf
 
 let print_possible_continue_merging conf base =
   let open Adef in
-  match (p_getenv conf.env "ini1", p_getenv conf.env "ini2") with
+  match
+    (Util.p_getenv conf.Config.env "ini1", Util.p_getenv conf.Config.env "ini2")
+  with
   | Some ini1, Some ini2 ->
-      let ini1 = iper_of_string ini1 in
-      let ini2 = iper_of_string ini2 in
-      let p1 = poi base ini1 in
-      let p2 = poi base ini2 in
+      let ini1 = Gwdb.iper_of_string ini1 in
+      let ini2 = Gwdb.iper_of_string ini2 in
+      let p1 = Gwdb.poi base ini1 in
+      let p2 = Gwdb.poi base ini2 in
       Output.print_sstring conf {|<p><a href="|};
-      Output.print_string conf (commd conf);
+      Output.print_string conf (Util.commd conf);
       Output.print_sstring conf {|m=MRG_IND&i=|};
-      Output.print_string conf (string_of_iper ini1 |> Mutil.encode);
+      Output.print_string conf (Gwdb.string_of_iper ini1 |> Mutil.encode);
       Output.print_sstring conf {|&i2=|};
-      Output.print_string conf (string_of_iper ini2 |> Mutil.encode);
+      Output.print_string conf (Gwdb.string_of_iper ini2 |> Mutil.encode);
       Output.print_sstring conf {|">|};
       Output.print_sstring conf
-        (Utf8.capitalize_fst (transl conf "continue merging"));
+        (Utf8.capitalize_fst (Util.transl conf "continue merging"));
       Output.print_sstring conf {|</a> |};
       print_someone conf base p1;
       Output.print_sstring conf " ";
-      Output.print_sstring conf (transl_nth conf "and" 0);
+      Output.print_sstring conf (Util.transl_nth conf "and" 0);
       Output.print_sstring conf " ";
       print_someone conf base p2;
       Output.print_sstring conf "</p>"
   | _ -> (
-      match p_getenv conf.env "ip" with
+      match Util.p_getenv conf.Config.env "ip" with
       | Some ip ->
-          let ip = iper_of_string ip in
+          let ip = Gwdb.iper_of_string ip in
           let s1 =
-            match p_getenv conf.env "iexcl" with
+            match Util.p_getenv conf.Config.env "iexcl" with
             | Some "" | None -> Adef.encoded ""
             | Some s -> "&iexcl=" ^<^ Mutil.encode s
           in
           let s2 =
-            match p_getenv conf.env "fexcl" with
+            match Util.p_getenv conf.Config.env "fexcl" with
             | Some "" | None -> Adef.encoded ""
             | Some s -> "&fexcl=" ^<^ Mutil.encode s
           in
           if s1 <^> Adef.encoded "" || s2 <^> Adef.encoded "" then (
-            let p = poi base ip in
-            let s = gen_person_text conf base p in
+            let p = Gwdb.poi base ip in
+            let s = Util.gen_person_text conf base p in
             Output.print_sstring conf {|<p><a href="|};
-            Output.print_string conf (commd conf);
+            Output.print_string conf (Util.commd conf);
             Output.print_sstring conf {|m=MRG_DUP&ip=|};
-            Output.print_string conf (string_of_iper ip |> Mutil.encode);
+            Output.print_string conf (Gwdb.string_of_iper ip |> Mutil.encode);
             Output.print_string conf s1;
             Output.print_string conf s2;
             Output.print_sstring conf {|">|};
             Output.print_sstring conf
-              (Utf8.capitalize_fst (transl conf "continue merging"));
+              (Utf8.capitalize_fst (Util.transl conf "continue merging"));
             Output.print_sstring conf {| (|};
             Output.print_sstring conf
-              (transl_a_of_b conf
-                 (transl conf "possible duplications")
-                 (reference conf base p s :> string)
+              (Util.transl_a_of_b conf
+                 (Util.transl conf "possible duplications")
+                 (Util.reference conf base p s :> string)
                  (s :> string));
             Output.print_sstring conf {|)</p>|})
       | None -> ())
--- lib/mergeDisplay.mli
+++ lib/mergeDisplay.mli
@@ -1,14 +1,11 @@
 (* $Id: merge.mli,v 5.5 2007-01-19 01:53:16 ddr Exp $ *)
 (* Copyright (c) 1998-2007 INRIA *)
 
-open Gwdb
-open Config
-
-val print_someone : config -> base -> person -> unit
+val print_someone : Config.config -> Gwdb.base -> Gwdb.person -> unit
 (** Prints person's key on the socket *)
 
-val print : config -> base -> person -> unit
+val print : Config.config -> Gwdb.base -> Gwdb.person -> unit
 (** Displays a menu for merging two persons *)
 
-val print_possible_continue_merging : config -> base -> unit
+val print_possible_continue_merging : Config.config -> Gwdb.base -> unit
 (** Prints link on the page to continue merging two persons (or two duplications). *)
