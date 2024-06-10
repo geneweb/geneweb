245aa72e314f78bc40603cc37588bd5963d3b5f8
Michel Normand <michel_mno@laposte.net>
2024-06-10 09:29:13 +0200

    Move back calendars library from external
    * initially moved out by cid 25b1407a
      commit 25b1407a80a5327f561a98ce39db679dfe55c35a (tag: Geneweb-25b1407a)
      Author: Julien Sagot <julien.sagot@geneanet.org>
      Date:   Thu Oct 31 07:59:29 2019 +0100
      Exported Calendar module as external lib
    
    * I retrieved back inside because external library is not maintain in
      https://github.com/geneweb/calendars.git
      I retrieved the version 1.0.0 that is master branch cid 584fb84
      For tracking purpose here is the list of commits up to this version
      * 584fb84 Fixed dune version in opam file
      * 3382a99 Added first tests
      * 8841e50 Initialized project with code adapted from GeneWeb
    
    * I only grabbed the two calendars.ml* files not the test/* file
      to avoid to adapt to new test engine.
      (subject to a potential futur commit if needed)
    
    * Note that I voluntarily did not change source files
      even if badly formatted, to ease code comparison from calendars.git
      The next commit will do those format changes.
    
    Signed-off-by: Michel Normand <michel_mno@laposte.net>

---
+++ lib/util/calendars.mli
@@ -0,0 +1,22 @@
+type d = { day : int ; month : int ; year : int ; delta : int }
+
+val gregorian_of_sdn : int -> d
+val julian_of_sdn : int -> d
+val french_of_sdn : int -> d
+val hebrew_of_sdn : int -> d
+
+val sdn_of_gregorian : d -> int
+val sdn_of_julian : d -> int
+val sdn_of_french : d -> int
+val sdn_of_hebrew : d -> int
+
+val gregorian_of_julian : d -> d
+val julian_of_gregorian : d -> d
+val gregorian_of_french : d -> d
+val french_of_gregorian : d -> d
+val gregorian_of_hebrew : d -> d
+val hebrew_of_gregorian : d -> d
+
+type moon_phase = NewMoon | FirstQuarter | FullMoon | LastQuarter
+
+val moon_phase_of_sdn : int -> (moon_phase * int * int) option * int
