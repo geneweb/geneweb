e5a5430daf4366354cc07c056d17e0ce47488263
Michel Normand <michel_mno@laposte.net>
2024-06-10 09:40:00 +0200

    fmt changes for lib/util/calendars.ml*
    Signed-off-by: Michel Normand <michel_mno@laposte.net>

--- lib/util/calendars.mli
+++ lib/util/calendars.mli
@@ -1,15 +1,13 @@
-type d = { day : int ; month : int ; year : int ; delta : int }
+type d = { day : int; month : int; year : int; delta : int }
 
 val gregorian_of_sdn : int -> d
 val julian_of_sdn : int -> d
 val french_of_sdn : int -> d
 val hebrew_of_sdn : int -> d
-
 val sdn_of_gregorian : d -> int
 val sdn_of_julian : d -> int
 val sdn_of_french : d -> int
 val sdn_of_hebrew : d -> int
-
 val gregorian_of_julian : d -> d
 val julian_of_gregorian : d -> d
 val gregorian_of_french : d -> d
