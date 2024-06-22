df7629b79c980ebcdb4789bd55164ce5ffefc1ce
Michel Normand <michel_mno@laposte.net>
2024-06-22 18:56:51 +0200

    ged2gwb properly aline online help
    with correct setting of doc strings in speclist variable.
    * Need to be a one line string, not a multi-line.
    * Add a space first character as per Arg.align.
    
    Refer to Arg module documentation https://ocaml.org/manual/5.2/api/Arg.html
    
    Signed-off-by: Michel Normand <michel_mno@laposte.net>

--- bin/ged2gwb/ged2gwb.ml
+++ bin/ged2gwb/ged2gwb.ml
@@ -3183,18 +3183,18 @@ let speclist =
   [ ( "-o", Arg.String (fun s -> out_file := s)
     , "<file> Output database (default: \"a\")." )
   ; ( "-f", Arg.Set force
-    , "Remove database if already existing" )
+    , " Remove database if already existing" )
   ; ( "-log", Arg.String (fun s -> log_oc := open_out s)
     , "<file> Redirect log trace to this file." )
   ; ( "-lf", Arg.Set lowercase_first_names
-    , "Convert first names to lowercase letters, with initials in uppercase." )
+    , " Convert first names to lowercase letters, with initials in uppercase." )
   ; ( "-trackid", Arg.Set track_ged2gw_id,
-      "Print gedcom id to gw id matches." )
+      " Print gedcom id to gw id matches." )
   ; ( "-ls", Arg.Unit (fun () -> case_surnames := LowerCase)
-    , "Convert surnames to lowercase letters, with initials in uppercase. \
+    , " Convert surnames to lowercase letters, with initials in uppercase. \
        Try to keep lowercase particles." )
   ; ( "-us", Arg.Unit (fun () -> case_surnames := UpperCase)
-    , "Convert surnames to uppercase letters." )
+    , " Convert surnames to uppercase letters." )
   ; ( "-fne"
     , Arg.String begin fun s ->
         if String.length s = 2
@@ -3206,29 +3206,30 @@ let speclist =
        is considered to be the usual first name: e.g. -fne '\"\"' or \
        -fne \"()\"." )
   ; ( "-efn", Arg.Set extract_first_names
-    , "When creating a person, if the GEDCOM first name part holds several \
+    , " When creating a person, if the GEDCOM first name part holds several \
        names, the first of this names becomes the person \"first name\" and \
        the complete GEDCOM first name part a \"first name alias\"." )
   ; ( "-no_efn", Arg.Clear extract_first_names,
-      "Cancels the previous option." )
+      " Cancels the previous option." )
   ; ( "-epn", Arg.Set extract_public_names
-    , "When creating a person, if the GEDCOM first name part looks like a \
-       public name, i.e. holds:\n\
-       * a number or a roman number, supposed to be a number of a nobility title,\n\
-       * one of the words: \"der\", \"den\", \"die\", \"el\", \"le\", \"la\", \"the\", \
+    , " When creating a person, if the GEDCOM first name part looks like a \
+       public name, i.e. holds either \
+       a number or a roman number, supposed to be a number of a nobility title, \
+       or one of the words: \
+       \"der\", \"den\", \"die\", \"el\", \"le\", \"la\", \"the\", \
        supposed to be the beginning of a qualifier, \
        then the GEDCOM first name part becomes the person \"public name\" \
        and its first word his \"first name\"." )
   ; ( "-no_epn", Arg.Clear extract_public_names
-    , "Cancels the previous option." )
+    , " Cancels the previous option." )
   ; ( "-no_pit", Arg.Set no_public_if_titles
-    , "Do not consider persons having titles as public")
+    , " Do not consider persons having titles as public")
   ; ( "-tnd", Arg.Set try_negative_dates
-    , "Set negative dates when inconsistency (e.g. birth after death)" )
+    , " Set negative dates when inconsistency (e.g. birth after death)" )
   ; ( "-no_nd", Arg.Set no_negative_dates
-    , "Don't interpret a year preceded by a minus sign as a negative year" )
-  ; ( "-nc", Arg.Clear do_check, "No consistency check" )
-  ; ( "-nopicture", Arg.Set no_picture, "Don't extract individual picture." )
+    , " Don't interpret a year preceded by a minus sign as a negative year" )
+  ; ( "-nc", Arg.Clear do_check, " No consistency check" )
+  ; ( "-nopicture", Arg.Set no_picture, " Don't extract individual picture." )
   ; ( "-udi"
     , Arg.String begin fun s ->
         match String.index_opt s '-' with
@@ -3241,21 +3242,22 @@ let speclist =
           dead_years := b ;
         | None -> raise (Arg.Bad "bad parameter for -udi")
       end
-    , "x-y Set the interval for persons whose death part is undefined:\n\
-       - if before x years, they are considered as alive\n\
-       - if after y year, they are considered as death\n\
-       - between x and y year, they are considered as \"don't know\"\n\
-       Default x is " ^ string_of_int !alive_years ^ " and y is " ^ string_of_int !dead_years)
+    , "x-y Set the interval for persons whose death part is undefined: \
+       If before x years, they are considered as alive. \
+       If after y year, they are considered as death. \
+       Between x and y year, they are considered as \"don't know\". \
+       Default x is " ^ string_of_int !alive_years ^ " \
+       Default y is " ^ string_of_int !dead_years)
   ; ( "-uin", Arg.Set untreated_in_notes
-    , "Put untreated GEDCOM tags in notes" )
+    , " Put untreated GEDCOM tags in notes" )
   ; ( "-ds", Arg.Set_string default_source
-    , "Set the source field for persons and families without source data" )
+    , " Set the source field for persons and families without source data" )
   ; ( "-dates_dm", Arg.Unit (fun () -> month_number_dates := DayMonthDates)
-    ,"Interpret months-numbered dates as day/month/year" )
+    ," Interpret months-numbered dates as day/month/year" )
   ; ( "-dates_md", Arg.Unit (fun () -> month_number_dates := MonthDayDates)
-    , "Interpret months-numbered dates as month/day/year" )
+    , " Interpret months-numbered dates as month/day/year" )
   ; ( "-rs_no_mention", Arg.Unit (fun () -> relation_status := NoMention)
-    , "Force relation status to NoMention (default is Married)" )
+    , " Force relation status to NoMention (default is Married)" )
   ; ( "-charset"
     , Arg.String begin function
         | "ANSEL" -> charset_option := Some Ansel
@@ -3263,7 +3265,7 @@ let speclist =
         | "MSDOS" -> charset_option := Some Msdos
         | _ -> raise (Arg.Bad "bad -charset value")
       end
-    , "[ANSEL|ASCII|MSDOS] Force given charset decoding, \
+    , " [ANSEL|ASCII|MSDOS] Force given charset decoding, \
        overriding the possible setting in GEDCOM" )
   ; ( "-particles"
     , Arg.String (fun s -> particles := Mutil.input_particles s)
