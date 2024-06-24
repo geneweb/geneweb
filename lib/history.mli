c5995d770af7347c66ace3e630b3d050e6cbf5a2
Elie Canonici Merle <elie.canonicimerle@geneanet.org>
2024-06-24 16:53:09 +0200

    history: filter_map_history

--- lib/history.mli
+++ lib/history.mli
@@ -38,3 +38,18 @@ val print_search : config -> base -> unit
 
 val line_fields : string -> (string * string * string * string option) option
 (** Parses one line of history file that delimits one modification record. *)
+
+val filter_map_history :
+  conf:config ->
+  skip:int ->
+  n:int ->
+  filter:
+    (time:string -> user:string -> action:string -> keyo:string option -> bool) ->
+  f:(time:string -> user:string -> action:string -> keyo:string option -> 'a) ->
+  'a list
+
+val total_entries :
+  conf:config ->
+  filter:
+    (time:string -> user:string -> action:string -> keyo:string option -> bool) ->
+  int
