02856ad286c10a89d8f366c567a213e764bcc138
Elie Canonici Merle <elie.canonicimerle@geneanet.org>
2024-07-22 10:43:26 +0200

    test_place => place_test with alcotest

--- test/test.ml
+++ test/test.ml
@@ -4,10 +4,10 @@ let _ =
   run_test_tt_main
     ("Geneweb"
     >::: [] @ Test_calendar.suite @ Test_chronology.suite
-         @ Test_place.suite @ Test_sosa.suite @ Test_utils.suite
+         @ Test_sosa.suite @ Test_utils.suite
          @ Test_wiki.suite)
 
 let () =
   try
-    Alcotest.run ~and_exit:false "Geneweb" []
+    Alcotest.run ~and_exit:false "Geneweb" Place_test.v
   with Alcotest.Test_error -> ()
