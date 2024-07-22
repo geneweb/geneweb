162a77cb8e134795bc9f4636630c1ff4d2f34275
Elie Canonici Merle <elie.canonicimerle@geneanet.org>
2024-07-22 12:01:21 +0200

    test_ports => ports_test with alcotest

--- test/test.ml
+++ test/test.ml
@@ -1,8 +1,7 @@
-
 let () =
   let test_suite =
     Place_test.v @ Date_test.v @ Sosa_test.v @ Wiki_test.v @ Util_test.v
+    @ Ports_test.v
   in
-  try
-    Alcotest.run ~and_exit:false "Geneweb" test_suite
+  try Alcotest.run ~and_exit:false "Geneweb" test_suite
   with Alcotest.Test_error -> ()
