4315960f684ba4a8e55dbf5372aad9f9cf57d98a
Henri83 <henri.gouraud@laposte.net>
2024-07-22 14:31:41 +0200

    rewrite cache_files for autocompletion datalists, add gzip compression support
    * New datalist caches for the new books (titles, domains, public_names, qualifiers and aliases).
    * New “-bd” option for cache_files tool.
    * Remove all <option> HTML tags in the cache files to reduce their size by nearly 50% (A2).
    * New Opam dependancy "camlzip" added to compress the cache files with gzip (A2).
    * Format the “cache_file.exe” report log as a table (A2+hg).

--- bin/cache_files/cache_files.ml
+++ bin/cache_files/cache_files.ml
@@ -1,33 +1,65 @@
-open Geneweb
 open Gwdb
+open Def
 
 let bname = ref ""
 let trace = ref false
 let fnames = ref false
-let places = ref false
-let fname_alias = ref false
 let snames = ref false
 let alias = ref false
-let qual = ref false
+let pub_names = ref false
+let fname_alias = ref false
+let sname_alias = ref false
+let places = ref false
+let estates = ref false
+let titles = ref false
+let occupations = ref false
+let qualifiers = ref false
+let sources = ref false
 let all = ref false
 let prog = ref false
+let width = ref 50
+
+(* Attention: cache files are reorg independant *)
+let set_cache_dir bname =
+  String.concat Filename.dir_sep [ Secure.base_dir (); "etc"; bname; "cache" ]
+
+let cache_dir = ref ""
 
 let write_cache_file bname fname list =
-  let bname =
-    if Filename.check_suffix bname ".gwb" then Filename.remove_extension bname
-    else bname
-  in
-  let fname =
-    Filename.concat
-      (Util.base_path [] (bname ^ ".gwb"))
-      (bname ^ "_" ^ fname ^ "_cache.txt")
-  in
-  Printf.printf "Write to : %s\n" fname;
-  match try Some (Secure.open_out fname) with Sys_error _ -> None with
-  | Some oc ->
-      List.iter (fun (v, _) -> output_string oc ("<option>" ^ v ^ "\n")) list;
-      close_out oc
-  | None -> ()
+  let filename = bname ^ "_" ^ fname ^ ".cache" in
+  let file = Filename.concat !cache_dir filename in
+  let gz_file = file ^ ".gz" in
+  try
+    let temp_file = Filename.temp_file "temp_" ".cache" in
+    let oc = Stdlib.open_out temp_file in
+    List.iter (fun (v, _) -> Stdlib.output_string oc (v ^ "\n")) list;
+    Stdlib.close_out oc;
+    (* Using camlzip to compress the file *)
+    let in_channel = Stdlib.open_in_bin temp_file in
+    let out_channel = Gzip.open_out gz_file in
+    try
+      let buffer = Bytes.create 8192 in
+      let rec copy_contents () =
+        let bytes_read =
+          Stdlib.input in_channel buffer 0 (Bytes.length buffer)
+        in
+        if bytes_read > 0 then (
+          Gzip.output out_channel buffer 0 bytes_read;
+          copy_contents ())
+      in
+      copy_contents ();
+      Stdlib.close_in in_channel;
+      Gzip.close_out out_channel;
+      Sys.remove temp_file
+    with exn ->
+      Stdlib.close_in_noerr in_channel;
+      Gzip.close_out out_channel;
+      Sys.remove temp_file;
+      raise exn
+  with exn ->
+    Printf.printf "Debug: Exception occurred: %s\n" (Printexc.to_string exn);
+    Printf.printf "Debug: Stack trace:\n%s\n" (Printexc.get_backtrace ());
+    raise exn
 
 let places_all base bname fname =
   let start = Unix.gettimeofday () in
@@ -42,7 +74,7 @@ let places_all base bname fname =
   in
   let len = nb_of_persons base in
   if !prog then (
-    Printf.printf "Places\n";
+    Printf.printf "\nplaces\n";
     flush stdout;
     ProgrBar.full := '*';
     ProgrBar.start ());
@@ -81,27 +113,26 @@ let places_all base bname fname =
     (Gwdb.ifams base);
 
   if !prog then ProgrBar.finish ();
-  flush stderr;
   let places_list = Hashtbl.fold (fun _k v acc -> (v, 1) :: acc) ht [] in
   let places_list =
     List.sort (fun (v1, _) (v2, _) -> Gutil.alphabetic_utf_8 v1 v2) places_list
   in
   write_cache_file bname fname places_list;
-  flush stderr;
   let stop = Unix.gettimeofday () in
-  Printf.printf "Number of places: %d\n" (List.length places_list);
-  Printf.printf "Execution time: %fs\n" (stop -. start);
+  let full_name =
+    Filename.concat !cache_dir (bname ^ "_" ^ fname ^ ".cache.gz")
+  in
+  Format.printf "@[<h>%-*s@ %8d@ %-14s@ %6.2f s@]@," !width full_name
+    (List.length places_list) "places" (stop -. start);
   flush stderr
 
-let names_all base bname fname =
-  let fn = fname = "fnames" in
-  let sn = fname = "snames" in
+let names_all base bname fname alias =
   let start = Unix.gettimeofday () in
   let ht = Hashtbl.create 1 in
   let nb_ind = nb_of_persons base in
   flush stderr;
   if !prog then (
-    Printf.printf "%s\n" fname;
+    Printf.printf "\n%s\n" fname;
     flush stdout;
     ProgrBar.full := '*';
     ProgrBar.start ());
@@ -111,59 +142,95 @@ let names_all base bname fname =
       if !prog then ProgrBar.run i nb_ind;
       let p = poi base ip in
       let nam =
-        if fn then sou base (get_first_name p)
-        else if sn then sou base (get_surname p)
-        else ""
+        match fname with
+        | "fnames" -> [ get_first_name p ]
+        | "snames" -> [ get_surname p ]
+        | "aliases" -> get_aliases p
+        | "occupations" -> [ get_occupation p ]
+        | "qualifiers" -> get_qualifiers p
+        | "pub_names" -> [ get_public_name p ]
+        | "estates" ->
+            List.fold_left (fun acc t -> t.t_place :: acc) [] (get_titles p)
+        | "titles" ->
+            List.fold_left (fun acc t -> t.t_ident :: acc) [] (get_titles p)
+        | "sources" ->
+            let p_sources =
+              List.fold_right
+                (fun evt events ->
+                  let src = evt.epers_src in
+                  src :: events)
+                (get_pevents p)
+                [ get_psources p ]
+            in
+            let ifams = Array.to_list (get_family p) in
+            let f_sources =
+              List.fold_left
+                (fun acc ifam ->
+                  List.fold_right
+                    (fun evt fam_fevents ->
+                      let src = evt.efam_src in
+                      src :: fam_fevents)
+                    (get_fevents (foi base ifam))
+                    []
+                  :: acc)
+                [] ifams
+            in
+            p_sources @ List.flatten f_sources
+        | _ -> []
       in
-      let al = if !alias then get_aliases p else [] in
-      let qual = if !qual then get_qualifiers p else [] in
-      let fna = if !fname_alias && fn then get_first_names_aliases p else [] in
-      let key = nam in
-      if nam <> "" then
-        if not (Hashtbl.mem ht key) then Hashtbl.add ht key (nam, 1)
-        else
-          let vv, i = Hashtbl.find ht key in
-          Hashtbl.replace ht key (vv, i + 1)
-      else ();
+      List.iter
+        (fun nam ->
+          let key = sou base nam in
+          if not (Hashtbl.mem ht key) then Hashtbl.add ht key (key, 1)
+          else
+            let vv, i = Hashtbl.find ht key in
+            Hashtbl.replace ht key (vv, i + 1))
+        nam;
+
       let nam2 =
-        if al <> [] then al
-        else if fna <> [] then fna
-        else if qual <> [] then qual
-        else []
+        match (fname, alias) with
+        | "fnames", "fna" -> get_first_names_aliases p
+        | "snames", "sna" -> get_surnames_aliases p
+        | _, _ -> []
       in
-      if nam2 <> [] then
-        List.iter
-          (fun nam ->
-            let nam = sou base nam in
-            let key = nam in
-            if not (Hashtbl.mem ht key) then Hashtbl.add ht key (nam, 1)
-            else
-              let vv, i = Hashtbl.find ht key in
-              Hashtbl.replace ht key (vv, i + 1))
-          nam2;
-      if !prog then ProgrBar.run i nb_ind else ())
+      List.iter
+        (fun nam ->
+          let key = sou base nam in
+          if not (Hashtbl.mem ht key) then Hashtbl.add ht key (key, 1)
+          else
+            let vv, i = Hashtbl.find ht key in
+            Hashtbl.replace ht key (vv, i + 1))
+        nam2)
     (Gwdb.ipers base);
 
   if !prog then ProgrBar.finish ();
-  flush stderr;
   let name_list = Hashtbl.fold (fun _k v acc -> v :: acc) ht [] in
   let name_list = List.sort (fun v1 v2 -> compare v1 v2) name_list in
   write_cache_file bname fname name_list;
-  flush stderr;
   let stop = Unix.gettimeofday () in
-  Printf.printf "Number of %s : %d\n" fname (Hashtbl.length ht);
-  Printf.printf "Execution time: %fs\n" (stop -. start);
+  let full_name =
+    Filename.concat !cache_dir (bname ^ "_" ^ fname ^ ".cache.gz")
+  in
+  Format.printf "@[<h>%-*s@ %8d@ %-14s@ %6.2f s@]@," !width full_name
+    (List.length name_list) fname (stop -. start);
   flush stderr
 
 let speclist =
   [
-    ("-fn", Arg.Set fnames, " produce first names");
-    ("-sn", Arg.Set snames, " produce surnames");
-    ("-al", Arg.Set alias, " produce aliases");
-    ("-qu", Arg.Set qual, " produce qualifiers");
-    ("-pl", Arg.Set places, " produce places");
-    ("-all", Arg.Set all, " produce all");
-    ("-fna", Arg.Set fname_alias, " add first names aliases");
+    ("-bd", Arg.String Secure.set_base_dir, " bases folder");
+    ("-fn", Arg.Set fnames, " first names");
+    ("-sn", Arg.Set snames, " surnames");
+    ("-al", Arg.Set alias, " aliases");
+    ("-pu", Arg.Set pub_names, " public names");
+    ("-fna", Arg.Set fname_alias, " add first name aliases");
+    ("-sna", Arg.Set sname_alias, " add surname aliases");
+    ("-qu", Arg.Set qualifiers, " qualifiers");
+    ("-pl", Arg.Set places, " places");
+    ("-es", Arg.Set estates, " estates");
+    ("-ti", Arg.Set titles, " titles");
+    ("-oc", Arg.Set occupations, " occupations");
+    ("-so", Arg.Set sources, " sources");
+    ("-all", Arg.Set all, " all");
     ("-prog", Arg.Set prog, " show progress bar");
   ]
   |> List.sort compare |> Arg.align
@@ -174,30 +241,51 @@ let usage =
   "Usage: cache_files [options] base\n cd bases; before running cache_files."
 
 let main () =
+  Secure.set_base_dir ".";
   Arg.parse speclist anonfun usage;
+  bname := Filename.remove_extension (Filename.basename !bname);
   if !bname = "" || !bname <> Filename.basename !bname then (
     Arg.usage speclist usage;
     exit 2);
   let base = Gwdb.open_base !bname in
   bname := Filename.basename !bname;
+  cache_dir := set_cache_dir !bname;
+  if not (Sys.file_exists !cache_dir) then Mutil.mkdir_p !cache_dir;
+  Printf.printf "Generating cache(s) compressed with gzip\n";
+
+  let full_name =
+    Filename.concat !cache_dir (!bname ^ "_occupations.cache.gz")
+  in
+  width := String.length full_name + 2;
+  Format.printf "@[<v>";
+  let fn_alias = if !fname_alias then "fna" else "" in
+  let sn_alias = if !sname_alias then "sna" else "" in
+  (* Ouvre une boite verticale *)
   if !places then places_all base !bname "places";
-  if !fnames then names_all base !bname "fnames";
-  if !snames then names_all base !bname "snames";
-  if !alias then names_all base !bname "aliases";
-  if !qual then names_all base !bname "qualifiers";
+  if !fnames then names_all base !bname "fnames" fn_alias;
+  if !snames then names_all base !bname "snames" sn_alias;
+  if !alias then names_all base !bname "aliases" "";
+  if !pub_names then names_all base !bname "pub_names" "";
+  if !estates then names_all base !bname "estates" "";
+  if !titles then names_all base !bname "titles" "";
+  if !occupations then names_all base !bname "occupations" "";
+  if !sources then names_all base !bname "sources" "";
+  if !qualifiers then names_all base !bname "qualifiers" "";
   if !all then (
+    let fn_alias = "fna" in
+    let sn_alias = "sna" in
     places_all base !bname "places";
-    fnames := true;
-    names_all base !bname "fnames";
-    fnames := false;
-    snames := true;
-    names_all base !bname "snames";
-    snames := false;
-    alias := true;
-    names_all base !bname "aliases";
-    alias := false;
-    qual := true;
-    names_all base !bname "qualifiers";
-    qual := false)
+    names_all base !bname "fnames" fn_alias;
+    names_all base !bname "snames" sn_alias;
+    names_all base !bname "aliases" "";
+    names_all base !bname "pub_names" "";
+    names_all base !bname "estates" "";
+    names_all base !bname "titles" "";
+    names_all base !bname "occupations" "";
+    names_all base !bname "sources" "";
+    names_all base !bname "qualifiers" "");
+  Format.printf "@]";
+  (* Ferme la boite verticale *)
+  flush stderr
 
 let _ = main ()
