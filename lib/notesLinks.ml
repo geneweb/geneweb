ceb0b822754a7a6ce8fed31004d9e7ec15044650
Henri83 <henri.gouraud@laposte.net>
2024-08-23 22:34:00 +0200

    rework wiki.ml to print original fn, sn

--- lib/notesLinks.ml
+++ lib/notesLinks.ml
@@ -119,7 +119,6 @@ let misc_notes_link s i =
                 let oc1 = try int_of_string name with Failure _ -> -1 in
                 let oc = try int_of_string oc with Failure _ -> 0 in
                 if oc1 = -1 then (fn, sn, oc, name)
-                  (* else if not Wiki.wi_person_exists (fn, sn, oc1) then (fn, sn, oc, fn ^ " " ^ sn) *)
                 else (fn, sn, oc1, fn ^ " " ^ sn)
               with Not_found ->
                 let sn = String.sub b k (String.length b - k) in
--- lib/util.mli
+++ lib/util.mli
@@ -358,7 +358,10 @@ val string_of_witness_kind : config -> sex -> witness_kind -> Adef.safe_string
 
 val relation_txt : config -> sex -> family -> (('a -> 'b) -> 'b, 'a, 'b) format
 val string_of_decimal_num : config -> float -> string
-val person_exists : config -> base -> string * string * int -> bool
+
+val person_exists :
+  config -> base -> string * string * int -> bool * string * string
+
 val husband_wife : config -> base -> person -> bool -> Adef.safe_string
 
 val find_person_in_env : config -> base -> string -> person option
