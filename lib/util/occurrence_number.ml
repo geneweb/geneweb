11f78256a4be2e716b1623d800aa89cdec41e5be
Idir Lankri <idir.lankri@geneanet.org>
2024-09-04 11:42:38 +0200

    Cleanup: Inline some code

--- lib/util/occurrence_number.ml
+++ lib/util/occurrence_number.ml
@@ -1,7 +1,6 @@
 let smallest_free occurrence_numbers =
-  let list_occ = Ext_int.Set.elements occurrence_numbers in
   let rec loop cnt1 = function
     | cnt2 :: list -> if cnt1 = cnt2 then loop (cnt1 + 1) list else cnt1
     | [] -> cnt1
   in
-  loop 0 list_occ
+  loop 0 (Ext_int.Set.elements occurrence_numbers)
