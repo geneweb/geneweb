a9cae98a0dab0eaa99f9b773c9e0b920b64f547d
Elie Canonici Merle <elie.canonicimerle@geneanet.org>
2024-09-12 14:45:06 +0200

    expose new gwdb persons lowered names functions

--- lib/gwdb-versioned/compat.mli
+++ lib/gwdb-versioned/compat.mli
@@ -468,6 +468,17 @@ module type Driver_S = sig
   (** Returns data structure that allows to make optimised search throughout
     index by surname *)
 
+  val persons_of_lower_first_name : base -> string_person_index
+  (** Returns data structure that allows to make optimised search throughout
+      index by first name *)
+
+  val persons_of_lower_surname : base -> string_person_index
+  (** Returns data structure that allows to make optimised search throughout
+      index by surname *)
+
+  val persons_stream_of_first_name_prefix : base -> string -> iper list Stream.t
+  val persons_stream_of_surname_prefix : base -> string -> iper list Stream.t
+
   val spi_first : string_person_index -> string -> istr
   (** Returns first [first/sur]name id starting with that string *)
 
