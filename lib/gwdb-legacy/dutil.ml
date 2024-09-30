4c77a8a8e3ccd4b2f95cdf88a43ac9716eac722d
Elie Canonici Merle <elie.canonicimerle@geneanet.org>
2024-09-30 13:42:44 +0200

    all prefxes of name

--- lib/gwdb-legacy/dutil.ml
+++ lib/gwdb-legacy/dutil.ml
@@ -58,15 +58,12 @@ let compare_snames_i base_data is1 is2 =
       (base_data.strings.get is1)
       (base_data.strings.get is2)
 
-let compare_snames_lower s1 s2 =
-  String.compare (Name.lower s1) (Name.lower s2)
+let compare_snames_lower s1 s2 = String.compare (Name.lower s1) (Name.lower s2)
 
 let compare_snames_i_lower base_data is1 is2 =
   if is1 = is2 then 0
   else
-    compare_snames_lower
-      (base_data.strings.get is1)
-      (base_data.strings.get is2)
+    compare_snames_lower (base_data.strings.get is1) (base_data.strings.get is2)
 
 let compare_fnames = String.compare
 
