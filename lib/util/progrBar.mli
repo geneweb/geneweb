68cc7e0239ecf476a23570aaf979586fac4ad372
Pierre Villemot <pierre.villemot@ocamlpro.com>
2024-12-04 17:33:10 +0100

    Add a better progress bar
    A progress bar needs to flush the output each time you change its value.
    But IO access are very slow comparatively to CPU computations, which
    means we cannot use the `run` function in hot loops (like it is done in
    `cache_files` binary.
    
    - The new progress bar do not print anything if the last print is too recent.
    - Deprecated the old progress bar (I didn't traverse all the codebase to
      remote the old bar to keep this commit as atomic as possible).

--- lib/util/progrBar.mli
+++ lib/util/progrBar.mli
@@ -1,5 +1,33 @@
 (* $Id: progrBar.mli,v 5.3 2007-02-01 10:28:55 ddr Exp $ *)
 
+type t
+(** Type of a progress bar. *)
+
+val progress : t -> int -> int -> unit
+(** [progress t n max] changes the completion of the progress bar to be
+    [n / max] of its length.
+
+    This function can be used in moderately hot loops. *)
+
+val with_bar :
+  ?width:int ->
+  ?empty:char ->
+  ?full:char ->
+  ?disabled:bool ->
+  Format.formatter ->
+  (t -> 'a) ->
+  'a
+(** [with_bar ppf f] wraps the call to [f] with a progress
+    bar printed on the formatter [ppf]. The function [step] can be call to
+    change the progression of the bar in [f].
+
+    To work properly, one should not print anything on [ppf] in [f].
+
+    If [ppf] is the standard output or the error output, one should not print
+    anything of both of them. *)
+
+(* XXX: The below bar is deprecated and should not be used in new code. *)
+
 val empty : char ref
 (** Character that represents not passed part of progression bar *)
 
@@ -12,6 +40,9 @@ val start : unit -> unit
 val run : int -> int -> unit
 (** [run i len] modifies progression bar that is now filled proportionally to
     [i] by comparison with [len]. *)
+
+(* XXX: This function cannot be used in hot loops without impacting
+        performance. *)
 
 val finish : unit -> unit
 (** Stop printing progression bar and prints a new line. *)
