837dd8d899665cd5f72d4a055baa63b18afd3d06
A2 <a2line@gmail.com>
2024-12-18 00:30:51 +0100

    Add a tool to check common typographic errors for dictionaries
    Add new system to detect typographic errors in dictonaries (books):
    - Invisible characters
    - Bad capitalization (with special rules for Roman numerals and Irish name prefixes)
    - Multiple spaces
    - Non-breaking spaces (detects both regular and narrow forms)
    
    Implemented for all dictionaries (first names, surnames, places, public names, qualifiers,
      aliases, occupations, titles, domains and sources) with specific rules:
    - First names/surnames/public names: exclude Roman numerals (e.g., "Louis XIV")
      and Irish prefixes (Mac-/Mc-/Fitz-)
    - Places: detect space variations that could affect location matching
    - Other dictionaries: standard typographic rules
    
    Features:
    - Navigation bar between dictionaries
    - Visual highlighting of detected issues
    - Error categorization with explanatory tooltips for invisible characters
    - Direct link to correction interface (upddata) for each entry
    - Structure designed for easy addition of new dictionaries and error types

---
+++ lib/checkData.mli
@@ -0,0 +1,24 @@
+type dict_type =
+  | Fnames
+  | Snames
+  | Places
+  | PubNames
+  | Qualifiers
+  | Aliases
+  | Occupation
+  | Estates
+  | Titles
+  | Sources
+
+type error_type =
+  | InvisibleCharacters
+  | BadCapitalization
+  | MultipleSpaces
+  | NonBreakingSpace
+
+val find_error_positions : error_type -> string -> int list
+val fix_error : error_type -> string -> string
+val make_error_html : Config.config -> string -> string -> error_type -> string
+
+val collect_all_errors :
+  Gwdb.base -> dict_type -> (string * error_type list) list
---
+++ lib/checkDataDisplay.ml
@@ -0,0 +1,89 @@
+open Config
+
+let print_nav_button conf current_data data icon title =
+  let href = Printf.sprintf "?m=CHK_DATA&data=%s" data in
+  let active = if current_data = data then "active" else "" in
+  Output.printf conf
+    {|<a href="%s" class="btn btn-outline-primary %s">
+       <i class="fa fa-%s mr-1"></i>%s
+     </a>|}
+    href active icon
+    (Utf8.capitalize_fst (Util.transl_nth conf title 1))
+
+let print_nav_buttons conf current_data =
+  Output.printf conf "<div class=\"d-flex btn-group mt-3\" role=\"group\">\n";
+  print_nav_button conf current_data "fn" "child" "first name/first names";
+  print_nav_button conf current_data "sn" "signature" "surname/surnames";
+  print_nav_button conf current_data "place" "map-location-dot" "place/places";
+  print_nav_button conf current_data "pubn" "pen" "public name/public names";
+  print_nav_button conf current_data "qual" "comment" "qualifier/qualifiers";
+  print_nav_button conf current_data "alias" "mask" "alias/aliases";
+  print_nav_button conf current_data "occu" "user-doctor"
+    "occupation/occupations";
+  print_nav_button conf current_data "title" "crown" "title/titles";
+  print_nav_button conf current_data "domain" "chess-rook" "domain/domains";
+  print_nav_button conf current_data "src" "box-archive" "source/sources";
+  Output.printf conf "</div>\n"
+
+let display_error_section conf data entries error_type error_title =
+  let filtered_entries =
+    List.filter_map
+      (fun (entry, errors) ->
+        if List.mem error_type errors then
+          Some (CheckData.make_error_html conf data entry error_type)
+        else None)
+      entries
+  in
+  if filtered_entries <> [] then (
+    Output.printf conf "<h3 class=\"mt-3\">%s</h3>" error_title;
+    Output.print_sstring conf "<ul class=\"list-group\">";
+    List.iter
+      (fun html ->
+        Output.printf conf "<li class=\"list-group-item\">%s</li>" html)
+      filtered_entries;
+    Output.print_sstring conf "</ul>")
+
+let print conf base =
+  let data = Util.p_getenv conf.env "data" |> Option.value ~default:"" in
+
+  let dict =
+    match data with
+    | "fn" -> Some CheckData.Fnames
+    | "sn" -> Some CheckData.Snames
+    | "place" -> Some CheckData.Places
+    | "pubn" -> Some CheckData.PubNames
+    | "qual" -> Some CheckData.Qualifiers
+    | "alias" -> Some CheckData.Aliases
+    | "occu" -> Some CheckData.Occupation
+    | "title" -> Some CheckData.Titles
+    | "domain" -> Some CheckData.Estates
+    | "src" -> Some CheckData.Sources
+    | _ -> None
+  in
+
+  let entries =
+    match dict with
+    | Some dict_type -> CheckData.collect_all_errors base dict_type
+    | None -> []
+  in
+
+  let title _ =
+    Output.print_sstring conf (Util.transl conf "data typographic checker")
+  in
+  Hutil.header conf title;
+
+  print_nav_buttons conf data;
+
+  display_error_section conf data entries CheckData.InvisibleCharacters
+    (Util.transl conf "chk_data invisible characters");
+
+  display_error_section conf data entries CheckData.BadCapitalization
+    (Util.transl conf "chk_data bad capitalization");
+
+  display_error_section conf data entries CheckData.MultipleSpaces
+    (Util.transl conf "chk_data multiple spaces");
+
+  display_error_section conf data entries CheckData.NonBreakingSpace
+    (Util.transl conf "chk_data non-breaking spaces");
+
+  Hutil.trailer conf
