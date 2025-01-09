2894afe1680c7dc9d06dd33fe0413527419eaa65
Pierre Villemot <pierre.villemot@ocamlpro.com>
2025-01-09 15:40:42 +0100

    Extra the module iterator with a generic interface

---
+++ lib/search/iterator.ml
@@ -0,0 +1,154 @@
+type ('a, 'c) t = { curr : unit -> 'a; next : unit -> unit; seek : 'a -> unit }
+
+exception End
+
+module type Comparator = sig
+  type t
+  type wit
+
+  val dummy : t
+  val compare : t -> t -> int
+end
+
+type ('a, 'c) comparator =
+  (module Comparator with type t = 'a and type wit = 'c)
+
+let make _cmp ~curr ~next ~seek = { curr; next; seek }
+let[@inline always] curr it = it.curr ()
+let[@inline always] next it = it.next ()
+let[@inline always] seek it e = it.seek e
+
+let equal (type a c) (module C : Comparator with type t = a and type wit = c)
+    (it1 : (a, c) t) (it2 : (a, c) t) =
+  let rec loop () =
+    let v1 = try Some (it1.curr ()) with End -> None in
+    let v2 = try Some (it2.curr ()) with End -> None in
+    match (v1, v2) with
+    | Some v1, Some v2 ->
+        if C.compare v1 v2 <> 0 then false
+        else (
+          it1.next ();
+          it2.next ();
+          loop ())
+    | None, Some _ | Some _, None -> false
+    | None, None -> true
+  in
+  loop ()
+
+let union (type a c) (module C : Comparator with type t = a and type wit = c)
+    (l : (a, c) t list) =
+  let arr = Array.of_list l in
+  if Array.length arr = 0 then invalid_arg "union";
+  let module H = Heap.Make (struct
+    type t = int * a
+
+    let dummy = (0, C.dummy)
+    let compare (_, v1) (_, v2) = C.compare v1 v2
+  end) in
+  let len = Array.length arr in
+  let hp = H.create len in
+  for i = 0 to len - 1 do
+    match curr arr.(i) with exception End -> () | v -> H.insert hp (i, v)
+  done;
+  let seek w =
+    let rec loop () =
+      match H.min hp with
+      | exception H.Empty -> ()
+      | _, v when C.compare w v <= 0 -> ()
+      | i, _ ->
+          let (_ : int * a) = H.delete_min hp in
+          seek arr.(i) w;
+          let () =
+            match curr arr.(i) with
+            | exception End -> ()
+            | v -> H.insert hp (i, v)
+          in
+          loop ()
+    in
+    let () = loop () in
+    assert (
+      match H.min hp with
+      | exception H.Empty -> true
+      | _, v -> C.compare w v <= 0)
+  in
+  let next () =
+    match H.delete_min hp with
+    | exception H.Empty -> ()
+    | i, _ -> (
+        next arr.(i);
+        match curr arr.(i) with exception End -> () | v -> H.insert hp (i, v))
+  in
+  let curr () =
+    match H.min hp with exception H.Empty -> raise End | _, v -> v
+  in
+  { curr; next; seek }
+
+(* This implementation follows the leapfrog join describes in the paper
+   https://openproceedings.org/ICDT/2014/paper_13.pdf *)
+let join (type a c) (module C : Comparator with type t = a and type wit = c)
+    (l : (a, c) t list) =
+  let arr = Array.of_list l in
+  if Array.length arr = 0 then
+    (* Intersection of 0th elements cannot be represented by a finite
+       set of any type. *)
+    invalid_arg "join";
+  let ended = ref false in
+  (* Index of an iterator [it] in [arr] such that its current value
+     is the smallest among current values of iterators of [arr]. *)
+  let pos = ref 0 in
+  (* Helper function that advances the iterators of [arr] until
+     the next meeting point. *)
+  let search () =
+    let k = Array.length arr in
+    let rec loop x =
+      let y = curr arr.(!pos) in
+      if C.compare y x < 0 then (
+        seek arr.(!pos) x;
+        match curr arr.(!pos) with
+        | exception End -> ended := true
+        | x' ->
+            (* As y < x and the iterator [arr.(!pos)] has not
+               reached its end, the previous seek call must
+               advance this iterator. *)
+            assert (C.compare y x' < 0);
+            pos := (!pos + 1) mod k;
+            loop x')
+    in
+    loop (curr arr.((k + !pos - 1) mod k))
+  in
+  let () =
+    try
+      Array.sort (fun it1 it2 -> C.compare (curr it1) (curr it2)) arr;
+      search ()
+    with End -> ended := true
+  in
+  let seek w =
+    seek arr.(!pos) w;
+    match curr arr.(!pos) with
+    | exception End -> ended := true
+    | _ ->
+        let k = Array.length arr in
+        pos := (!pos + 1) mod k;
+        search ()
+  in
+  let next () =
+    next arr.(!pos);
+    match curr arr.(!pos) with
+    | exception End -> ended := true
+    | _ ->
+        let k = Array.length arr in
+        pos := (!pos + 1) mod k;
+        search ()
+  in
+  let curr () = if !ended then raise End else curr arr.(0) in
+  { curr; next; seek }
+
+let to_seq it =
+  let rec loop () =
+    match curr it with
+    | exception End -> Seq.Nil
+    | v ->
+        next it;
+        Seq.Cons (v, loop)
+  in
+  loop
---
+++ lib/search/iterator.mli
@@ -0,0 +1,68 @@
+type ('a, 'c) t = private {
+  curr : unit -> 'a;
+  next : unit -> unit;
+  seek : 'a -> unit;
+}
+(** Type of an iterator. *)
+
+exception End
+(** Exception raised when attempting to access elements beyond the
+        end of an iterator. *)
+
+module type Comparator = sig
+  type t
+  type wit
+
+  val dummy : t
+  val compare : t -> t -> int
+end
+
+type ('a, 'c) comparator =
+  (module Comparator with type t = 'a and type wit = 'c)
+
+val make :
+  ('a, 'c) comparator ->
+  curr:(unit -> 'a) ->
+  next:(unit -> unit) ->
+  seek:('a -> unit) ->
+  ('a, 'c) t
+
+val curr : ('a, 'c) t -> 'a
+(** [curr it] returns the element currently pointed by the iterator [it].
+    @raise End if the iterator [it] has reached the end of the
+           collection. *)
+
+val next : ('a, 'c) t -> unit
+(** [next it] advances the iterator [it] to the next element. No effect
+    if the iterator [it] has already reached the end of the collection. *)
+
+val seek : ('a, 'c) t -> 'a -> unit
+(** [seek e] advances the iterator [it] to the smallest element in
+    the collection that is greater or equal to [e]. If already positioned
+    at this element, the iterator remains unchanged.
+
+    For a sequence of calls [seek it ei] where [e1 <= ... <= en], the
+    amortized complexity is expected to be O(1 + log(N/n)) where N is the
+    cardinal of the collection. *)
+
+val union : ('a, 'c) comparator -> ('a, 'c) t list -> ('a, 'c) t
+(** [union l] computes the union iterator of the iterators [l].
+    The resulting iterator produces elements of the union of [l] in
+    ascending order.
+
+    @raise Invalid_argument if the list [l] is empty. *)
+
+val join : ('a, 'c) comparator -> ('a, 'c) t list -> ('a, 'c) t
+(** [join l] computes the join iterator of the iterators [l]. The resulting
+    iterator produces elements of the intersection of [l] in ascending
+    order.
+
+    @raise Invalid_argument if the list [l] is empty. *)
+
+val equal : ('a, 'c) comparator -> ('a, 'c) t -> ('a, 'c) t -> bool
+(** [equal it1 it2] checks if the two iterators [it1] and [it2] are equal.
+    This function consumes both [it1] and [it2]. *)
+
+val to_seq : ('a, 'c) t -> 'a Seq.t
+(** [to_seq it] converts the iterator [it] into a sequence. Forcing the
+    resulting sequence consumes [it]. *)
