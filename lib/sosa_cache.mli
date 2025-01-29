ee8e2cf885299dd71d4ce31cf678541b54087b78
Elie Canonici Merle <elie.canonicimerle@geneanet.org>
2025-01-29 09:13:43 +0100

    add intermediary module sosa_cache + update perso module to use it

---
+++ lib/sosa_cache.mli
@@ -0,0 +1,16 @@
+type t
+
+val build_static_sosa_cache : conf:Config.config -> base:Gwdb.base -> t option
+val output_static_sosa_cache : base:Gwdb.base -> cache:t -> unit
+val get_sosa_cache : conf:Config.config -> base:Gwdb.base -> t option
+
+val get_sosa :
+  conf:Config.config ->
+  base:Gwdb.base ->
+  cache:t ->
+  iper:Gwdb.iper ->
+  sosa_ref:Gwdb.person ->
+  Sosa.t option
+
+val next_sosa : t -> Sosa.t -> (Sosa.t * Gwdb.iper) option
+val previous_sosa : t -> Sosa.t -> (Sosa.t * Gwdb.iper) option
