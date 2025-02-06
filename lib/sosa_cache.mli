8c7e661746186ad64d980e963ecddc7dc370ec44
Elie Canonici Merle <elie.canonicimerle@geneanet.org>
2025-02-06 12:38:51 +0100

    remove unused arguments + no sosa cache stored in perso env

--- lib/sosa_cache.mli
+++ lib/sosa_cache.mli
@@ -2,14 +2,7 @@ type t
 
 val write_static_sosa_cache : conf:Config.config -> base:Gwdb.base -> unit
 val get_sosa_cache : conf:Config.config -> base:Gwdb.base -> t option
-
-val get_sosa :
-  conf:Config.config ->
-  base:Gwdb.base ->
-  cache:t ->
-  iper:Gwdb.iper ->
-  sosa_ref:Gwdb.person ->
-  Sosa.t option
+val get_sosa : base:Gwdb.base -> cache:t -> iper:Gwdb.iper -> Sosa.t option
 
 val get_sosa_person :
   conf:Config.config -> base:Gwdb.base -> person:Gwdb.person -> Sosa.t
