90d54a9a74b1d8bc309b2da4769a3bd1df19189c
Henri83 <henri.gouraud@laposte.net>
2025-02-14 19:56:30 +0100

    close wizard file after use in gwuLib and update_nldb

--- bin/update_nldb/update_nldb.ml
+++ bin/update_nldb/update_nldb.ml
@@ -43,7 +43,7 @@ let notes_links s =
   loop [] [] 1 0
 
 let read_file_contents fname =
-  match try Some (open_in fname) with Sys_error _ -> None with
+  match try Some (Secure.open_in fname) with Sys_error _ -> None with
   | Some ic -> (
       let len = ref 0 in
       try
@@ -52,7 +52,9 @@ let read_file_contents fname =
           loop ()
         in
         loop ()
-      with End_of_file -> Buff.get !len)
+      with End_of_file ->
+        close_in ic;
+        Buff.get !len)
   | None -> ""
 
 type cache_linked_pages_t = (Def.NLDB.key, int) Hashtbl.t
