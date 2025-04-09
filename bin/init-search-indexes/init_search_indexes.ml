bf1527505d1dccbd8903f93b605eca4cfc8bb45f
Idir Lankri <idir.lankri@geneanet.org>
2025-04-09 13:03:35 +0200

    bin: Add a script to initialize the search indexes *statically*

---
+++ bin/init-search-indexes/init_search_indexes.ml
@@ -0,0 +1,30 @@
+let initialize_search_indexes database =
+  let on_lock_error () =
+    Lock.print_try_again ();
+    raise Exit
+  in
+  Gwdb.initialize_lowercase_name_index ~on_lock_error ~kind:`First_name database;
+  Gwdb.initialize_lowercase_name_index ~on_lock_error ~kind:`Surname database
+
+let main () =
+  let options = [] in
+  let usage = Printf.sprintf "Usage: %s <database-name>" Sys.argv.(0) in
+  if Array.length Sys.argv <> 2 then (
+    Arg.usage options usage;
+    exit 2)
+  else
+    Arg.parse options
+      (fun database ->
+        let database =
+          try Gwdb.open_base database
+          with e ->
+            Printexc.raise_with_backtrace e (Printexc.get_raw_backtrace ())
+        in
+        Fun.protect
+          ~finally:(fun () -> Gwdb.close_base database)
+          (fun () ->
+            if not @@ Gwdb.search_indexes_can_be_initialized_on_the_fly database
+            then initialize_search_indexes database))
+      usage
+
+let () = main ()
