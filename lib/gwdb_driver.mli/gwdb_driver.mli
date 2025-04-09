128d0fff9fd6c428d8f113a404194518e6ce3f0b
Idir Lankri <idir.lankri@geneanet.org>
2025-04-09 15:58:44 +0200

    gwdb: Parametrize some code

--- lib/gwdb_driver.mli/gwdb_driver.mli
+++ lib/gwdb_driver.mli/gwdb_driver.mli
@@ -729,4 +729,7 @@ val gc :
 val set_fpoi_cache : base -> bool -> unit
 
 val initialize_lowercase_name_index :
-  kind:[< `First_name | `Surname ] -> base -> unit
+  ?on_lock_error:(unit -> unit) ->
+  kind:[< `First_name | `Surname ] ->
+  base ->
+  unit
