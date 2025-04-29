2d49ef0aeebfd58d8c1a86dfbdea28b9265bae96
Idir Lankri <idir.lankri@geneanet.org>
2025-04-29 16:44:59 +0200

    lib/util: Move some code

--- lib/util/html.ml
+++ lib/util/html.ml
@@ -12,14 +12,14 @@ let map ?escape_attribute ?escape_text f s =
   s |> parse_as_body_content |> Markup.map f
   |> pretty_print ?escape_attribute ?escape_text
 
+let text_content s =
+  s |> parse_as_body_content
+  |> Markup.filter Ext_markup.is_text
+  |> pretty_print ~escape_attribute:Fun.id ~escape_text:Fun.id
+
 let is_plain_text s =
   s |> parse_as_body_content
   |> Markup.fold
        (fun is_plain_text_so_far element ->
          is_plain_text_so_far && Ext_markup.is_text element)
        true
-
-let text_content s =
-  s |> parse_as_body_content
-  |> Markup.filter Ext_markup.is_text
-  |> pretty_print ~escape_attribute:Fun.id ~escape_text:Fun.id
