854efa5774bfd045ae6658cd752b2f460d875705
Idir Lankri <idir.lankri@geneanet.org>
2025-05-09 11:46:51 +0200

    lib: Move some code

--- lib/changeChildren.ml
+++ lib/changeChildren.ml
@@ -64,13 +64,6 @@ let change_child conf base parent_surname changed ip =
     let ipl = Gutil.person_ht_find_all base key in
     check_conflict base p key new_occ ipl;
     Image.rename_portrait conf base p (new_first_name, new_surname, new_occ);
-    (* On ajoute les enfants dans le type Change_children_name       *)
-    (* pour la future mise à jour de l'historique et du fichier gwf. *)
-    let changed =
-      ( (Gwdb.p_first_name base p, Gwdb.p_surname base p, Gwdb.get_occ p, ip),
-        (new_first_name, new_surname, new_occ, ip) )
-      :: changed
-    in
     let new_p =
       {
         (Gwdb.gen_person_of_person p) with
@@ -78,6 +71,13 @@ let change_child conf base parent_surname changed ip =
         surname = Gwdb.insert_string base new_surname;
         occ = new_occ;
       }
+    in
+    (* On ajoute les enfants dans le type Change_children_name       *)
+    (* pour la future mise à jour de l'historique et du fichier gwf. *)
+    let changed =
+      ( (Gwdb.p_first_name base p, Gwdb.p_surname base p, Gwdb.get_occ p, ip),
+        (new_first_name, new_surname, new_occ, ip) )
+      :: changed
     in
     Gwdb.patch_person base ip new_p;
     changed)
