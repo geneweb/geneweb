8b32718e3f8e2a04f4ab38a0603b9fa446ec31f3
Henri83 <henri.gouraud@laposte.net>
2025-07-22 09:46:16 +0200

    gwu: test for first related definition

--- bin/gwu/gwuLib.ml
+++ bin/gwu/gwuLib.ml
@@ -1308,12 +1308,13 @@ let print_relations opts base gen ml =
   in
   let rec loop = function
     | [] -> ()
-    | (p, if_def) :: pl ->
+    | (p, _if_def) :: pl ->
         let def_p = ref [] in
+        let if_def = Collection.Marker.get gen.mark_rel (Driver.get_iper p) in
         if Driver.get_rparents p <> [] && gen.per_sel (Driver.get_iper p) then (
           print_relations_for_person opts base gen def_p if_def p;
           List.iter (print_notes_for_person opts base gen) !def_p;
-          if not !old_gw then
+            if not !old_gw then
             List.iter (print_pevents_for_person opts base gen) !def_p);
         loop (pl @ List.map (fun p -> (p, false)) !def_p)
   in
@@ -1328,8 +1329,9 @@ let print_isolated_relations opts base gen p =
   in
   let rec loop = function
     | [] -> ()
-    | (p, if_def) :: pl ->
+    | (p, _if_def) :: pl ->
         let def_p = ref [] in
+        let if_def = Collection.Marker.get gen.mark_rel (Driver.get_iper p) in
         if Driver.get_rparents p <> [] && gen.per_sel (Driver.get_iper p) then (
           print_relations_for_person opts base gen def_p if_def p;
           List.iter (print_notes_for_person opts base gen) !def_p);
