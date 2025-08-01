b5c71a85bb01018e87e10f877866813932786b0f
Idir Lankri <idir.lankri@geneanet.org>
2025-08-01 16:32:24 +0200

    Cleanup: Get rid of some top-level `open`s

--- plugins/gwxjg/gwxjg_trans.ml
+++ plugins/gwxjg/gwxjg_trans.ml
@@ -1,5 +1,4 @@
 module Lexicon_parser = Gwxjg_lexicon_parser
-open Jingoo
 
 let fast_concat = function
   | [] -> ""
@@ -18,12 +17,10 @@ let fast_concat = function
       Bytes.unsafe_to_string b
 
 let import_trad ht keyword line =
-  let open Jg_types in
-  let open Jg_runtime in
   Hashtbl.add ht keyword @@ fun ?(kwargs = []) i ->
   let i = if i < 0 || i >= Array.length line then 0 else i in
   let arg s = List.assoc s kwargs in
-  Tstr
+  Jingoo.Jg_types.Tstr
     (fast_concat
     @@
     let a = Array.unsafe_get line i in
@@ -32,12 +29,17 @@ let import_trad ht keyword line =
       else
         match Array.unsafe_get a i with
         | Lexicon_parser.Str s -> loop (s :: acc) (i - 1)
-        | Arg n -> loop (string_of_tvalue (arg n) :: acc) (i - 1)
+        | Arg n ->
+            loop (Jingoo.Jg_runtime.string_of_tvalue (arg n) :: acc) (i - 1)
         | Declension (c, n) ->
-            loop ((arg n |> string_of_tvalue |> Mutil.decline c) :: acc) (i - 1)
+            loop
+              ((arg n |> Jingoo.Jg_runtime.string_of_tvalue |> Mutil.decline c)
+              :: acc)
+              (i - 1)
         | Elision (s1, s2) ->
             let x =
-              try unbox_string @@ arg "elision" with Not_found -> List.hd acc
+              try Jingoo.Jg_types.unbox_string @@ arg "elision"
+              with Not_found -> List.hd acc
             in
             if
               x <> ""
