5a31bfa380c7a804c7389fe0559bf5f3b37d9665
A2 <a2line@gmail.com>
2025-12-09 11:27:58 +0100

    detail dagDisplay doc

--- lib/dagDisplay.mli
+++ lib/dagDisplay.mli
@@ -33,7 +33,11 @@ val print_dag_page :
   (int * Dag2html.align * Adef.safe_string Dag2html.table_data) array array ->
   Adef.escaped_string ->
   unit
-(** Render DAG as HTML page using dag.txt template. *)
+(** [print_dag_page conf base page_title hts next_txt] renders the DAG as HTML
+    using the dag.txt template.
+    - [page_title]: title displayed in the page header
+    - [hts]: HTML table structure from {!make_tree_hts}
+    - [next_txt]: link text for pagination (empty if none) *)
 
 val make_and_print_dag :
   Config.config ->
@@ -48,7 +52,15 @@ val make_and_print_dag :
   Adef.safe_string ->
   Adef.escaped_string ->
   unit
-(** Build DAG from person set and render as HTML page. *)
+(** [make_and_print_dag conf base elem_txt vbar_txt invert set spl page_title
+     next_txt] builds a DAG from the person set and renders it as HTML.
+    - [elem_txt]: function to generate display text for each person
+    - [vbar_txt]: function to generate vertical bar link for each person
+    - [invert]: if true, display descendants at top, ancestors at bottom
+    - [set]: list of person identifiers to include in the DAG
+    - [spl]: spouse pairs to connect in the graph
+    - [page_title]: title displayed in the page header
+    - [next_txt]: link text for pagination (empty if none) *)
 
 val print : Config.config -> Geneweb_db.Driver.base -> unit
 (** Entry point for m=DAG request. *)
