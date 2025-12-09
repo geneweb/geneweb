59c33714a72753c3a98f4623943434090b5b54bd
A2 <a2line@gmail.com>
2025-12-09 01:31:20 +0100

    refactor(dag): remove obsolete <pre>/ASCII rendering mode
    Remove the legacy text-mode DAG renderer, originally intended for printers
     unable to handle large HTML tables.
    
    * Drop all `print_*_pre` functions and related ASCII helpers (≈450 LOC)
    * Remove unused env variants (`Vdag`, `Vdcellp`, `Vdlinep`)
    * Remove obsolete `eval_dag_var` fields
    * Remove “slices” feature and associated template logic
    * Rename `print_slices_menu_or_dag_page` → `print_dag_page`
    * Simplify `print_html_table` to delegate directly to `print_table`
    * Update callers and documentation accordingly
    * Clean up `dag.txt` by removing all `<pre>` and slice-related sections

--- lib/dagDisplay.mli
+++ lib/dagDisplay.mli
@@ -1,11 +1,15 @@
-(* TODOOCP *)
+(** DAG (Directed Acyclic Graph) display for genealogical trees. *)
+
 val image_txt :
   Config.config ->
   Geneweb_db.Driver.base ->
   Geneweb_db.Driver.person ->
   Adef.safe_string
+(** Generate HTML for person's portrait image. *)
 
-type item = Item of Geneweb_db.Driver.person * Adef.safe_string
+type item =
+  | Item of Geneweb_db.Driver.person * Adef.safe_string
+      (** DAG node item: person with optional annotation. *)
 
 val make_tree_hts :
   Config.config ->
@@ -19,16 +23,17 @@ val make_tree_hts :
   list ->
   (Geneweb_db.Driver.iper, 'a) Def.choice Dag2html.dag ->
   (int * Dag2html.align * Adef.safe_string Dag2html.table_data) array array
-(** [make_tree_hts conf base elem_txt vbar_txt invert set spl d] *)
+(** Build HTML table structure from DAG.
+    [make_tree_hts conf base elem_txt vbar_txt invert set spl dag] *)
 
-val print_slices_menu_or_dag_page :
+val print_dag_page :
   Config.config ->
   Geneweb_db.Driver.base ->
   Adef.safe_string ->
   (int * Dag2html.align * Adef.safe_string Dag2html.table_data) array array ->
   Adef.escaped_string ->
   unit
-(** [print_slices_menu_or_dag_page conf page_title hts next_txt] *)
+(** Render DAG as HTML page using dag.txt template. *)
 
 val make_and_print_dag :
   Config.config ->
@@ -43,7 +48,7 @@ val make_and_print_dag :
   Adef.safe_string ->
   Adef.escaped_string ->
   unit
-(** [make_and_print_dag conf base elem_txt vbar_txt invert set spl page_title
-     next_txt] *)
+(** Build DAG from person set and render as HTML page. *)
 
 val print : Config.config -> Geneweb_db.Driver.base -> unit
+(** Entry point for m=DAG request. *)
