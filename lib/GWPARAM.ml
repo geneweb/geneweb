2b8d7b49eda66eab2e15bcb649fbd1a18693b42c
Henri83 <henri.gouraud@laposte.net>
2025-12-19 22:25:33 +0100

    fix split_key which was failing with spaces in fn or sn

--- lib/GWPARAM.ml
+++ lib/GWPARAM.ml
@@ -333,14 +333,45 @@ let output_error =
 
 let is_semi_public p = Driver.get_access p = Def.SemiPublic
 
+(* assumes fn+sn, fn sn, fn.occ+sn, fn.occ sn *)
 let split_key key =
-  let dot = match String.index_opt key '.' with Some i -> i | _ -> -1 in
-  let space = match String.index_opt key ' ' with Some i -> i | _ -> -1 in
-  if dot <> -1 && space <> -1 then
-    ( String.sub key 0 dot,
-      String.sub key (dot + 1) (space - dot - 1),
-      String.sub key (space + 1) (String.length key - space - 1) )
-  else ("?", "", "?")
+  let dot = String.index_opt key '.' in
+  let plus = String.index_opt key '+' in
+
+  match (dot, plus) with
+  | Some d, _ ->
+      (* Has dot: firstname.occ separator *)
+      let space_after_dot =
+        try Some (String.index_from key (d + 1) ' ') with Not_found -> None
+      in
+      let plus_after_dot =
+        match plus with Some p when p > d -> Some p | _ -> None
+      in
+      let sn_sep =
+        match (space_after_dot, plus_after_dot) with
+        | Some s, Some p -> min s p (* Use whichever comes first *)
+        | Some s, None -> s
+        | None, Some p -> p
+        | None, None -> -1
+      in
+      if sn_sep <> -1 then
+        ( String.sub key 0 d,
+          String.sub key (d + 1) (sn_sep - d - 1),
+          String.sub key (sn_sep + 1) (String.length key - sn_sep - 1) )
+      else ("?", "", "?")
+  | None, Some p ->
+      (* No dot but has plus: firstname+surname *)
+      ( String.sub key 0 p,
+        "",
+        String.sub key (p + 1) (String.length key - p - 1) )
+  | None, None -> (
+      (* No dot or plus: use first space *)
+      match String.index_opt key ' ' with
+      | Some s ->
+          ( String.sub key 0 s,
+            "",
+            String.sub key (s + 1) (String.length key - s - 1) )
+      | None -> ("?", "", "?"))
 
 (* Determine if person is related to the current user *)
 let ancestors _conf base max_generations family ip =
