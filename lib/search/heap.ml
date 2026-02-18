305d5cd34163e96665fc64ac69bcb2f122fa8136
Pierre Villemot <pierre.villemot@ocamlpro.com>
2026-02-18 21:59:28 +0100

    Change the sentinel value and remove swap function

--- lib/search/heap.ml
+++ lib/search/heap.ml
@@ -48,15 +48,17 @@ module Make (O : OrderedType) = struct
     let vi = get t i in
     let rec loop j =
       let l = left j and r = right j in
-      let mi =
+      (* Find the smallest child to bubble up, or stay at [j] if the heap
+         property is satisfied. *)
+      let s =
         if l < t.sz && O.compare (get t l) vi < 0 then
           if r < t.sz && O.compare (get t r) (get t l) < 0 then r else l
         else if r < t.sz && O.compare (get t r) vi < 0 then r
-        else i
+        else j
       in
-      if i <> mi then (
-        set t j (get t mi);
-        loop mi)
+      if s <> j then (
+        set t j (get t s);
+        loop s)
       else set t j vi
     in
     loop i
@@ -69,16 +71,11 @@ module Make (O : OrderedType) = struct
       t.sz <- i + 1;
       percolate_up t i
 
-  let[@inline] swap t i p =
-    let tmp = get t i in
-    set t i (get t p);
-    set t p tmp
-
   let delete_min t =
     if empty t then raise Empty
     else
       let d = get t 0 in
-      swap t 0 (t.sz - 1);
+      set t 0 (get t (t.sz - 1));
       t.sz <- t.sz - 1;
       percolate_down t 0;
       d
