5e75f39343840a8fcafa81bcf074a0219817d852
A2 <a2line@gmail.com>
2026-03-30 18:36:05 +0200

    pin Unidecode to 0.5.0

--- nix/unidecode.nix
+++ nix/unidecode.nix
@@ -3,11 +3,11 @@
 
 buildDunePackage (finalAttrs: {
   pname = "unidecode";
-  version = "0.4.0";
+  version = "0.5.0";
   duneVersion = "3";
 
   src = fetchTarball {
     url = "https://github.com/geneweb/unidecode/archive/refs/tags/v${finalAttrs.version}.tar.gz";
-    sha256 = "1fahjzgr2zw8cplw5azd39z3m0dfv7wjbrzpb8953nadyzk87cjl";
+    sha256 = "87ccf6a1da1aa7a8d3c0278940934df7c887a50d28485b668dea1d654d066631";
   };
 })
