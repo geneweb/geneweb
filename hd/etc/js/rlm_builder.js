e7ae6053517c97654818910e9c3159067fc585f4
A2 <a2line@gmail.com>
2026-04-04 02:20:06 +0200

    port rlm_builder to vanilla javascript
    event.stopPropagation on its buttons

--- hd/etc/js/rlm_builder.js
+++ hd/etc/js/rlm_builder.js
@@ -1,52 +1,70 @@
-<!-- $Id: js/rlm_builder.js v7.1 10/01/2025 04:15:36 $ -->
-$('#saveButton').on('click', saveIndParams);
-$('#clearGraphButton').on('click', clearCache);
-updateUI();
+<!-- $Id: js/rlm_builder.js v7.1 04/04/2026 01:56:46 $ -->
 
-function saveIndParams() {
-  const $RLM = $('#RLM');
-  const { i, p, n, oc, self } = $RLM.data();
-  const t = $('#description').val();
-  const ind = { i, p, n, oc, t, self };
-  const storedInds = JSON.parse(localStorage.getItem('inds')) || [];
-  storedInds.push(ind);
-  localStorage.setItem('inds', JSON.stringify(storedInds));
-  $('#description').val('');
-  updateUI();
-}
+'use strict';
 
-function clearCache() {
-  localStorage.removeItem('inds');
-  updateUI();
-}
-
-function updateUI() {
-  const storedInds = JSON.parse(localStorage.getItem('inds')) || [];
-  const { url, tooltip } = buildUrlAndTooltip(storedInds);
-  const eBase = $('#RLM').data('eb') || '';
+const RLM = document.getElementById('RLM');
+if (RLM) {
+  const { i, p, n, oc, self, eb: eBase, accessbykey: ak } = RLM.dataset;
+  const descInput = document.getElementById('description');
+  const saveBtn = document.getElementById('saveButton');
+  const clearBtn = document.getElementById('clearGraphButton');
+  const graphBtns = document.getElementById('graphButtons');
+  const genBtn = document.getElementById('generateGraphButton');
+  const editBtn = document.getElementById('editGraphButton');
   const cgibase = eBase ? '&b=' + eBase : '';
-  const urlRLM = `?m=RLM${cgibase}${url}`;
-  const urlEditRLM = `?m=TP&v=updRLM${cgibase}${url}`;
-  $('#generateGraphButton').attr('href', urlRLM).attr('title', tooltip);
-  $('#editGraphButton').attr('href', urlEditRLM);
-  $('#clearGraphButton, #graphButtons').toggleClass('d-none', storedInds.length < 1);
-}
 
-function buildUrlAndTooltip(storedInds) {
-  let url = '';
-  let tooltip = '';
-  storedInds.forEach((ind, i) => {
-    url += buildUrlParams(ind, i + 1);
-    tooltip += buildTooltipLine(ind, i + 1);
+  // Prevent dropdown close on interactive elements
+  [saveBtn, clearBtn].forEach(el => {
+    if (el) el.addEventListener('click', e => e.stopPropagation());
   });
-  return { url, tooltip: tooltip.trim() };
-}
 
-function buildUrlParams(ind, n) {
-  const ak = $('#RLM').data('accessbykey') || '';
-  return `&${ak === 1 && ind.self !== '? ? ' ? `p${n}=${ind.p}&n${n}=${ind.n}${ind.oc ? `&oc${n}=${ind.oc}` : ''}` : `i${n}=${ind.i}`}${ind.t ? `&t${n}=${encodeURIComponent(ind.t)}` : ''}`;
-}
+  saveBtn.addEventListener('click', () => {
+    const t = descInput.value;
+    const ind = { i, p, n, oc, t, self };
+    const storedInds = JSON.parse(localStorage.getItem('inds')) || [];
+    storedInds.push(ind);
+    localStorage.setItem('inds', JSON.stringify(storedInds));
+    descInput.value = '';
+    updateUI();
+  });
 
-function buildTooltipLine(ind, n) {
-  return `${n}. ${ind.self} (${ind.i ? `i=${ind.i}` : ''}${ind.p ? `/${ind.p}` : ''}${ind.n ? `/${ind.n}` : ''}${ind.oc && !ind.i ? `/${ind.oc}` : ''}${ind.t ? `/${ind.t}` : ''})\n`;
+  clearBtn.addEventListener('click', () => {
+    localStorage.removeItem('inds');
+    updateUI();
+  });
+
+  function updateUI() {
+    const storedInds = JSON.parse(localStorage.getItem('inds')) || [];
+    const { url, tooltip } = buildUrlAndTooltip(storedInds);
+    const urlRLM = `?m=RLM${cgibase}${url}`;
+    const urlEditRLM = `?m=TP&v=updRLM${cgibase}${url}`;
+    genBtn.href = urlRLM;
+    genBtn.title = tooltip;
+    editBtn.href = urlEditRLM;
+    const hasData = storedInds.length >= 1;
+    clearBtn.classList.toggle('d-none', !hasData);
+    graphBtns.classList.toggle('d-none', !hasData);
+  }
+
+  function buildUrlAndTooltip(storedInds) {
+    let url = '';
+    let tooltip = '';
+    storedInds.forEach((ind, idx) => {
+      const num = idx + 1;
+      url += buildUrlParams(ind, num);
+      tooltip += `${num}. ${ind.self} (${ind.i ? 'i=' + ind.i : ''}${ind.p ? '/' + ind.p : ''}${ind.n ? '/' + ind.n : ''}${ind.oc && !ind.i ? '/' + ind.oc : ''}${ind.t ? '/' + ind.t : ''})\n`;
+    });
+    return { url, tooltip: tooltip.trim() };
+  }
+
+  function buildUrlParams(ind, num) {
+    const useKey = ak === '1' && ind.self !== '? ? ';
+    const ident = useKey
+      ? `p${num}=${ind.p}&n${num}=${ind.n}${ind.oc ? `&oc${num}=${ind.oc}` : ''}`
+      : `i${num}=${ind.i}`;
+    const text = ind.t ? `&t${num}=${encodeURIComponent(ind.t)}` : '';
+    return `&${ident}${text}`;
+  }
+
+  updateUI();
 }
