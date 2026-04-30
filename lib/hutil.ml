e2c56139a317d70d6c4c43c02a680f3e07ee55ab
Henri83 <henri.gouraud@laposte.net>
2026-04-30 11:34:32 +0200

    fix Claude review for hutil.ml

--- lib/hutil.ml
+++ lib/hutil.ml
@@ -30,12 +30,19 @@ let include_home_template conf =
       }
   in
   try Templ.output conf ifun Templ.Env.empty () "home"
-  with _ -> error_cannot_access conf "home"
+  with Sys_error _ | Not_found -> error_cannot_access conf "home"
 
 let link_to_referer conf =
   let referer = Util.get_referer conf in
   let back = Utf8.capitalize_fst (Util.transl conf "back") in
-  if (referer :> string) <> "" then
+  (* Validate the scheme to block javascript: URIs that survive escape_html
+     and could be used as XSS vectors in an href attribute. *)
+  let referer_s = (referer :> string) in
+  let safe_scheme =
+    Util.starts_with referer_s "http://"
+    || Util.starts_with referer_s "https://"
+  in
+  if referer_s <> "" && safe_scheme then
     ({|<a href="|} ^<^ referer
      ^>^ {|" class="btn btn-sm btn-link p-0 border-0" title="|} ^ back
      ^ {|"><i class="fa fa-arrow-left-long fa-fw fa-sm"></i></a>|}
@@ -44,11 +51,15 @@ let link_to_referer conf =
 
 let header_without_http_nor_home conf title =
   let robot = List.assoc_opt "robot_index" conf.base_env = Some "yes" in
+  (* conf.lang comes from the URL; escape it to prevent attribute injection
+     (e.g. lang="fr" onload="..."). In practice lang is validated as a known
+     language code, but the injection site itself should be safe regardless. *)
   let str1 =
     Printf.sprintf {|<!DOCTYPE html>
 <html lang="%s">
 <head>
-<title>|} conf.lang
+<title>|}
+      (Util.escape_html conf.lang :> string)
   in
   let str2 =
     Printf.sprintf
@@ -105,6 +116,10 @@ let header_with_title ?(error = false) ?(fluid = false) conf title =
   Output.print_sstring conf "</h1>\n"
 
 let header_with_adaptive_title ?(fluid = false) conf title_content =
+  (* Note: title_content is injected directly into HTML. Callers are
+     responsible for ensuring it is already safe (escaped or trusted). Unlike
+     other header functions that receive a printing callback, this one accepts
+     a pre-rendered string, so there is no type-level guarantee of safety. *)
   let fluid = fluid || is_fluid conf in
   Util.html conf;
   header_without_http_nor_home conf (fun _ -> ());
@@ -182,15 +197,27 @@ let trailer_with_extra_js conf extra_js_sources =
                      Output.printf conf
                        {|<script src="js/%s?hash=%s"></script>|} source hash
                  | None ->
+                     let prefix =
+                       let p = conf.etc_prefix in
+                       if p = "" || p.[String.length p - 1] = '/' then p
+                       else p ^ "/"
+                     in
                      Output.printf conf {|<script src="%sjs/%s"></script>|}
-                       conf.etc_prefix source)
+                       prefix source)
              | None -> ()
            else
-             try
-               Output.print_sstring conf "<script>\n";
-               Templ.output_simple conf Templ.Env.empty source;
-               Output.print_sstring conf "\n</script>\n"
-             with _ -> ())
+             (* First check the template is accessible, then emit the <script>
+                wrapper. This avoids an unclosed <script> tag when the template
+                is missing, which would cause the browser to interpret the rest
+                of the HTML page as script content. *)
+             match Util.open_etc_file conf source with
+             | None -> ()
+             | Some (ic, _) ->
+                 close_in ic;
+                 Output.print_sstring conf "<script>\n";
+                 (try Templ.output_simple conf Templ.Env.empty source
+                  with Sys_error _ | Not_found -> ());
+                 Output.print_sstring conf "\n</script>\n")
        sources);
   Templ.output_simple conf Templ.Env.empty "js";
   let query_time = Unix.gettimeofday () -. conf.query_start in
