4e55dad833f643a712bd585ac378c12d4e58df18
Elie Canonici Merle <elie.canonicimerle@geneanet.org>
2026-06-01 12:18:06 +0200

    check for files in base directory before sync

--- bin/init-search-indexes/init_search_indexes.ml
+++ bin/init-search-indexes/init_search_indexes.ml
@@ -1,3 +1,14 @@
+let files_exist database_path =
+  List.exists
+    (fun fname -> Sys.file_exists @@ Filename.concat database_path fname)
+    [
+      "snames_marital.dat";
+      "snames_marital.inx";
+      "snames_marital_lower.dat";
+      "snames_marital_lower.inx";
+      "patches";
+    ]
+
 let initialize_search_indexes database =
   Gwdb.load_persons_array database;
   Gwdb.load_strings_array database;
@@ -27,15 +38,24 @@ let main () =
       raise Exit
     in
     Arg.parse options
-      (fun database ->
-        Lock.control (Files.lock_file database) false ~onerror:on_lock_error
+      (fun database_path ->
+        let database_path =
+          if Filename.check_suffix database_path ".gwb" then database_path
+          else database_path ^ ".gwb"
+        in
+        Lock.control
+          (Files.lock_file database_path)
+          false ~onerror:on_lock_error
         @@ fun () ->
         let database =
-          let () = Secure.set_base_dir @@ Filename.dirname database in
-          try Gwdb.open_base database
+          let () = Secure.set_base_dir @@ Filename.dirname database_path in
+          try Gwdb.open_base database_path
           with e ->
             Printexc.raise_with_backtrace e (Printexc.get_raw_backtrace ())
         in
+        if files_exist database_path then (
+          Printf.eprintf "Base %s already has necessary files\n" database_path;
+          raise Exit);
         Fun.protect
           ~finally:(fun () -> Gwdb.close_base database)
           (fun () -> initialize_search_indexes database))
