215782f500daa85ce390381b0da88e04ac982e69
Idir Lankri <idir.lankri@geneanet.org>
2026-06-05 17:11:40 +0200

    Cleanup: Dispose of the function `Ext_array.forall2`
    In favor of the function `Array.for_all2`.

--- lib/util/ext_array.ml
+++ lib/util/ext_array.ml
@@ -9,13 +9,3 @@ let except v a =
     else loop (i + 1)
   in
   loop 0
-
-let forall2 f a1 a2 =
-  if Array.length a1 <> Array.length a2 then invalid_arg "array_forall2"
-  else
-    let rec loop i =
-      if i = Array.length a1 then true
-      else if f a1.(i) a2.(i) then loop (i + 1)
-      else false
-    in
-    loop 0
