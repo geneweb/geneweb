geneweb/geneweb

commit d7507e3956361c06517037b8585c69fd19e29dcc

Refactor gwc binary
The code of gwc binary is a pretty mess. This commit improves it a bit.

Changes:
- Refactor and clarify the CLI parsing.
- Do not overwrite `.gwo` if it already exists.
- Do not write `.gwo` files in the same directory of it source `.gw`. It
  is really unexpected behavior. Usually, commands write output in the
  current directory by default or specified directory with `-o`.
  Unfortunately, the `-o` option is already used to give the name of the
  output base.
- Do not use a global reference to pass arguments to the `Gwcomp.compile`
  function. We are not writing in COBOL.
- Simplify the code for the iterator over several gwo files. We should
  introduce a pipeline in `gwcomp` or use an immutable state for the
  linker.

bin/gwc/gwc.ml
@@ -1,100 +1,151 @@
 (* Copyright (c) 1998-2007 INRIA *)
-
+module Compat = Geneweb_compat
+module GWPARAM = Geneweb.GWPARAM
 module Dirs = Geneweb_dirs
 
-(** Checks a .gwo header and prints fails if header is absent or not compatible.
-*)
-let check_magic fname ic =
-  let b = really_input_string ic (String.length Gwcomp.magic_gwo) in
-  if b <> Gwcomp.magic_gwo then
-    if String.sub Gwcomp.magic_gwo 0 4 = String.sub b 0 4 then
-      failwith ("\"" ^ fname ^ "\" is a GeneWeb object file, but not compatible")
-    else
+type kind = Gw | Gwo
+
+type input = {
+  fname : string;
+  kind : kind;
+  separate : bool;
+  bnotes : Db1link.bnotes;
+  shift : int;
+}
+
+let failwith fmt = Format.ksprintf failwith fmt
+
+(* Checks a .gwo header and prints fails if header is absent or not
+   compatible. *)
+let check_magic ~fname ic =
+  let len = String.length Gwcomp.magic_gwo in
+  let magic = really_input_string ic len in
+  if String.compare magic Gwcomp.magic_gwo <> 0 then
+    if String.sub Gwcomp.magic_gwo 0 4 = String.sub magic 0 4 then
       failwith
-        ("\"" ^ fname
-       ^ "\" is not a GeneWeb object file, or it is a very old version")
-
-(** [next_family_fun_templ gwo_list fi] creates a function that read sucessivly
-    a [Gwcomp.gw_syntax] for all .gwo files. In details it does :
-
-    - Switch to the next element in the [gwo_list] if reached the end of the
-      current file. Each element is [(gwo,separate, bnotes, shift)] where [gwo]
-      is .gwo filename and [separate], [bnotes], [shift] are captured options
-      from command line related to the giving file.
-    - Modify [fi] with mentioned previusly information if needed.
-    - Start/continue to read current .gwo file content and return
-      [Gwcomp.gw_syntax]. [None] is returned when reading of the last .gwo file
-      reaches end of file *)
-let next_family_fun_templ gwo_list fi =
-  let ngwo = List.length gwo_list in
-  let run =
-    if ngwo < 10 || not !Gwcomp.verbose then fun () -> ()
-    else if ngwo < 60 then (fun () ->
-      Printf.eprintf ".";
-      flush stderr)
+        "%S is a GeneWeb object file but not compatible with the current \
+         GeneWeb version."
+        fname
     else
-      let bar_cnt = ref 0 in
-      let run () =
-        ProgrBar.run !bar_cnt ngwo;
-        incr bar_cnt
-      in
-      ProgrBar.empty := 'o';
-      ProgrBar.full := '*';
-      ProgrBar.start ();
-      run
+      failwith "%S is not a GeneWeb object file or it is a very old version."
+        fname
+
+let open_gwo_file file_info (fname, separate, bnotes, shift, _was_gw) =
+  let ic = open_in_bin fname in
+  check_magic ~fname ic;
+  file_info.Db1link.f_curr_src_file <- input_value ic;
+  file_info.Db1link.f_curr_gwo_file <- fname;
+  file_info.Db1link.f_separate <- separate;
+  file_info.Db1link.f_bnotes <- bnotes;
+  file_info.Db1link.f_shift <- shift;
+  Hashtbl.clear file_info.Db1link.f_local_names;
+  ic
+
+let iterator_gwo_file file_info input =
+  let ic = open_gwo_file file_info input in
+  let next () =
+    match (input_value ic : Gwcomp.gw_syntax) with
+    | exception End_of_file -> None
+    | v -> Some v
   in
-  let ic_opt = ref None in
-  let gwo_list = ref gwo_list in
-  fun () ->
-    let rec loop () =
-      let r =
-        match !ic_opt with
-        | Some ic -> (
-            match
-              try Some (input_value ic : Gwcomp.gw_syntax)
-              with End_of_file -> None
-            with
-            | Some fam -> Some fam
-            | None ->
-                close_in ic;
-                ic_opt := None;
-                None)
-        | None -> None
+  (ic, next)
+
+(* FIXME: File descriptor could leak if an exception is raised by the code
+   using this iterator. This is not a big issue as these exceptions are
+   not catch in the current code and file descriptors are released by the
+   operating system at the end of the current process. *)
+let iterator_gwo_files bar inputs file_info =
+  let queue = Queue.of_seq @@ List.to_seq inputs in
+  let curr = ref (iterator_gwo_file file_info @@ Queue.pop queue) in
+  let len = Queue.length queue in
+  let cnt = ref 0 in
+  let rec next () =
+    let ic, it = !curr in
+    match it () with
+    | exception exn ->
+        let bt = Printexc.get_raw_backtrace () in
+        close_in_noerr ic;
+        Printexc.raise_with_backtrace exn bt
+    | Some _ as v -> v
+    | None -> (
+        close_in ic;
+        match Queue.pop queue with
+        | exception Queue.Empty -> None
+        | gwo ->
+            incr cnt;
+            ProgrBar.progress bar !cnt len;
+            curr := iterator_gwo_file file_info gwo;
+            next ())
+  in
+  next
+
+let generate_database ~no_warn ~bdir bar gwo_files =
+  let iterator = iterator_gwo_files bar gwo_files in
+  match Db1link.link ~no_warn iterator bdir with
+  | exception Db1link.Critical_import_error msg ->
+      Fmt.epr "CRITICAL ERROR: %s@." msg;
+      exit 2
+  | true -> ()
+  | false ->
+      Fmt.epr "Database not created.@.";
+      exit 2
+
+let compile_gw_files ~bname bar inputs =
+  let len = List.length inputs in
+  List.mapi
+    (fun i { fname; kind; separate; bnotes; shift } ->
+      let fname, was_gw =
+        match kind with
+        | Gw ->
+            let output = Format.sprintf "%so" (Filename.basename fname) in
+            let () =
+              try Gwcomp.compile ~bname ~output fname
+              with e ->
+                let bt = Printexc.get_raw_backtrace () in
+                Printf.eprintf "File %S, line %d:\n" fname !Gwcomp.line_cnt;
+                Printexc.raise_with_backtrace e bt
+            in
+            (output, true)
+        | Gwo -> (fname, false)
       in
-      let bnotes_of_string = function
-        | "merge" -> `merge
-        | "erase" -> `erase
-        | "first" -> `first
-        | "drop" -> `drop
-        | _ -> assert false
-      in
-      match r with
-      | Some fam -> Some fam
-      | None -> (
-          (* switch to the next .gwo file *)
-          match !gwo_list with
-          | (x, separate, bnotes, shift) :: rest ->
-              run ();
-              gwo_list := rest;
-              let ic = open_in_bin x in
-              check_magic x ic;
-              fi.Db1link.f_curr_src_file <- input_value ic;
-              fi.Db1link.f_curr_gwo_file <- x;
-              fi.Db1link.f_separate <- separate;
-              fi.Db1link.f_bnotes <- bnotes_of_string bnotes;
-              fi.Db1link.f_shift <- shift;
-              Hashtbl.clear fi.Db1link.f_local_names;
-              ic_opt := Some ic;
-              loop ()
-          | [] ->
-              if ngwo < 10 || not !Gwcomp.verbose then ()
-              else if ngwo < 60 then (
-                Printf.eprintf "\n";
-                flush stderr)
-              else ProgrBar.finish ();
-              None)
-    in
-    loop ()
+      ProgrBar.progress bar i len;
+      (fname, separate, bnotes, shift, was_gw))
+    inputs
+
+let base_dir = ref None
+let set_base_dir s = base_dir := Some s
+let just_comp = ref false
+let kill_gwo = ref false
+let no_warn = ref false
+let ngrams_arg = ref None
+let prog = ref false
+let default_separate = false
+let separate = ref default_separate
+let default_bnotes = Db1link.Merge
+let bnotes = ref default_bnotes
+let raise_bad fmt = Format.ksprintf (fun s -> raise (Arg.Bad s)) fmt
+
+let bnotes_to_string b =
+  match b with
+  | Db1link.Merge -> "merge"
+  | Erase -> "erase"
+  | First -> "first"
+  | Drop -> "drop"
+
+let parse_bnotes s =
+  match s with
+  | "merge" -> Db1link.Merge
+  | "erase" -> Erase
+  | "first" -> First
+  | "drop" -> Drop
+  | _ -> raise_bad "Unexpected bnote value %S" s
+
+let set_bnotes s = bnotes := parse_bnotes s
+let shift = ref 0
+let rev_inputs : input list ref = ref []
+let add_input i = rev_inputs := i :: !rev_inputs
+let output : string option ref = ref None
+let set_output s = output := Some s
 
 (* Parse -ngrams argument: <bi>[,<tri>[,<quad>]] *)
 let parse_ngrams s =
@@ -111,45 +162,38 @@ let parse_ngrams s =
       (bi, tri, quad)
   | [ bi_str; tri_str; quad_str ] ->
       (int_of_string bi_str, int_of_string tri_str, int_of_string quad_str)
-  | _ -> failwith "Invalid -ngrams format. Use: <bi>[,<tri>[,<quad>]]"
+  | _ -> raise_bad "Invalid -ngrams format. Use: <bi>[,<tri>[,<quad>]]"
 
-let just_comp = ref false
-let in_file = ref ""
-let separate = ref false
-let bnotes = ref "merge"
-let shift = ref 0
-let files = ref []
-let kill_gwo = ref false
-let no_warn = ref false
-let ngrams_arg = ref None
+let set_ngrams_arg s = ngrams_arg := Some (parse_ngrams s)
 
 let speclist =
   [
     ( "-bd",
-      Arg.String Secure.set_base_dir,
+      Arg.String set_base_dir,
       Fmt.str
         "<DIR> Specify where the “bases” directory with databases is installed \
          (default if empty is %S)."
         Dirs.(name Secure.default_base_dir) );
     ( "-bnotes",
-      Arg.Set_string bnotes,
+      Arg.String set_bnotes,
       " [drop|erase|first|merge] Behavior for base notes of the next file. \
        [drop]: dropped. [erase]: erase the current content. [first]: dropped \
        if current content is not empty. [merge]: concatenated to the current \
-       content. Default: " ^ !bnotes ^ "" );
+       content. Default: "
+      ^ bnotes_to_string default_bnotes );
     ("-c", Arg.Set just_comp, " Only compiling");
     ("-cg", Arg.Set Db1link.do_consang, " Compute consanguinity");
     ( "-ds",
       Arg.Set_string Db1link.default_source,
       "<str> Set the source field for persons and families without source data"
     );
-    ("-f", Arg.Set Geneweb.GWPARAM.force, " Remove database if already existing");
+    ("-f", Arg.Set GWPARAM.force, " Remove database if already existing");
     ("-gwo", Arg.Set kill_gwo, " Suppress .gwo files after base creation");
     ("-mem", Arg.Set Geneweb_db.Outbase.save_mem, " Save memory, but slower");
     ("-nc", Arg.Clear Db1link.do_check, " No consistency check");
     ("-nofail", Arg.Set Gwcomp.no_fail, " No failure in case of error");
     ( "-ngrams",
-      Arg.String (fun s -> ngrams_arg := Some (parse_ngrams s)),
+      Arg.String set_ngrams_arg,
       "<bi>[,<tri>[,<quad>]] N-gram indexing thresholds (e.g., '500,20,10' or \
        '500')" );
     ("-nolock", Arg.Set Lock.no_lock_flag, " Do not lock database");
@@ -158,18 +202,19 @@ let speclist =
       " Do not create associative pictures" );
     ("-nowarn", Arg.Set no_warn, " Do not show warnings during import");
     ( "-o",
-      Arg.Set_string Gwcomp.out_file,
+      Arg.String set_output,
       "<file> Output database (default: <input file name>.gwb, a.gwb if not \
        available). Alphanumerics and -" );
     ( "-particles",
       Arg.Set_string Db1link.particules_file,
       "<file> Particles file (default = predefined particles)" );
     ("-q", Arg.Clear Gwcomp.verbose, " Quiet");
-    ("-reorg", Arg.Set Geneweb.GWPARAM.reorg, " Mode reorg");
+    ("-reorg", Arg.Set GWPARAM.reorg, " Mode reorg");
     ("-rgpd", Arg.String (fun s -> Gwcomp.rgpd_dir := s), "<dir> Rgpd directory");
     ("-sep", Arg.Set separate, " Separate all persons in next file");
     ("-sh", Arg.Set_int shift, "<int> Shift all persons numbers in next files");
     ("-stats", Arg.Set Db1link.pr_stats, " Print statistics");
+    ("-prog", Arg.Set prog, " show progress bar");
     ("-v", Arg.Set Gwcomp.verbose, " Verbose");
     ( "-roglo_special",
       Arg.Set Gwcomp.roglo_special,
@@ -177,118 +222,102 @@ let speclist =
   ]
   |> List.sort compare |> Arg.align
 
-let anonfun x =
+let anonfun fname =
   let bn = !bnotes in
   let sep = !separate in
-  if Filename.check_suffix x ".gw" then in_file := x
-  else if Filename.check_suffix x ".gwo" then in_file := x
-  else raise (Arg.Bad ("Don't know what to do with \"" ^ x ^ "\""));
-  separate := false;
-  bnotes := "merge";
-  files := (x, sep, bn, !shift) :: !files
-
-let errmsg =
-  "Usage: gwc [options] [files]\n\
-   where [files] are a list of files:\n\
-  \  source files end with .gw\n\
-  \  object files end with .gwo\n\
-   and [options] are:"
-
-let main () =
-  let print_duration time_elapsed =
-    flush stderr;
-    flush stdout;
-    Printf.printf "Duration: %d min %d sec\n"
-      (int_of_float (time_elapsed /. 60.0))
-      (int_of_float time_elapsed mod 60)
+  let kind =
+    if Filename.check_suffix fname ".gw" then Gw
+    else if Filename.check_suffix fname ".gwo" then Gwo
+    else raise_bad "Don't know what to do with %S" fname
   in
-  let start_time = Unix.gettimeofday () in
+  separate := default_separate;
+  bnotes := default_bnotes;
+  add_input { fname; kind; separate = sep; bnotes = bn; shift = !shift }
+
+let parse_output inputs output =
+  match (inputs, output) with
+  | [], _ -> raise_bad "You must specify at least one input file."
+  | _ :: _ :: _, None ->
+      raise_bad
+        "In presence of several input files, we cannot infer the output \
+         database name. Please specify it with -o option."
+  | [ { fname; _ } ], None ->
+      (* We infer the name of the database in this case. *)
+      Filename.(basename (remove_extension fname))
+  | _, Some bname -> bname
+
+let parse_cmd () =
+  let errmsg =
+    "Usage: gwc [options] [files]\n\
+     where [files] are a list of files:\n\
+    \  source files end with .gw\n\
+    \  object files end with .gwo\n\
+     and [options] are:"
+  in
   Arg.parse speclist anonfun errmsg;
-  if not (Array.mem "-bd" Sys.argv) then Secure.set_base_dir ".";
-  if Array.mem "-rgpd" Sys.argv then (
-    if
-      not (Sys.file_exists !Gwcomp.rgpd_dir && Sys.is_directory !Gwcomp.rgpd_dir)
-    then (
-      Printf.eprintf "Error: RGPD directory not found\n";
-      exit 2);
-    Gwcomp.rgpd := true);
-  in_file :=
-    if !in_file <> "" then
-      Filename.remove_extension (Filename.basename !in_file)
-    else !in_file;
-  if List.length !files > 1 && not (Array.mem "-o" Sys.argv) then (
-    Printf.eprintf "The database name must be specified with -o\n";
-    flush stdout;
-    exit 2);
-  if !in_file <> "" && not (Array.mem "-o" Sys.argv) then
-    Gwcomp.out_file := !in_file;
-  if not (Mutil.good_name (Filename.basename !Gwcomp.out_file)) then (
-    Printf.eprintf "The database name \"%s\" contains a forbidden character.\n"
-      !Gwcomp.out_file;
-    Printf.eprintf "Allowed characters: a..z, A..Z, 0..9, -\n";
-    flush stderr;
-    exit 2);
-  let bname = Filename.remove_extension (Filename.basename !Gwcomp.out_file) in
-  Geneweb.GWPARAM.init ();
+  let inputs = List.rev !rev_inputs in
+  let bname = parse_output inputs !output in
+  let base_dir = Option.value ~default:Filename.current_dir_name !base_dir in
+  (inputs, bname, base_dir)
+
+let with_timer f =
+  let start = Unix.gettimeofday () in
+  let result = f () in
+  let stop = Unix.gettimeofday () in
+  (result, stop -. start)
+
+let pp_duration ppf d =
+  Fmt.pf ppf "%d min %d sec" (int_of_float (d /. 60.0)) (int_of_float d mod 60)
+
+let ( // ) = Filename.concat
+
+let check_database_exists base_dir bname =
+  let path = base_dir // Fmt.str "%s.gwb" bname in
+  Sys.file_exists path
+
+let cleanup gwo_files =
+  List.iter
+    (fun (fname, _, _, _, was_gw) -> if was_gw then Mutil.rm fname)
+    gwo_files
+
+let () =
+  let inputs, bname, base_dir = parse_cmd () in
+  Secure.set_base_dir base_dir;
+  GWPARAM.init ();
   let dist_etc_d = Filename.concat (Filename.dirname Sys.argv.(0)) "etc" in
   if !Db1link.particules_file = "" then
     Db1link.particules_file := Filename.concat dist_etc_d "particles.txt";
-  if not !just_comp then Geneweb.GWPARAM.check_base_exists bname;
   if !Gwcomp.verbose then
     if !Gwcomp.rgpd then
-      Printf.eprintf "Rgpd status: True, files in: %s\n" !Gwcomp.rgpd_dir
-    else Printf.eprintf "Rgpd status: False\n";
-  let gwo = ref [] in
-  List.iter
-    (fun (x, separate, bnotes, shift) ->
-      if Filename.check_suffix x ".gw" then (
-        (try Gwcomp.comp_families x
-         with e ->
-           Printf.eprintf "File \"%s\", line %d:\n" x !Gwcomp.line_cnt;
-           raise e);
-        gwo := (x ^ "o", separate, bnotes, shift) :: !gwo)
-      else if Filename.check_suffix x ".gwo" then
-        gwo := (x, separate, bnotes, shift) :: !gwo
-      else raise (Arg.Bad ("Don't know what to do with \"" ^ x ^ "\"")))
-    (List.rev !files);
+      Format.eprintf "Rgpd status: True, files in: %s@." !Gwcomp.rgpd_dir
+    else Format.eprintf "Rgpd status: False@.";
+  let gwo_files, duration =
+    with_timer @@ fun () ->
+    ProgrBar.with_bar ~disabled:(not !prog) Format.std_formatter @@ fun bar ->
+    compile_gw_files bar ~bname inputs
+  in
+  Format.eprintf "Compilation: %a@." pp_duration duration;
   if not !just_comp then (
-    let bdir = Geneweb.GWPARAM.create_base_and_config bname in
-    let time_elapsed = Unix.gettimeofday () -. start_time in
-    print_duration time_elapsed;
+    if (not !GWPARAM.force) && check_database_exists base_dir bname then (
+      if !kill_gwo then cleanup gwo_files;
+      Fmt.epr "Database %S already exists. Use -f to overwrite.@." bname;
+      exit 2);
+    let bdir = GWPARAM.create_base_and_config bname in
     let lock_file = Mutil.lock_file bdir in
     let on_exn exn bt =
-      Format.eprintf "%a@." Lock.pp_exception (exn, bt);
+      Fmt.epr "%a@." Lock.pp_exception (exn, bt);
       exit 2
     in
-
-    Lock.control ~on_exn ~wait:false ~lock_file (fun () ->
-        let next_family_fun = next_family_fun_templ (List.rev !gwo) in
-        (match !ngrams_arg with
-        | Some (bi, tri, quad) ->
-            Geneweb_db.Outbase.set_ngram_thresholds bi tri quad;
-            Printf.eprintf "N-grams enabled: bi≥%d, tri≥%d, quad≥%d\n" bi tri
-              quad;
-            flush stderr
-        | None -> ());
-        try
-          if Db1link.link ~no_warn:!no_warn next_family_fun bdir then (
-            if !kill_gwo then
-              List.iter
-                (fun (x, _separate, _bnotes, _shift) ->
-                  if Sys.file_exists (x ^ "o") then Mutil.rm (x ^ "o"))
-                (List.rev !files);
-            let time_elapsed = Unix.gettimeofday () -. start_time in
-            print_duration time_elapsed)
-          else (
-            Printf.eprintf "*** database NOT created\n";
-            let time_elapsed = Unix.gettimeofday () -. start_time in
-            print_duration time_elapsed;
-            exit 2)
-        with Db1link.Critical_import_error msg ->
-          Printf.eprintf "\n*** CRITICAL ERROR: %s\n" msg;
-          Printf.eprintf "*** Import aborted. Database NOT created.\n";
-          Printf.eprintf "*** Fix your source file before retrying.\n";
-          flush stderr;
-          exit 2))
-
-let _ = main ()
+    Option.iter
+      (fun (bi, tri, quad) ->
+        Geneweb_db.Outbase.set_ngram_thresholds bi tri quad;
+        Format.eprintf "N-grams enabled: bi≥%d, tri≥%d, quad≥%d@." bi tri quad)
+      !ngrams_arg;
+    Lock.control ~on_exn ~wait:false ~lock_file @@ fun () ->
+    let (), duration =
+      with_timer @@ fun () ->
+      ProgrBar.with_bar ~disabled:(not !prog) Format.std_formatter @@ fun bar ->
+      generate_database ~no_warn:!no_warn ~bdir bar gwo_files
+    in
+    Format.eprintf "Database generation: %a@." pp_duration duration;
+    if !kill_gwo then cleanup gwo_files)

bin/gwc/db1link.ml
@@ -73,13 +73,15 @@ type cbase = {
 (** State of the base collecting all information at link time used to create
     further Geneweb database *)
 
+type bnotes = Merge | Erase | First | Drop
+
 type file_info = {
   (* current .gw filename *)
   mutable f_curr_src_file : string; (* current .gwo filename *)
   mutable f_curr_gwo_file : string;
       (* all persons from current file should be separated  *)
   mutable f_separate : bool; (* behavior for base notes from current file *)
-  mutable f_bnotes : [ `merge | `erase | `first | `drop ];
+  mutable f_bnotes : bnotes;
       (* shift all persons from the current file with the given number *)
   mutable f_shift : int;
       (* Table that associates person's names hash and its occurence number
@@ -1120,7 +1122,7 @@ let insert_notes fname gen key str =
     content [str] that is treated by the way mentioned in
     [gen.g_file_info.f_bnotes]. *)
 let insert_bnotes fname gen nfname str =
-  if gen.g_file_info.f_bnotes <> `drop then
+  if gen.g_file_info.f_bnotes <> Drop then
     let old_nread = gen.g_base.c_bnotes.nread in
     (* Convert path notation from 'dir1:dir2:file' to 'dir1/dir2/file'
        (if a valid path) *)
@@ -1134,11 +1136,10 @@ let insert_bnotes fname gen nfname str =
     let bnotes =
       let str =
         match gen.g_file_info.f_bnotes with
-        | `drop -> assert false
-        | `erase -> str
-        | `merge -> old_nread nfname RnAll ^ str
-        | `first -> (
-            match old_nread nfname RnAll with "" -> str | str -> str)
+        | Drop -> assert false
+        | Erase -> str
+        | Merge -> old_nread nfname RnAll ^ str
+        | First -> ( match old_nread nfname RnAll with "" -> str | str -> str)
       in
       {
         nread = (fun f n -> if f = nfname then str else old_nread f n);
@@ -1674,7 +1675,7 @@ let link ?(no_warn = false) next_family_fun bdir =
       f_curr_gwo_file = "";
       f_separate = false;
       f_shift = 0;
-      f_bnotes = `merge;
+      f_bnotes = Merge;
     }
   in
   let gen =

bin/gwc/db1link.mli
@@ -15,11 +15,13 @@ val pr_stats : bool ref
 val particules_file : string ref
 (** File containing the particles to use *)
 
+type bnotes = Merge | Erase | First | Drop
+
 type file_info = {
   mutable f_curr_src_file : string;
   mutable f_curr_gwo_file : string;
   mutable f_separate : bool;
-  mutable f_bnotes : [ `drop | `erase | `first | `merge ];
+  mutable f_bnotes : bnotes;
   mutable f_shift : int;
   mutable f_local_names : (int * int, int) Hashtbl.t;
 }

bin/gwc/dune
@@ -1,5 +1,11 @@
+(library
+ (name gwcomp)
+ (libraries geneweb)
+ (modules gwcomp))
+
 (executable
  (package geneweb)
  (name gwc)
  (public_name gwc)
- (libraries unix geneweb))
+ (libraries unix gwcomp)
+ (modules gwc db1link))

commit 2d4cc826818ec7e8967a5509f86185b18e94f8a8

Allow to create a fresh database with `gwc`
The commit https://github.com/geneweb/geneweb/commit/d7507e39 introduced
a bug in `gwc`. The command
```
dune exec -- bin/gwc/gwc.exe -o foo
```
should create an empty database from scratch. Instead, the command
emits an error because we don't provide a gwo or gw files.

This commit restores the previous behavior and a cram test to prevent
from breaking it again in the future.

bin/gwc/gwc.ml
@@ -56,28 +56,30 @@ let iterator_gwo_file file_info input =
    operating system at the end of the current process. *)
 let iterator_gwo_files bar inputs file_info =
   let queue = Queue.of_seq @@ List.to_seq inputs in
-  let curr = ref (iterator_gwo_file file_info @@ Queue.pop queue) in
-  let len = Queue.length queue in
-  let cnt = ref 0 in
-  let rec next () =
-    let ic, it = !curr in
-    match it () with
-    | exception exn ->
-        let bt = Printexc.get_raw_backtrace () in
-        close_in_noerr ic;
-        Printexc.raise_with_backtrace exn bt
-    | Some _ as v -> v
-    | None -> (
-        close_in ic;
-        match Queue.pop queue with
-        | exception Queue.Empty -> None
-        | gwo ->
-            incr cnt;
-            ProgrBar.progress bar !cnt len;
-            curr := iterator_gwo_file file_info gwo;
-            next ())
-  in
-  next
+  if Queue.is_empty queue then fun _ -> None
+  else
+    let curr = ref (iterator_gwo_file file_info @@ Queue.pop queue) in
+    let len = Queue.length queue in
+    let cnt = ref 0 in
+    let rec next () =
+      let ic, it = !curr in
+      match it () with
+      | exception exn ->
+          let bt = Printexc.get_raw_backtrace () in
+          close_in_noerr ic;
+          Printexc.raise_with_backtrace exn bt
+      | Some _ as v -> v
+      | None -> (
+          close_in ic;
+          match Queue.pop queue with
+          | exception Queue.Empty -> None
+          | gwo ->
+              incr cnt;
+              ProgrBar.progress bar !cnt len;
+              curr := iterator_gwo_file file_info gwo;
+              next ())
+    in
+    next
 
 let generate_database ~no_warn ~bdir bar gwo_files =
   let iterator = iterator_gwo_files bar gwo_files in
@@ -247,7 +249,10 @@ let anonfun fname =
 
 let parse_output inputs output =
   match (inputs, output) with
-  | [], _ -> raise_bad "You must specify at least one input file."
+  | [], None ->
+      raise_bad
+        "You must specify at least one input file or a base name for a fresh \
+         database."
   | _ :: _ :: _, None ->
       raise_bad
         "In presence of several input files, we cannot infer the output \

test/gwc/run.t
@@ -39,8 +39,34 @@
   [2]
 
   $ gwc -bd .
-  Fatal error: exception Stdlib.Arg.Bad("You must specify at least one input file.")
+  Fatal error: exception Stdlib.Arg.Bad("You must specify at least one input file or a base name for a fresh database.")
   [2]
+
+  $ gwc -bd . -o bar
+  Compilation: 0 min 0 sec
+  Migration check for bar:
+    Classic .gwf exists: false
+    Reorg .gwf exists: false
+  Creating default configuration: bar.gwf
+  
+  pcnt 0 persons 0
+  fcnt 0 families 0
+  scnt 2 strings 3
+  *** saving persons array
+  *** saving ascends array
+  *** saving unions array
+  *** saving families array
+  *** saving couples array
+  *** saving descends array
+  *** saving strings array
+  *** create name index
+  *** create strings of sname
+  *** create strings of fname
+  *** create string index
+  *** create surname index
+  *** create first name index
+  *** ok
+  Database generation: 0 min 0 sec
 
   $ gwc -gwo -bd . ../galichet.gw
   Compilation: 0 min 0 sec
@@ -178,4 +204,3 @@
   Database generation: 0 min 0 sec
   ../galichet.gwo galichet.gwo differ: char 16, line 1
   [1]
-