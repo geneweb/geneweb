12194f20981184d7f814021bb0320e48526259ab
Henri83 <henri.gouraud@laposte.net>
2026-06-27 13:56:21 +0200

    handle -o for gwb2ged too. In gwu no more relative file test

--- bin/gwb2ged/gwb2ged.ml
+++ bin/gwb2ged/gwb2ged.ml
@@ -27,6 +27,15 @@ let ansel_warning =
 let () =
   let opts = ref Gwexport.default_opts in
   Arg.parse (speclist opts) anonfun usage;
+  (* open output file now that bases_dir and out_file are both known *)
+  let oc, name, close =
+    if !Gwexport.out_file = "" then (stdout, "", fun () -> flush stdout)
+    else
+      let path = Filename.concat !bases_dir !Gwexport.out_file in
+      let oc = open_out path in
+      (oc, !Gwexport.out_file, fun () -> close_out oc)
+  in
+  opts := { !opts with Gwexport.oc = (name, output_string oc, close) };
   if !opts.Gwexport.charset = Gwexport.Ansel then
     Printf.eprintf "%s\n%!" ansel_warning;
   if !bname = "" then (
