aa0937d1d28422fc2545494210dcf6ec44fdad87
Callixte de Vulpillières <callixte.devulpillieres@mistral.ai>
2026-07-06 02:14:35 +0200

    No more empty/unsufficient-enthropy HMAC

--- bin/gwd/gwd.ml
+++ bin/gwd/gwd.ml
@@ -2020,10 +2020,7 @@ let null_reopen flags fd =
    If the [random] argument is [false], the salt is always the same.
    The default is [true]. *)
 let generate_secret_salt ?(random = true) () =
-  if random then (
-    Random.self_init ();
-    string_of_int @@ Random.bits ())
-  else ""
+  if random then Geneweb_oidc.Oidc.generate_token () else ""
 
 let retrieve_secret_salt () =
   match Unix.getenv "SECRET_SALT" with
