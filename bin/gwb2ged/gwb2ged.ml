9e5a349bb893194fdc8ad6aca2dd8308cc9e2e6a
A2 <a2line@gmail.com>
2026-07-07 07:06:43 +0200

    Fix base and output resolution in gwb2ged
    anonfun still set Secure.base_dir to the dirname of the bare base
    name and nothing set it to -bd afterwards, breaking database access
    under a non-default bases directory. -o was prefixed with -bd
    unconditionally, breaking absolute paths, and the output file was
    created before the base name was validated. Use
    Gwexport.bases_dir/resolve_out_file and open the output inside the
    Some branch.

--- bin/gwb2ged/gwb2ged.ml
+++ bin/gwb2ged/gwb2ged.ml
@@ -1,12 +1,9 @@
 module Driver = Geneweb_db.Driver
-module Dirs = Geneweb_dirs
 
 let with_indexes = ref false
-let bases_dir = ref (Dirs.path Secure.default_base_dir)
 
 let speclist opts =
-  ("-bd", Arg.String (fun s -> bases_dir := s), "Bases folder")
-  :: ("-indexes", Arg.Set with_indexes, " export indexes in gedcom")
+  ("-indexes", Arg.Set with_indexes, " export indexes in gedcom")
   :: Gwexport.speclist opts
   |> List.sort (fun (a, _, _) (b, _, _) -> String.compare a b)
   |> Arg.align
@@ -15,10 +12,8 @@ let bname = ref None
 
 let anonfun s =
   match !bname with
-  | None ->
-      Secure.set_base_dir (Filename.dirname s);
-      bname := Some s
-  | _ -> raise (Arg.Bad "Cannot treat several databases")
+  | None -> bname := Some s
+  | Some _ -> raise (Arg.Bad "Cannot treat several databases")
 
 let usage = "Usage: " ^ Filename.basename Sys.argv.(0) ^ " [options] base"
 
@@ -29,26 +24,23 @@ let ansel_warning =
 let () =
   let opts = ref Gwexport.default_opts in
   Arg.parse (speclist opts) anonfun usage;
-  (* open output file now that bases_dir and out_file are both known *)
-  let oc, name, close =
-    if !Gwexport.out_file = "" then (stdout, "", fun () -> flush stdout)
-    else
-      let path = Filename.concat !bases_dir !Gwexport.out_file in
-      let oc = open_out path in
-      (oc, !Gwexport.out_file, fun () -> close_out oc)
-  in
-  opts := { !opts with Gwexport.oc = (name, output_string oc, close) };
+  let bases_dir = !Gwexport.bases_dir in
+  Secure.set_base_dir bases_dir;
   if !opts.Gwexport.charset = Gwexport.Ansel then
     Printf.eprintf "%s\n%!" ansel_warning;
   match !bname with
   | None ->
       Arg.usage (speclist opts) usage;
       exit 2
-  | _ ->
-      ();
-      let bpath =
-        Filename.concat !bases_dir (Option.value ~default:"" !bname)
+  | Some bname ->
+      let oc, name, close =
+        if !Gwexport.out_file = "" then (stdout, "", fun () -> flush stdout)
+        else
+          let path = Gwexport.resolve_out_file () in
+          let oc = open_out path in
+          (oc, path, fun () -> close_out oc)
       in
-      Driver.with_database bpath @@ fun base ->
+      opts := { !opts with Gwexport.oc = (name, output_string oc, close) };
+      Driver.with_database (Filename.concat bases_dir bname) @@ fun base ->
       let select = Gwexport.select base !opts [] in
       Gwb2gedLib.gwb2ged base !with_indexes !opts select
