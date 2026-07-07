481e87ce8e0d15d5d07eff3a793052fced8adfe5
Idir Lankri <idir.lankri@geneanet.org>
2026-07-07 12:32:41 +0200

    lib/util: Add an infix operator for `Option.bind`

--- lib/util/ext_option.mli
+++ lib/util/ext_option.mli
@@ -1,3 +1,7 @@
 val return_if : bool -> (unit -> 'a) -> 'a option
 (** [return_if false return] is [None] and [return_if true return] is
     [Some (return ())].  *)
+
+module Infix : sig
+  val ( >>= ) : 'a option -> ('a -> 'b option) -> 'b option
+end
