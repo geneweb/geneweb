945f158c577b658141ad8db92a5b187dbe14a137
Callixte de Vulpillières <callixte.devulpillieres@mistral.ai>
2026-07-14 00:00:35 +0200

    M3: unified OIDC enabled check

--- bin/gwd/gwd_oidc.mli
+++ bin/gwd/gwd_oidc.mli
@@ -1,3 +1,7 @@
+val enabled : (string * string) list -> bool
+(** [enabled base_env] is [true] when [oidc_provider_url] is set for the base,
+    i.e. OIDC is turned on (even if other required keys are still missing). *)
+
 val cookie_access :
   secret:string -> string list -> string -> (char * string * string) option
 (** Access ([w]/[f], user, username) from a valid signed OIDC session cookie in
