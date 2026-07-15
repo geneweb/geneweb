geneweb/geneweb

commit 1333d08f3f52662cb2718931b59a1c738e3b8dc9

Cleanup: Use the type `Uri.t` for the URLs

lib/placeDisplay.ml
@@ -8,13 +8,12 @@ let print_html_places_surnames conf base
   let print_sn (sn, ips) =
     let len = List.length ips in
     Output.print_sstring conf "<a href=\"";
-    Output.print_string conf (Util.commd conf);
-    if link_to_ind && len = 1 then
-      Output.print_string conf
-        (Util.acces conf base @@ Util.pget conf base @@ List.hd ips)
-    else (
-      Output.print_sstring conf "m=N&v=";
-      Output.print_string conf (Mutil.encode sn));
+    Output.print_url conf
+      (Util.commd' conf
+         ~query:
+           (if link_to_ind && len = 1 then
+            Util.acces conf base @@ Util.pget conf base @@ List.hd ips
+           else [ ("m", "N"); ("v", sn) ]));
     Output.print_sstring conf "\">";
     Output.print_string conf (Util.escape_html sn);
     Output.print_sstring conf "</a> (";
@@ -59,12 +58,13 @@ let print_html_places_surnames conf base
   Output.print_sstring conf "</ul>\n"
 
 let print_aux_opt ~add_birth ~add_baptism ~add_death ~add_burial ~add_marriage =
-  Adef.encoded
-  @@ (if add_birth then "&bi=on" else "")
-  ^ (if add_baptism then "&bp=on" else "")
-  ^ (if add_death then "&de=on" else "")
-  ^ (if add_burial then "&bu=on" else "")
-  ^ if add_marriage then "&ma=on" else ""
+  let open Ext_list.Infix in
+  Ext_option.return_if add_birth (fun () -> ("bi", "on"))
+  @?: Ext_option.return_if add_baptism (fun () -> ("bp", "on"))
+  @?: Ext_option.return_if add_death (fun () -> ("de", "on"))
+  @?: Ext_option.return_if add_burial (fun () -> ("bu", "on"))
+  @?: Ext_option.return_if add_marriage (fun () -> ("ma", "on"))
+  @?: []
 
 let print_aux conf title fn =
   Hutil.header conf title;
@@ -94,22 +94,20 @@ let print_all_places_surnames_short conf base ~add_birth ~add_baptism ~add_death
         print_aux_opt ~add_birth ~add_baptism ~add_death ~add_burial
           ~add_marriage
       in
+      let open Ext_list.Infix in
       Output.print_sstring conf "<p><a href=\"";
-      Output.print_string conf (Util.commd conf);
-      Output.print_sstring conf "m=PS";
-      Output.print_string conf opt;
-      Output.print_sstring conf "&display=long\">";
+      Output.print_url conf
+        (Util.commd' conf
+           ~query:(("m", "PS") @:: opt @ [ ("display", "long") ]));
+      Output.print_sstring conf "\">";
       Output.print_sstring conf (Util.transl conf "long display");
       Output.print_sstring conf "</a></p><p>";
       let last = Array.length array - 1 in
       Array.iteri
         (fun i (s, x) ->
           Output.print_sstring conf "<a href=\"";
-          Output.print_string conf (Util.commd conf);
-          Output.print_sstring conf "m=PS";
-          Output.print_string conf opt;
-          Output.print_sstring conf "&k=";
-          Output.print_string conf (Mutil.encode s);
+          Output.print_url conf
+            (Util.commd' conf ~query:[ ("m", "PS"); ("k", s) ]);
           Output.print_sstring conf "\">";
           Output.print_string conf (Util.escape_html s);
           Output.print_sstring conf "</a> (";
@@ -168,13 +166,16 @@ let print_all_places_surnames_long conf base ini ~add_birth ~add_baptism
   in
   print_aux conf title (fun () ->
       if ini = "" then (
+        let open Ext_list.Infix in
         Output.print_sstring conf "<p><a href=\"";
-        Output.print_string conf (Util.commd conf);
-        Output.print_sstring conf "m=PS";
-        Output.print_string conf
-          (print_aux_opt ~add_birth ~add_baptism ~add_death ~add_burial
-             ~add_marriage);
-        Output.print_sstring conf "&display=short\">";
+        Output.print_url conf
+          (Util.commd' conf
+             ~query:
+               (("m", "PS")
+               @:: print_aux_opt ~add_birth ~add_baptism ~add_death ~add_burial
+                     ~add_marriage
+               @ [ ("display", "short") ]));
+        Output.print_sstring conf "\">";
         Output.print_sstring conf (Util.transl conf "short display");
         Output.print_sstring conf "</a></p><p>");
       if array <> [||] then print_html_places_surnames conf base array)

bin/gwd/request.ml
@@ -76,8 +76,8 @@ let specify conf base n pl =
             (Geneweb.NameDisplay.referenced_person_title_text conf base p)
       | t :: _ ->
           Geneweb.Output.print_sstring conf {|<a href="|};
-          Geneweb.Output.print_string conf (Geneweb.Util.commd conf);
-          Geneweb.Output.print_string conf (Geneweb.Util.acces conf base p);
+          Geneweb.Output.print_url conf
+            (Geneweb.Util.commd' conf ~query:(Geneweb.Util.acces conf base p));
           Geneweb.Output.print_sstring conf {|"> |};
           Geneweb.Output.print_string conf
             (Geneweb.NameDisplay.title_html_of_person conf base p t);
@@ -144,18 +144,11 @@ let person_selected_with_redirect ~conf ~base ?(parameters = []) ~person () =
   | Some "R" -> relation_print conf base person
   | Some _ -> incorrect_request conf
   | None ->
-      let open Def in
       let url =
-        (Geneweb.Util.commd conf ^^^ Geneweb.Util.acces conf base person
-          :> string)
+        Geneweb.Util.commd' conf
+          ~query:(Geneweb.Util.acces conf base person @ parameters)
       in
-      let url =
-        List.fold_left
-          (fun url (param_name, param_value) ->
-            Printf.sprintf "%s&%s=%s" url param_name param_value)
-          url parameters
-      in
-      Wserver.http_redirect_temporarily url
+      Wserver.http_redirect_temporarily @@ Ext_uri.to_string url
 
 let updmenu_print = Geneweb.Perso.interp_templ "updmenu"
 

lib/allnDisplay.ml
@@ -80,7 +80,7 @@ let surname_list_meta_description conf base =
 
 let print_alphabetic_big conf base is_surnames ini list len too_big =
   let title = print_title conf base is_surnames ini len in
-  let mode = if is_surnames then Adef.encoded "N" else Adef.encoded "P" in
+  let mode = if is_surnames then "N" else "P" in
   let meta =
     if is_surnames then surname_list_meta_description conf base else []
   in
@@ -91,19 +91,13 @@ let print_alphabetic_big conf base is_surnames ini list len too_big =
     (fun ini_k ->
       if ini_k = ini then (
         Output.print_sstring conf {|<a href="|};
-        Output.print_string conf (Util.commd conf);
-        Output.print_sstring conf "m=";
-        Output.print_string conf mode;
-        Output.print_sstring conf "&tri=A&v=";
-        Output.print_string conf (Mutil.encode ini_k);
+        Output.print_url conf
+          (Util.commd' conf ~query:[ ("m", mode); ("tri", "A"); ("v", ini_k) ]);
         Output.print_sstring conf {|">|})
       else (
         Output.print_sstring conf {|<a href="|};
-        Output.print_string conf (Util.commd conf);
-        Output.print_sstring conf "m=";
-        Output.print_string conf mode;
-        Output.print_sstring conf "&tri=A&k=";
-        Output.print_string conf (Mutil.encode ini_k);
+        Output.print_url conf
+          (Util.commd' conf ~query:[ ("m", mode); ("tri", "A"); ("k", ini_k) ]);
         Output.print_sstring conf {|">|});
       Output.print_string conf (tr '_' "&nbsp;" ini_k |> Util.escape_html);
       Output.print_sstring conf "</a>\n")
@@ -115,29 +109,24 @@ let print_alphabetic_big conf base is_surnames ini list len too_big =
     Output.print_sstring conf (Util.transl conf ":");
     Output.print_sstring conf "</p><ul><li>";
     Output.print_sstring conf {|<a href="|};
-    Output.print_string conf (Util.commd conf);
-    Output.print_sstring conf "m=";
-    Output.print_string conf mode;
-    Output.print_sstring conf "&tri=A&o=A&k=";
-    Output.print_string conf (Mutil.encode ini);
+    Output.print_url conf
+      (Util.commd' conf
+         ~query:[ ("m", mode); ("tri", "A"); ("o", "A"); ("k", ini) ]);
     Output.print_sstring conf {|">|};
     Output.print_sstring conf (Util.transl conf "long display");
     Output.print_sstring conf "</a></li><li>";
     Output.print_sstring conf {|<a href="|};
-    Output.print_string conf (Util.commd conf);
-    Output.print_sstring conf "m=";
-    Output.print_string conf mode;
-    Output.print_sstring conf "&tri=S&o=A&k=";
-    Output.print_string conf (Mutil.encode ini);
+    Output.print_url conf
+      (Util.commd' conf
+         ~query:[ ("m", mode); ("tri", "S"); ("o", "A"); ("k", ini) ]);
     Output.print_sstring conf {|">|};
     Output.print_sstring conf (Util.transl conf "short display");
     Output.print_sstring conf "</a></li><li>";
     Output.print_sstring conf {|<a href="|};
-    Output.print_string conf (Util.commd conf);
-    Output.print_sstring conf "m=";
-    Output.print_string conf mode;
-    Output.print_sstring conf "&tri=S&o=A&cgl=on&k=";
-    Output.print_string conf (Mutil.encode ini);
+    Output.print_url conf
+      (Util.commd' conf
+         ~query:
+           [ ("m", mode); ("tri", "S"); ("o", "A"); ("cgl", "on"); ("k", ini) ]);
     Output.print_sstring conf {|">|};
     Output.print_sstring conf (Util.transl conf "short display");
     Output.print_sstring conf " + ";
@@ -195,7 +184,7 @@ let print_alphabetic_all conf base is_surnames ini list len =
 
 let print_alphabetic_small conf base is_surnames ini list len =
   let title = print_title conf base is_surnames ini len in
-  let mode = Adef.encoded (if is_surnames then "N" else "P") in
+  let mode = if is_surnames then "N" else "P" in
   let meta =
     if is_surnames then surname_list_meta_description conf base else []
   in
@@ -207,12 +196,9 @@ let print_alphabetic_small conf base is_surnames ini list len =
       (fun (_, s, cnt) ->
         Output.print_sstring conf "<li>";
         Output.print_sstring conf "<a href=\"";
-        Output.print_string conf (Util.commd conf);
-        Output.print_sstring conf "m=";
-        Output.print_string conf mode;
-        Output.print_sstring conf "&v=";
-        Output.print_string conf (Mutil.encode s);
-        Output.print_sstring conf "&t=A\">";
+        Output.print_url conf
+          (Util.commd' conf ~query:[ ("m", mode); ("v", s); ("t", "A") ]);
+        Output.print_sstring conf "\">";
         Output.print_string conf
           (particle_at_the_end base is_surnames s |> Util.escape_html);
         Output.print_sstring conf "</a> (";
@@ -227,7 +213,7 @@ let print_alphabetic_small conf base is_surnames ini list len =
 
 let print_frequency_any conf base is_surnames list len =
   let title = print_title conf base is_surnames "" len in
-  let mode = Adef.encoded (if is_surnames then "N" else "P") in
+  let mode = if is_surnames then "N" else "P" in
   let n = ref 0 in
   let conf = Config.Trimmed.to_config conf in
   Hutil.header conf title;
@@ -241,11 +227,8 @@ let print_frequency_any conf base is_surnames list len =
         List.iter
           (fun s ->
             Output.print_sstring conf "<li><a href=\"";
-            Output.print_string conf (Util.commd conf);
-            Output.print_sstring conf "m=";
-            Output.print_string conf mode;
-            Output.print_sstring conf "&v=";
-            Output.print_string conf (Mutil.encode (Name.lower s));
+            Output.print_url conf
+              (Util.commd' conf ~query:[ ("m", mode); ("v", Name.lower s) ]);
             Output.print_sstring conf "\">";
             Output.print_string conf
               (particle_at_the_end base is_surnames s |> Util.escape_html);
@@ -295,7 +278,7 @@ let print_alphabetic ~prefix ~all ~at_least ~fast ~index conf base is_surnames =
 
 let print_alphabetic_short conf base is_surnames ini list len =
   let title = print_title conf base is_surnames ini len in
-  let mode = Adef.encoded (if is_surnames then "N" else "P") in
+  let mode = if is_surnames then "N" else "P" in
   let need_ref = len >= 250 in
   let meta =
     if is_surnames then surname_list_meta_description conf base else []
@@ -320,10 +303,10 @@ let print_alphabetic_short conf base is_surnames ini list len =
       Ext_list.iter_first
         (fun first (s, cnt) ->
           let href =
-            let open Def in
-            " href=\"" ^<^ Util.commd conf
-            ^^^ ("m=" ^<^ mode ^^^ "&v=" ^<^ Mutil.encode s ^>^ "&t=A\""
-                  :> Adef.escaped_string)
+            " href=\""
+            ^ Ext_uri.to_string
+                (Util.commd' conf ~query:[ ("m", mode); ("v", s); ("t", "A") ])
+            ^ "\""
           in
           let name =
             Adef.encoded
@@ -331,7 +314,7 @@ let print_alphabetic_short conf base is_surnames ini list len =
           in
           if not first then Output.print_sstring conf ",";
           Output.print_sstring conf "\n<a";
-          Output.print_string conf href;
+          Output.print_sstring conf href;
           Output.print_string conf name;
           Output.print_sstring conf ">";
           Output.print_string conf

lib/birthDeathDisplay.ml
@@ -150,11 +150,8 @@ let old_print_statistics conf =
   Output.print_sstring conf "<ul>";
   let aux m label =
     Output.print_sstring conf {|<li><a href="|};
-    Output.print_string conf (Util.commd conf);
-    Output.print_sstring conf {|m=|};
-    Output.print_sstring conf m;
-    Output.print_sstring conf {|&k=|};
-    Output.print_sstring conf (string_of_int n);
+    Output.print_url conf
+      (Util.commd' conf ~query:[ ("m", m); ("k", string_of_int n) ]);
     Output.print_sstring conf {|">|};
     Output.print_sstring conf (Printf.sprintf (Util.ftransl conf label) n);
     Output.print_sstring conf {|</a></li>|}

lib/cousinsDisplay.ml
@@ -23,65 +23,69 @@ let give_access conf base ~cnt_sp ia_asex p1 b1 p2 b2 =
     if Person.is_empty p then s
     else
       let href =
-        let open Def in
-        Util.commd conf ^^^ "m=RL&"
-        ^<^ Util.acces_n conf base (Adef.escaped "1") p1
-        ^^^ "&b1="
-        ^<^ Sosa.to_string (Util.old_sosa_of_branch conf base (ia_asex :: b1))
-        ^<^ "&"
-        ^<^ Util.acces_n conf base (Adef.escaped "2") p2
-        ^^^ "&b2="
-        ^<^ Sosa.to_string (Util.old_sosa_of_branch conf base (ia_asex :: b2))
-        ^<^ ((if
-              (List.assoc_opt "spouse" conf.Config.env :> string option)
-              = Some "on"
-             then Adef.encoded "&spouse=on"
-             else Adef.encoded "")
-             ^^^ (if
-                  (List.assoc_opt "image" conf.Config.env :> string option)
-                  = Some "off"
-                 then Adef.encoded "&image=off"
-                 else Adef.encoded "")
-             ^^^ "&bd="
-             ^<^ Option.value ~default:(Adef.encoded "0")
-                   (List.assoc_opt "bd" conf.Config.env)
-              :> Adef.escaped_string)
+        let open Ext_list.Infix in
+        Util.commd' conf
+          ~query:
+            ((("m", "RL") @:: Util.acces_n conf base 1 p1)
+            @ ( "b1",
+                Sosa.to_string
+                  (Util.old_sosa_of_branch conf base (ia_asex :: b1)) )
+            @:: Util.acces_n conf base 2 p2
+            @ ( "b2",
+                Sosa.to_string
+                  (Util.old_sosa_of_branch conf base (ia_asex :: b2)) )
+            @:: Ext_option.return_if
+                  ((List.assoc_opt "spouse" conf.Config.env :> string option)
+                  = Some "on")
+                  (fun () -> ("spouse", "on"))
+            @?: Ext_option.return_if
+                  ((List.assoc_opt "image" conf.Config.env :> string option)
+                  = Some "off")
+                  (fun () -> ("image", "off"))
+            @?: [
+                  ( "bd",
+                    Option.value ~default:"0"
+                      (Option.map Mutil.decode
+                         (List.assoc_opt "bd" conf.Config.env)) );
+                ])
       in
       let open Def in
-      "<a href=\"" ^<^ (href :> Adef.safe_string) ^^^ "\">" ^<^ s ^>^ "</a>"
+      "<a href=\"" ^<^ Ext_uri.to_string href ^<^ "\">" ^<^ s ^>^ "</a>"
   in
   let reference_sp p3 _ _ p s =
     if Person.is_empty p then s
     else
       let href =
-        let open Def in
-        Util.commd conf ^^^ "m=RL&"
-        ^<^ Util.acces_n conf base (Adef.escaped "1") p1
-        ^^^ "&b1="
-        ^<^ Sosa.to_string (Util.old_sosa_of_branch conf base (ia_asex :: b1))
-        ^<^ "&"
-        ^<^ Util.acces_n conf base (Adef.escaped "2") p2
-        ^^^ "&b2="
-        ^<^ Sosa.to_string (Util.old_sosa_of_branch conf base (ia_asex :: b2))
-        ^<^ "&"
-        ^<^ Util.acces_n conf base (Adef.escaped "4") p3
-        ^^^ ((if
-              (List.assoc_opt "spouse" conf.Config.env :> string option)
-              = Some "on"
-             then Adef.encoded "&spouse=on"
-             else Adef.encoded "")
-             ^^^ (if
-                  (List.assoc_opt "image" conf.Config.env :> string option)
-                  = Some "off"
-                 then Adef.encoded "&image=off"
-                 else Adef.encoded "")
-             ^^^ "&bd="
-             ^<^ Option.value ~default:(Adef.encoded "0")
-                   (List.assoc_opt "bd" conf.Config.env)
-              :> Adef.escaped_string)
+        let open Ext_list.Infix in
+        Util.commd' conf
+          ~query:
+            (("m", "RL")
+            @:: Util.acces_n conf base 1 p1
+            @ ( "b1",
+                Sosa.to_string
+                  (Util.old_sosa_of_branch conf base (ia_asex :: b1)) )
+            @:: Util.acces_n conf base 2 p2
+            @ ( "b2",
+                Sosa.to_string
+                  (Util.old_sosa_of_branch conf base (ia_asex :: b2)) )
+            @:: Util.acces_n conf base 4 p3
+            @ Ext_option.return_if
+                ((List.assoc_opt "spouse" conf.Config.env :> string option)
+                = Some "on")
+                (fun () -> ("spouse", "on"))
+            @?: Ext_option.return_if
+                  ((List.assoc_opt "image" conf.Config.env :> string option)
+                  = Some "off")
+                  (fun () -> ("image", "off"))
+            @?: [
+                  ( "bd",
+                    Option.value ~default:"0"
+                      (Option.map Mutil.decode
+                         (List.assoc_opt "bd" conf.Config.env)) );
+                ])
       in
       let open Def in
-      "<a href=\"" ^<^ (href :> Adef.safe_string) ^^^ "\">" ^<^ s ^>^ "</a>"
+      "<a href=\"" ^<^ Ext_uri.to_string href ^<^ "\">" ^<^ s ^>^ "</a>"
   in
   let print_nospouse _ =
     Sosa_cache.print_sosa ~conf ~base ~person:p2 ~link:true;
@@ -408,24 +412,20 @@ let print_anniv conf base p dead_people level =
       set Gwdb.IperMap.empty
   in
   let txt_of (up_sosa, down_br, spouse) conf base c =
-    let href : Adef.escaped_string =
-      let open Def in
-      Util.commd conf ^^^ "m=RL&"
-      ^<^ Util.acces_n conf base (Adef.escaped "1") p
-      ^^^ "&b1=" ^<^ string_of_int up_sosa ^<^ "&"
-      ^<^ Util.acces_n conf base (Adef.escaped "2")
-            (Option.fold ~none:c ~some:(Util.pget conf base) spouse)
-      ^^^ "&b2="
-      ^<^ string_of_int (sosa_of_persons conf base down_br)
-      ^<^ (if spouse = None then
-           "&" ^<^ Util.acces_n conf base (Adef.escaped "4") c
-          else Adef.escaped "")
-      ^>^ "&spouse=on"
+    let href =
+      let open Ext_list.Infix in
+      Util.commd' conf
+        ~query:
+          (("m", "RL") @:: Util.acces_n conf base 1 p
+          @ ("b1", string_of_int up_sosa)
+          @:: Util.acces_n conf base 2
+                (Option.fold ~none:c ~some:(Util.pget conf base) spouse)
+          @ ("b2", string_of_int (sosa_of_persons conf base down_br))
+          @:: (if spouse = None then Util.acces_n conf base 4 c else [])
+          @ [ ("spouse", "on") ])
     in
     let open Def in
-    "<a href=\""
-    ^<^ (href :> Adef.safe_string)
-    ^^^ "\">"
+    "<a href=\"" ^<^ Ext_uri.to_string href ^<^ "\">"
     ^<^ NameDisplay.person_title_text conf base c
     ^>^ "</a>"
   in

lib/dagDisplay.ml
@@ -4,23 +4,32 @@ let image_normal_txt conf base p fname width height =
   let k = Image.default_portrait_filename base p in
   let r =
     Format.sprintf
-      {|<img src="%sm=IM&d=%s&%s&k=%s"%s%s alt="%s" title="%s"
+      {|<img src="%s"%s%s alt="%s" title="%s"
         style="%s %s">|}
-      (Util.commd conf : Adef.escaped_string :> string)
-      (string_of_int
-      @@ int_of_float (mod_float s.Unix.st_mtime (float_of_int max_int)))
-      (Util.acces conf base p : Adef.escaped_string :> string)
-      k
+      (Ext_uri.to_string
+      @@ Util.commd' conf
+           ~query:
+             ([
+                ("m", "IM");
+                ( "d",
+                  string_of_int
+                  @@ int_of_float
+                       (mod_float s.Unix.st_mtime (float_of_int max_int)) );
+              ]
+             @ Util.acces conf base p
+             @ [ ("k", k) ]))
       (if width = 0 then "" else " width=" ^ string_of_int width)
       (if height = 0 then "" else " height=" ^ string_of_int height)
       image_txt image_txt
       (if width = 0 then "" else "max-width:" ^ string_of_int width ^ "px;")
       (if height = 0 then "" else "max-height:" ^ string_of_int height ^ "px;")
   in
-  Format.sprintf {|<a href="%sm=IM&%s&k=%s">%s</a>|}
-    (Util.commd conf : Adef.escaped_string :> string)
-    (Util.acces conf base p : Adef.escaped_string :> string)
-    k r
+  let open Ext_list.Infix in
+  Format.sprintf {|<a href="%s">%s</a>|}
+    (Ext_uri.to_string
+    @@ Util.commd' conf
+         ~query:((("m", "IM") @:: Util.acces conf base p) @ [ ("k", k) ]))
+    r
   |> Adef.safe
 
 let image_url_txt conf url_p url height : Adef.safe_string =
@@ -28,7 +37,7 @@ let image_url_txt conf url_p url height : Adef.safe_string =
   Format.sprintf
     {|<a href="%s"><img src="%s" alt="%s" title="%s"
       style="%s"></a>|}
-    (url_p : Adef.escaped_string :> string)
+    (Ext_uri.to_string url_p)
     (url : Adef.escaped_string :> string)
     image_txt image_txt
     (if height = 0 then "" else "max-height:" ^ string_of_int height ^ "px;")
@@ -39,7 +48,7 @@ let image_url_txt_with_size conf url_p url width height : Adef.safe_string =
   Format.sprintf
     {|<a href="%s"><img src="%s"%s%s alt="%s" title="%s"
       style="%s %s">%s</a>|}
-    (url_p : Adef.escaped_string :> string)
+    (Ext_uri.to_string url_p)
     (url : Adef.escaped_string :> string)
     (if width = 0 then "" else " width=" ^ string_of_int width)
     (if height = 0 then "" else " height=" ^ string_of_int height)
@@ -68,19 +77,13 @@ let image_txt conf base p =
           ^ (image_normal_txt conf base p s w h |> Adef.as_string)
           ^ "</td></tr></table></center>"
       | Some (`Url url, Some (wid, hei)) ->
-          let url_p =
-            let open Def in
-            Util.commd conf ^^^ Util.acces conf base p
-          in
+          let url_p = Util.commd' conf ~query:(Util.acces conf base p) in
           {|<br><center><table border="0"><tr align="left"><td>|}
           ^ (image_url_txt_with_size conf url_p (Util.escape_html url) wid hei
             |> Adef.as_string)
           ^ {|</td></tr></table></center>|}
       | Some (`Url url, None) ->
-          let url_p =
-            let open Def in
-            Util.commd conf ^^^ Util.acces conf base p
-          in
+          let url_p = Util.commd' conf ~query:(Util.acces conf base p) in
           let height = 75 in
           (* La hauteur est ajoutée à la table pour que les textes soient alignés. *)
           {|<br><center><table border="0" style="height:|}
@@ -512,36 +515,31 @@ let print_next_pos conf pos1 pos2 tcol =
           match k with "pos1" | "pos2" -> env | _ -> (k, v) :: env)
         conf.Config.env []
     in
-    let aux env =
-      List.iter
-        (fun (k, v) ->
-          Output.print_sstring conf k;
-          Output.print_sstring conf "=";
-          Output.print_string conf v;
-          Output.print_sstring conf ";")
-        env
-    in
+    let aux env = List.map (fun (k, v) -> (k, Mutil.decode v)) env in
     Output.print_sstring conf {|<div style="text-align:right">|};
     if pos1 = 0 then Output.print_sstring conf "&nbsp;"
     else (
       Output.print_sstring conf "<a href=\"";
-      Output.print_string conf (Util.commd conf);
-      Output.print_sstring conf "\"";
-      aux env;
-      Output.print_sstring conf "pos1=";
-      Output.print_sstring conf (string_of_int @@ (pos1 + overlap - dpos));
-      Output.print_sstring conf "&pos2=";
-      Output.print_sstring conf (string_of_int @@ (pos1 + overlap));
+      Output.print_url conf
+        (Util.commd' conf
+           ~query:
+             (aux env
+             @ [
+                 ("pos1", string_of_int @@ (pos1 + overlap - dpos));
+                 ("pos2", string_of_int @@ (pos1 + overlap));
+               ]));
       Output.print_sstring conf "\">&lt;&lt;</a>");
     if pos2 >= tcol then Output.print_sstring conf "&nbsp;"
     else (
       Output.print_sstring conf "<a href=\"";
-      Output.print_string conf (Util.commd conf);
-      aux env;
-      Output.print_sstring conf "pos1=";
-      Output.print_sstring conf (string_of_int @@ (pos2 - overlap));
-      Output.print_sstring conf "&pos2=";
-      Output.print_sstring conf (string_of_int @@ (pos2 - overlap + dpos));
+      Output.print_url conf
+        (Util.commd' conf
+           ~query:
+             (aux env
+             @ [
+                 ("pos1", string_of_int @@ (pos2 - overlap));
+                 ("pos2", string_of_int @@ (pos2 - overlap + dpos));
+               ]));
       Output.print_sstring conf "\">&gt;&gt;</a>");
     Output.print_sstring conf "</div>")
 
@@ -692,15 +690,12 @@ let print_html_table conf hts =
     Output.print_sstring conf {|<p><div style="text-align:|};
     Output.print_sstring conf conf.Config.right;
     Output.print_sstring conf {|"><a href="|};
-    Output.print_string conf (Util.commd conf);
-    List.iter
-      (fun (k, v) ->
-        Output.print_sstring conf k;
-        Output.print_sstring conf "=";
-        Output.print_string conf v;
-        Output.print_sstring conf ";")
-      conf.Config.env;
-    Output.print_sstring conf {|notab=on&slices=on"><tt>//</tt></a></div></p>|});
+    Output.print_url conf
+      (Util.commd' conf
+         ~query:
+           (List.map (fun (k, v) -> (k, Mutil.decode v)) conf.Config.env
+           @ [ ("notab", "on"); ("slices", "on") ]));
+    Output.print_sstring conf {|"><tt>//</tt></a></div></p>|});
   if
     Util.p_getenv conf.Config.env "notab" = Some "on"
     || Util.p_getenv conf.Config.env "pos2" <> None
@@ -843,7 +838,7 @@ let print_dag_page conf page_title hts next_txt =
   print_html_table conf hts;
   if (next_txt : Adef.escaped_string :> string) <> "" then (
     Output.print_sstring conf {|<p><a href="|};
-    Output.print_string conf (Util.commd conf);
+    Output.print_sstring conf (Util.commd_prefix conf);
     Output.print_string conf next_txt;
     Output.print_sstring conf {|">&gt;&gt;</a></p>|});
   Hutil.trailer conf

lib/descendDisplay.ml
@@ -503,7 +503,9 @@ let print_elem conf base paths precision (n, pll) =
           List.iter
             (fun p ->
               Output.print_sstring conf "<li><strong>";
-              Util.wprint_geneweb_link conf (Util.acces conf base p)
+              Util.wprint_geneweb_link conf
+                (Adef.escaped @@ Ext_uri.encoded_of_query
+               @@ Util.acces' conf base p)
                 (Gwdb.p_first_name base p |> Util.escape_html
                   :> Adef.safe_string);
               Output.print_sstring conf "</strong>";
@@ -1073,11 +1075,17 @@ let make_tree_hts conf base gv p =
           let options = Util.display_options conf in
           let ncol = nb_column 0 (v - 1) p in
           let vbar_txt =
-            let open Def in
-            Util.commd conf ^^^ "m=D&t=T&v=" ^<^ string_of_int gv ^<^ "&"
-            ^<^ options ^^^ "&" ^<^ Util.acces conf base p
+            let open Ext_list.Infix in
+            Util.commd' conf
+              ~query:
+                (("m", "D") @:: ("t", "T")
+                @:: ("v", string_of_int gv)
+                @:: options @ Util.acces conf base p)
           in
-          ((2 * ncol) - 1, Dag2html.CenterA, Dag2html.TDbar (Some vbar_txt))
+          ( (2 * ncol) - 1,
+            Dag2html.CenterA,
+            Dag2html.TDbar (Some (Adef.escaped @@ Ext_uri.to_string vbar_txt))
+          )
       | None -> (1, Dag2html.LeftA, Dag2html.TDnothing)
     in
     td :: tdl

lib/history.ml
@@ -480,7 +480,9 @@ and eval_string s = function
   | _ -> raise Not_found
 
 and eval_person_field_var conf base env p = function
-  | [ "access" ] -> safe_val (Util.acces conf base p :> Adef.safe_string)
+  | [ "access" ] ->
+      safe_val @@ Adef.safe @@ Ext_uri.encoded_of_query
+      @@ Util.acces' conf base p
   | [ "dates" ] -> safe_val (DateDisplay.short_dates_text conf base p)
   | [ "has_history" ] ->
       let fn = Gwdb.sou base (Gwdb.get_first_name p) in

commit 329bcec47bab874d54921d43b6b53fd46de97435

Places: Fix a bug introduced by 1333d08f3f52662cb2718931b59a1c738e3b8dc9

lib/placeDisplay.ml
@@ -107,7 +107,7 @@ let print_all_places_surnames_short conf base ~add_birth ~add_baptism ~add_death
         (fun i (s, x) ->
           Output.print_sstring conf "<a href=\"";
           Output.print_url conf
-            (Util.commd' conf ~query:[ ("m", "PS"); ("k", s) ]);
+            (Util.commd' conf ~query:(("m", "PS") @:: opt @ [ ("k", s) ]));
           Output.print_sstring conf "\">";
           Output.print_string conf (Util.escape_html s);
           Output.print_sstring conf "</a> (";