3c96ca0879ea841497b0014a7da779d168dc1a9d
Pierre Villemot <pierre.villemot@ocamlpro.com>
2026-08-19 20:09:06 +0200

    Terminate workers if the master terminates
    The workers could outlive the parent on Unix. For instance, run
    ```console
    dune exec gwd -- --debug
    kill -TERM `pid`
    ```
    where `pid` is the PID of the master process.
    
    On UNIX, the termination of the parent process doesn't produce a signal
    to its children. A simple and portable way to ensure termination of the
    children is to open pipe between the parent and the child. The pipe is
    closed if the parent dies and you can detect this event in the child.
    
    Notice that no cleanup operations is executed during the termination of
    the child.

--- http/pool.ml
+++ http/pool.ml
@@ -2,35 +2,53 @@ let src = Logs.Src.create ~doc:"Pool" "POOL"
 
 module Log = (val Logs.src_log src : Logs.LOG)
 
-type worker = { pid : int } [@@unboxed]
+type worker = int
 type t = { workers : (worker, unit) Hashtbl.t } [@@unboxed]
 
-let add_worker t k =
+(* Fork the current process using a dead man approach to detect
+   parent termination.
+
+   A pipe is established between the parent and the child process. The child
+   monitors the read-end of the pipe. If the parent process dies, the operating
+   system closes the write-end, unblocking the [Unix.select] call. *)
+let fork child parent =
+  let fd_in, fd_out = Unix.pipe ~cloexec:true () in
+  let check_dead_man () =
+    let fds, _, _ = My_unix.select_noeintr [ fd_in ] [] [] (-1.) in
+    assert (fds == [ fd_in ]);
+    Unix._exit 1
+  in
   match Unix.fork () with
   | 0 ->
-      while true do
-        k @@ Unix.getpid ()
-      done
+      Unix.close fd_out;
+      (* One doesn't need to join this watchdog thread at the end of the child
+         process, as the process will exit in [child]. *)
+      let (_ : Thread.t) = Thread.create check_dead_man () in
+      child ()
   | pid ->
-      Log.debug (fun k -> k "Creating worker %d" pid);
-      Hashtbl.replace t.workers { pid } ()
+      (* Intentionally keep [fd_out] open. It must remain inaccessible to
+         the rest of the parent and will be closed automatically by the
+         operating system if the parent dies. *)
+      Unix.close fd_in;
+      parent pid
 
-let cleanup { workers } =
-  Log.debug (fun k -> k "Cleanup workers...");
-  Hashtbl.iter
-    (fun { pid } () ->
-      Log.debug (fun k -> k "Kill worker %d" pid);
-      try Unix.kill pid Sys.sigterm with _ -> ())
-    workers
+let add_worker t k =
+  let child () =
+    while true do
+      k @@ Unix.getpid ()
+    done
+  in
+  let parent pid =
+    Log.debug (fun k -> k "Creating worker %d" pid);
+    Hashtbl.replace t.workers pid ()
+  in
+  fork child parent
 
 let wait_any_child () = My_unix.waitpid_noeintr [] (-1) |> fst
 
 let start n k =
   if (not Sys.unix) || n < 1 then invalid_arg "start";
   let t = { workers = Hashtbl.create n } in
-  let parent_pid = Unix.getpid () in
-  let finally () = if Unix.getpid () = parent_pid then cleanup t in
-  Fun.protect ~finally @@ fun () ->
   for _ = 0 to n - 1 do
     add_worker t k
   done;
@@ -39,7 +57,7 @@ let start n k =
     (* We never run out of children because each time a worker terminates,
        it is immediately replaced with a new one. *)
     assert (pid > 0);
-    match Hashtbl.find t.workers { pid } with
+    match Hashtbl.find t.workers pid with
     | () ->
         Log.debug (fun k -> k "Worker %d is dead, replace it" pid);
         add_worker t k
