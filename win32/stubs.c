c0f25af86ec8b69a9fa69e780939471b87913c8d
Pierre Villemot <pierre.villemot@ocamlpro.com>
2026-08-21 18:16:37 +0200

    WIP

--- win32/stubs.c
+++ win32/stubs.c
@@ -4,32 +4,32 @@
 #include <caml/unixsupport.h>
 #include <caml/fail.h>
 
-CAMLprim value geneweb_win32_fd_of_file_descr (value v) {
-  CAMLparam1 (v);
+CAMLprim value geneweb_win32_fd_of_file_descr (value file_descr) {
+  CAMLparam1 (file_descr);
   int fd;
 
 #if defined (_WIN32)
-  fd = caml_win32_CRT_fd_of_filedescr (v);
+  fd = caml_win32_CRT_fd_of_filedescr (file_descr);
 #else
-  fd = Int_val (v);
+  fd = Int_val (file_descr);
 #endif
 
   CAMLreturn (Val_int (fd));
 }
 
-CAMLprim value geneweb_win32_file_descr_of_fd (value v) {
-  CAMLparam1 (v);
-  CAMLlocal1 (fd);
+CAMLprim value geneweb_win32_file_descr_of_fd (value fd) {
+  CAMLparam1 (fd);
+  CAMLlocal1 (file_descr);
 
 #if defined (_WIN32)
-  HANDLE h = (HANDLE) _get_osfhandle (fd);
+  HANDLE h = (HANDLE) _get_osfhandle (Int_val (fd));
   if (h == INVALID_HANDLE_VALUE)
     caml_invalid_argument ("is not a handle!");
 
-  fd = caml_win32_alloc_socket ((SOCKET) h);
+  file_descr = caml_win32_alloc_socket ((SOCKET) h);
 #else
-  fd = v;
+  file_descr = fd;
 #endif
 
-  CAMLreturn (fd);
+  CAMLreturn (file_descr);
 }
