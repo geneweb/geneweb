geneweb/geneweb

commit 41e1f0707041a1d6536fc87aeb837f01120ed162

hide sensitive data unless wizard; sort bvar entries unless sort_bvar_entries=no

lib/templ.ml
@@ -183,6 +183,16 @@ let rec eval_variable (conf : Config.config) = function
       in
       func conf.lang Version.available_languages 0
   | [ "bvar"; "list" ] ->
+      let wizard_only =
+        [
+          "friend_passwd";
+          "wizard_passwd";
+          "manitou";
+          "supervisor";
+          "friend_passwd_file";
+          "wizard_passwd_file";
+        ]
+      in
       let is_duplicate key assoc_list =
         let rec aux count = function
           | [] -> count > 1
@@ -191,7 +201,9 @@ let rec eval_variable (conf : Config.config) = function
         aux 0 assoc_list
       in
       let l =
-        List.sort (fun (k1, _v1) (k2, _v2) -> compare k1 k2) conf.base_env
+        if List.assoc "sort_bvar_entries" conf.base_env = "no" then
+          conf.base_env
+        else List.sort (fun (k1, _v1) (k2, _v2) -> compare k1 k2) conf.base_env
       in
       List.fold_left
         (fun acc (k, v) ->
@@ -200,8 +212,11 @@ let rec eval_variable (conf : Config.config) = function
           in
           acc
           ^ Format.sprintf "<b%s>%s</b>=%s<br>\n" duplicate k
-              (Util.escape_html v :> string))
-        "" conf.base_env
+              (if conf.wizard || not (List.mem k wizard_only) then
+                 (Util.escape_html v :> string)
+               else if (Util.escape_html v :> string) = "" then ""
+               else "####"))
+        "" l
   | [ "gwd"; "arglist" ] -> !GWPARAM.gwd_cmd
   | [ "bvar"; v ] | [ "b"; v ] -> (
       try List.assoc v conf.base_env with Not_found -> "")

commit a354b93b9e9456fb817b0500ca722ee785f9d92a

Fix %bvar.list; rendered raw when sort_bvar_entries is unset
List.assoc raised Not_found when the .gwf does not define
sort_bvar_entries, which is the default case. The exception escaped
eval_variable and the template engine printed the variable verbatim
instead of the configuration parameter list in conf.txt.

Use List.assoc_opt: an absent key keeps the default sorted output,
only sort_bvar_entries=no preserves the .gwf order.

Fixes: 41e1f0707 ("... sort bvar entries unless sort_bvar_entries=no")

lib/templ.ml
@@ -201,7 +201,7 @@ let rec eval_variable (conf : Config.config) = function
         aux 0 assoc_list
       in
       let l =
-        if List.assoc "sort_bvar_entries" conf.base_env = "no" then
+        if List.assoc_opt "sort_bvar_entries" conf.base_env = Some "no" then
           conf.base_env
         else List.sort (fun (k1, _v1) (k2, _v2) -> compare k1 k2) conf.base_env
       in